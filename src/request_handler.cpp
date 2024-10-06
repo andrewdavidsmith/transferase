7685d8e2fb01c75593c1f672ab205a8eb82226a3
Andrew D Smith <andrewds@usc.edu>
2024-10-06 12:24:04 -0700

    src/request_handler.chpp: adding the server-side sources for handling requests; these deal with the methylomes and methylome set doing the actual computation

---
+++ src/request_handler.cpp
@@ -0,0 +1,129 @@
+/* MIT License
+ *
+ * Copyright (c) 2024 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "response.hpp"
+#include "request.hpp"
+#include "request_handler.hpp"
+#include "methylome.hpp"
+
+#include <filesystem>
+#include <format>
+#include <print>
+#include <sstream>
+#include <string>
+#include <cstdint>
+#include <vector>
+#include <fstream>
+#include <regex>
+#include <tuple>
+#include <chrono>
+
+namespace fs = std::filesystem;
+
+using std::format;
+using std::println;
+using std::string;
+using std::pair;
+using std::uint32_t;
+using std::get;
+
+namespace chrono = std::chrono;
+using hr_clock = chrono::high_resolution_clock;
+
+[[nodiscard]] static inline auto
+duration(const auto start, const auto stop) {
+  return chrono::duration_cast<chrono::duration<double>>(stop - start).count();
+}
+
+[[nodiscard]] static auto
+is_valid_accession(const string &accession) -> bool {
+  static constexpr auto experiment_ptrn = R"(^(D|E|S)RX\d+$)";
+  std::regex experiment_re(experiment_ptrn);
+  return std::regex_search(accession, experiment_re);
+}
+
+// ADS: This function needs a complete request *except* it does not
+// need the offsets to have been allocated or have any values read
+// into them.
+auto
+request_handler::handle_header(const request &req, response &resp) -> void {
+  // ADS: just to be sure?
+  resp.methylome_size = 0;
+  resp.counts.clear();
+
+  // verify that the accession makes sense
+  if (!is_valid_accession(req.accession)) {
+    if (verbose)
+      println("Malformed accession: {}.", req.accession);
+    resp.status = status_code::invalid_accession;
+    return;
+  }
+
+  // ADS TODO: move more of this into methylome_set?
+
+  const auto get_methylome_start{hr_clock::now()};
+  const auto meth_ec = ms.get_methylome(req.accession);
+  const auto get_methylome_stop{hr_clock::now()};
+  if (verbose)
+    println("Elapsed time for get methylome: {:.3}s",
+            duration(get_methylome_start, get_methylome_stop));
+  if (get<1>(meth_ec)) {
+    if (verbose)
+      println("Methylome not found: {}", req.accession);
+    resp.status = status_code::methylome_not_found;
+    return;
+  }
+
+  // confirm that the methylome size is as expected
+  if (req.methylome_size != ms.n_total_cpgs) {
+    if (verbose)
+      println("Incorrect methylome size (provided={}, expected={}).",
+              req.methylome_size, ms.n_total_cpgs);
+    resp.status = status_code::invalid_methylome_size;
+    return;
+  }
+
+  // ADS TODO: set up the methylome for loading here?
+  /* This would involve the methylome_set */
+
+  // ADS TODO: allocate the space to start reading offsets?
+  resp.methylome_size = req.methylome_size;
+}
+
+auto
+request_handler::handle_get_counts(const request &req, response &resp) -> void {
+  // assume methylome availability has been determined
+  const auto [m, ec] = ms.get_methylome(req.accession);
+  if (ec) {
+    if (verbose)
+      println("Failed to load methylome: {}", ec);
+    resp.status = status_code::server_failure;
+    return;
+  }
+
+  if (verbose)
+    println("Computing counts for methylome: {}", req.accession);
+
+  // generate the counts
+  resp.counts = m->get_counts(req.offsets);
+}
---
+++ src/request_handler.hpp
@@ -0,0 +1,58 @@
+/* MIT License
+ *
+ * Copyright (c) 2024 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SRC_REQUEST_HANDLER_HPP_
+#define SRC_REQUEST_HANDLER_HPP_
+
+#include "methylome_set.hpp"
+
+#include <string>
+
+struct response;
+struct request;
+
+// handles all incoming requests
+struct request_handler {
+  request_handler(const request_handler&) = delete;
+  request_handler& operator=(const request_handler&) = delete;
+
+  explicit request_handler(const std::string &methylome_dir,
+                           const std::uint32_t max_live_methylomes,
+                           bool verbose) :
+    methylome_dir{methylome_dir},
+    ms(verbose, max_live_methylomes, methylome_dir), verbose{verbose} {}
+
+  auto
+  handle_header(const request &req, response &resp) -> void;
+
+  auto
+  handle_get_counts(const request &req, response &resp) -> void;
+
+  std::string methylome_dir;  // dir of available methylomes
+  std::string cpg_index_file; // file with cpg_index
+  methylome_set ms;
+  cpg_index index;
+  bool verbose{};
+};
+
+#endif  // SRC_REQUEST_HANDLER_HPP_
