e9e9bde45dcc81c35e093023cd69de617f2598fc
Andrew D Smith <andrewds@usc.edu>
2024-10-08 17:04:21 -0700

    src/check.cpp: fixing some program logic

--- src/check.cpp
+++ src/check.cpp
@@ -28,20 +28,22 @@
 #include <boost/program_options.hpp>
 
 #include <chrono>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <print>
 #include <ranges>
 #include <string>
+#include <system_error>
 #include <tuple>
 #include <utility>
 #include <vector>
-#include <filesystem>
-#include <system_error>
 
-using std::cerr;
+using std::cout;
+using std::error_code;
 using std::make_pair;
 using std::ofstream;
+using std::ostream;
 using std::pair;
 using std::print;
 using std::println;
@@ -50,7 +52,6 @@ using std::string_view;
 using std::tuple;
 using std::uint32_t;
 using std::vector;
-using std::error_code;
 
 namespace vs = std::views;
 namespace fs = std::filesystem;
@@ -88,9 +89,10 @@ check_main(int argc, char *argv[]) -> int {
       return EXIT_SUCCESS;
     }
     po::notify(vm);
-  } catch (po::error &e) {
-    println(cerr, "{}", e.what());
-    desc.print(cerr);
+  }
+  catch (po::error &e) {
+    println(cout, "{}", e.what());
+    desc.print(cout);
     return EXIT_FAILURE;
   }
 
@@ -102,46 +104,49 @@ check_main(int argc, char *argv[]) -> int {
 
   cpg_index index{};
   if (index.read(index_file) != 0) {
-    println(cerr, "failed to read cpg index: {}", index_file);
+    println("failed to read cpg index: {}", index_file);
     return EXIT_FAILURE;
   }
 
   if (verbose)
     println("index:\n{}", index);
 
-  error_code errc;
-  const auto methylome_size =
-    fs::file_size(meth_file, errc)/methylome::record_size;
-  if (errc) {
-    println("Error obtaining methylome size from file: {}", meth_file);
-    return EXIT_FAILURE;
-  }
-
-  // total coverage; sites covered; mean methylation; weighted mean
-  // methylation
-
   methylome meth{};
   if (meth.read(meth_file, index.n_cpgs_total) != 0) {
-    println(cerr, "failed to read methylome: {}", meth_file);
+    println(cout, "failed to read methylome: {}", meth_file);
     return EXIT_FAILURE;
   }
+
+  const auto methylome_size = std::size(meth.cpgs);
+  const auto check_outcome =
+    (methylome_size == index.n_cpgs_total) ? "pass" : "fail";
 
   const auto total_counts = meth.total_counts();
 
-  println("methylome_size: {}", methylome_size);
-  println("index_size: {}", index.n_cpgs_total);
-  println("total_counts: {}", total_counts);
-  println("sites_covered_fraction: {}",
-          static_cast<double>(total_counts.n_covered) / methylome_size);
-  println("mean_meth_weighted: {}",
-          static_cast<double>(total_counts.n_meth) /
-          (total_counts.n_meth + total_counts.n_unmeth));
-
-  ofstream out(output_file);
+  std::ofstream of;
+  if (!output_file.empty())
+    of.open(output_file);
+  ostream out(output_file.empty() ? cout.rdbuf() : of.rdbuf());
   if (!out) {
-    println(cerr, "failed to open output file: {}", output_file);
+    println(cout, "failed to open output file: {}", output_file);
     return EXIT_FAILURE;
   }
+
+  const double n_reads = total_counts.n_meth + total_counts.n_unmeth;
+  const auto mean_meth_weighted = total_counts.n_meth / n_reads;
+  const auto sites_covered_fraction =
+    static_cast<double>(total_counts.n_covered) / methylome_size;
+
+  print(out,
+        "check: {}\n"
+        "methylome_size: {}\n"
+        "index_size: {}\n"
+        "total_counts: {}\n"
+        "sites_covered_fraction: {}\n"
+        "mean_meth_weighted: {}\n",
+        check_outcome,
+        methylome_size, index.n_cpgs_total, total_counts,
+        sites_covered_fraction, mean_meth_weighted);
 
   return EXIT_SUCCESS;
 }
