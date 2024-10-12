d808c8d5acbb81dc708624fdf4485608b8c2ad95
Andrew D Smith <andrewds@usc.edu>
2024-10-11 17:08:18 -0700

    src/lookup_client.cpp: added a request header and buffer, along with a response header and buffer in the mc16_client class

--- src/lookup_client.cpp
+++ src/lookup_client.cpp
@@ -68,8 +68,11 @@ using tcp = asio::ip::tcp;
 
 struct mc16_client {
   mc16_client(asio::io_context &io_context, const string &server,
-              const string &port, request &req, bool verbose = false) :
-    resolver(io_context), socket(io_context), req{std::move(req)},
+              const string &port, request_header &req_hdr, request &req,
+              bool verbose = false) :
+    resolver(io_context), socket(io_context),
+    req_hdr{req_hdr},
+    req{std::move(req)},  // part might be big
     verbose{verbose} {
     resolver.async_resolve(
       server, port,
@@ -92,14 +95,30 @@ struct mc16_client {
       if (verbose)
         println("Connected to server: {}",
                 boost::lexical_cast<string>(socket.remote_endpoint()));
-      req.to_buffer(buf);  // ADS: remove copying from this?
-      asio::async_write(
-        socket,
-        vector<asio::const_buffer>{
-          asio::buffer(buf.buf),
-          asio::buffer(req.offsets),
-        },
-        std::bind(&mc16_client::handle_write_request, this, ph::error));
+      const auto [hdr_ptr, hdr_err] =
+        to_chars(req_buf.data(), req_buf.data() + request_buf_size, req_hdr);
+      // req.to_buffer(buf);  // ADS: remove copying from this?
+      if (hdr_err) {
+        if (verbose)
+          println("Error forming request header: {}", hdr_err.message());
+      }
+      else {
+        const auto [req_ptr, req_err] =
+          to_chars(hdr_ptr, req_buf.data() + request_buf_size, req);
+        if (req_err) {
+          if (verbose)
+            println("Error forming request: {}", req_err.message());
+        }
+        else {
+          asio::async_write(
+            socket,
+            vector<asio::const_buffer>{
+              asio::buffer(req_buf),
+              asio::buffer(req.offsets),
+            },
+            std::bind(&mc16_client::handle_write_request, this, ph::error));
+        }
+      }
     }
     else if (verbose)
       println("Error connecting: {}", err.message());
@@ -108,7 +127,7 @@ struct mc16_client {
   auto handle_write_request(const bs::error_code &err) -> void {
     if (!err) {
       asio::async_read(
-        socket, asio::buffer(resp.buf), asio::transfer_exactly(resp.buf_size),
+        socket, asio::buffer(resp_buf), asio::transfer_exactly(response_buf_size),
         std::bind(&mc16_client::handle_read_response_header, this, ph::error));
     }
     else if (verbose)
@@ -118,15 +137,17 @@ struct mc16_client {
   auto handle_read_response_header(const bs::error_code &err) -> void {
     if (!err) {
       // ADS: convert the buffer into the values
-      const auto result = resp.from_buffer();
-      if (verbose)
-        println("Response header: {}", resp.summary_serial());
-      if (result == server_response_code::ok) {
-        do_read_counts();
+      const auto [resp_ptr, resp_err] = from_chars(resp_buf.data(),
+                                                   resp_buf.data() + response_buf_size,
+                                                   resp_hdr);
+      if (resp_err) {
+        println("Received error: {}", resp_err);
+        do_response_header_error(resp_err);  // ADS TODO: convert result into an error
       }
       else {
-        println("Received error: {}", result);
-        do_finish(err);  // ADS TODO: convert result into an error
+        if (verbose)
+          println("Response header: {}", resp_hdr.summary_serial());
+        do_read_counts();
       }
     }
     else if (verbose)
@@ -144,7 +165,17 @@ struct mc16_client {
   // ADS: need to move to using status codes as we probably don't
   // really care here about the bs::error_code here
   // auto do_finish(const status_code::value status) const -> void
+  // auto do_finish(const bs::error_code &err) const -> void {
   auto do_finish(const bs::error_code &err) const -> void {
+    if (!err) {  // status == status_code::ok
+      if (verbose)
+        println("Completing transaction: {}", err.message());  // status
+    }
+    else if (verbose)
+      println("Error reading counts: {}", err.message());  // status
+  }
+
+  auto do_response_header_error(const std::error_condition &err) const -> void {
     if (!err) {  // status == status_code::ok
       if (verbose)
         println("Completing transaction: {}", err.message());  // status
@@ -155,8 +186,11 @@ struct mc16_client {
 
   tcp::resolver resolver;
   tcp::socket socket;
-  request_buffer buf;
+  request_buffer req_buf;
+  request_header req_hdr;
   request req;
+  response_buffer resp_buf;
+  response_header resp_hdr;
   response resp;
   bool verbose{};
 };
@@ -233,14 +267,11 @@ lookup_client_main(int argc, char *argv[]) -> int {
     println("Elapsed time to get offsets: {:.3}s",
             duration(get_offsets_start, get_offsets_stop));
 
-  request req{accession,
-              index.n_cpgs_total,
-              0,  // ADS TODO: turn this into an enum?
-              static_cast<uint32_t>(size(offsets)),
-              offsets};
+  request_header hdr{accession, index.n_cpgs_total, 0};
+  request req{static_cast<uint32_t>(size(offsets)), offsets};
 
   asio::io_context io_context;
-  mc16_client mc16c(io_context, hostname, port, req, verbose);
+  mc16_client mc16c(io_context, hostname, port, hdr, req, verbose);
   const auto client_start{hr_clock::now()};
   io_context.run();
   const auto client_stop{hr_clock::now()};
@@ -249,7 +280,7 @@ lookup_client_main(int argc, char *argv[]) -> int {
             duration(client_start, client_stop));
 
   if (verbose)
-    println("Response summary:\n{}", mc16c.resp.summary());
+    println("Response summary:\n{}", mc16c.resp_hdr.summary());
 
   std::ofstream out(output_file);
   if (!out) {
