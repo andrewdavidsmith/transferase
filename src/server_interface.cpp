f6b670ea946202bd8f1026471a44b8b25c8ff97e
Andrew D Smith <andrewds@usc.edu>
2024-11-06 17:07:39 -0800

    src/server_interface.cpp: Gathering cli args as part of a move to enable config files

--- src/server_interface.cpp
+++ src/server_interface.cpp
@@ -30,14 +30,18 @@
 #include <boost/program_options.hpp>
 
 #include <cstdint>
+#include <format>
 #include <fstream>
 #include <iostream>
 #include <memory>
 #include <print>
 #include <string>
+#include <tuple>
+#include <vector>
 
 using std::cerr;
 using std::cout;
+using std::format;
 using std::make_shared;
 using std::ofstream;
 using std::ostream;
@@ -45,40 +49,51 @@ using std::print;
 using std::println;
 using std::shared_ptr;
 using std::string;
+using std::tuple;
 using std::uint32_t;
+using std::vector;
 
 namespace po = boost::program_options;
+namespace rg = std::ranges;
+
+template <mc16_log_level the_level, typename... Args>
+auto
+log_args(rg::input_range auto &&key_value_pairs) {
+  logger &lgr = logger::instance();
+  for (auto &&[k, v] : key_value_pairs)
+    lgr.log<the_level>("{}: {}", k, v);
+}
+
+struct server_interface_args {
+  static constexpr mc16_log_level default_log_level{mc16_log_level::debug};
+  static constexpr auto default_n_threads{4};
+  std::uint32_t n_threads{};
+  mc16_log_level log_level{};
+  std::string port{"5000"};
+  std::string hostname{};
+  std::string methylome_dir{};
+  std::string log_filename{};
+  std::uint32_t max_live_methylomes{};
+};
 
 auto
 server_interface_main(int argc, char *argv[]) -> int {
-  static constexpr auto default_n_threads{4};
-  static constexpr auto log_level{mc16_log_level::debug};
-
   static const auto description = "server";
 
-  bool verbose{};
-
-  string port{};
-  string hostname{};
-  string methylome_dir;
-  string log_filename;
-
-  uint32_t n_threads{};
-  uint32_t max_live_methylomes{};
+  server_interface_args args{};
 
   po::options_description desc(description);
   desc.add_options()
     // clang-format off
     ("help,h", "produce help message")
-    ("port,p", po::value(&port)->required(), "port")
-    ("hostname,H", po::value(&hostname)->required(), "server hostname")
-    ("methylomes,m", po::value(&methylome_dir)->required(), "methylome dir")
-    ("threads,t", po::value(&n_threads)->default_value(default_n_threads),
+    ("hostname,H", po::value(&args.hostname)->required(), "server hostname")
+    ("port,p", po::value(&args.port)->required(), "port")
+    ("methylomes,m", po::value(&args.methylome_dir)->required(), "methylome dir")
+    ("threads,t", po::value(&args.n_threads)->default_value(args.default_n_threads),
      "number of threads")
-    ("live,l", po::value(&max_live_methylomes)->default_value(
+    ("live,l", po::value(&args.max_live_methylomes)->default_value(
      methylome_set::default_max_live_methylomes), "max live methylomes")
-    ("log", po::value(&log_filename), "log file name")
-    ("verbose,v", po::bool_switch(&verbose), "print more run info")
+    ("log", po::value(&args.log_filename), "log file name")
     // clang-format on
     ;
   try {
@@ -97,25 +112,28 @@ server_interface_main(int argc, char *argv[]) -> int {
   }
 
   shared_ptr<ostream> log_file =
-    log_filename.empty() ? make_shared<ostream>(cout.rdbuf())
-                         : make_shared<ofstream>(log_filename, std::ios::app);
+    args.log_filename.empty()
+      ? make_shared<ostream>(cout.rdbuf())
+      : make_shared<ofstream>(args.log_filename, std::ios::app);
 
-  logger &lgr = logger::instance(log_file, description, log_level);
+  logger &lgr = logger::instance(log_file, description, args.log_level);
   if (!lgr) {
     println("Failure initializing logging: {}.", lgr.get_status().message());
     return EXIT_FAILURE;
   }
 
-  if (verbose)
-    println("Hostname: {}\n"
-            "Port: {}\n"
-            "Log file: {}\n"
-            "Methylome directory: {}\n"
-            "Max live methylomes: {}\n",
-            hostname, port, log_filename.empty() ? "console" : log_filename,
-            methylome_dir, max_live_methylomes);
+  log_args<mc16_log_level::info>(vector<tuple<string, string>>{
+    // clang-format off
+    {"Hostname", args.hostname},
+    {"Port", args.port},
+    {"Log file", args.log_filename.empty() ? "console" : args.log_filename},
+    {"Methylome directory", args.methylome_dir},
+    {"Max live methylomes", format("{}", args.max_live_methylomes)},
+    // clang-format on
+  });
 
-  server s(hostname, port, n_threads, methylome_dir, max_live_methylomes, lgr);
+  server s(args.hostname, args.port, args.n_threads, args.methylome_dir,
+           args.max_live_methylomes, lgr);
 
   s.run();
 
