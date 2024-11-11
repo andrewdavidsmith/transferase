b59171a5325323c8728933a76089d1c4ed3782c9
Andrew D Smith <andrewds@usc.edu>
2024-11-11 11:52:08 -0800

    src/server.cpp: added a function to start the server in daemon mode, which currently does all the daemonization work in a monolithic function that also does the ordinary work to start the server. this should be refactored

--- src/server.cpp
+++ src/server.cpp
@@ -30,13 +30,20 @@
 #include <csignal>  // std::raise
 #include <cstdint>
 #include <cstring>  // strsignal
-#include <memory>   // std::make_shared<>
+#include <filesystem>
+#include <format>
+#include <memory>  // std::make_shared<>
 #include <print>
 #include <string>
+#include <system_error>
 #include <thread>
 #include <utility>
 #include <vector>
 
+#include <syslog.h>
+#include <unistd.h>
+
+using std::format;
 using std::jthread;
 using std::make_shared;
 using std::println;
@@ -47,10 +54,9 @@ using std::vector;
 namespace asio = boost::asio;
 namespace bs = boost::system;
 namespace ip = asio::ip;
+namespace fs = std::filesystem;
 
 using tcp = ip::tcp;
-
-typedef mc16_log_level lvl;
 
 server::server(const string &address, const string &port,
                const uint32_t n_threads, const string &methylome_dir,
@@ -70,22 +76,21 @@ server::server(const string &address, const string &port,
   boost::system::error_code resolver_ec;
   const auto resolved = resolver.resolve(address, port, resolver_ec);
   if (resolver_ec) {
-    lgr.log<lvl::error>("{} {}:{}", resolver_ec, address, port);
+    lgr.error("{} {}:{}", resolver_ec, address, port);
     std::raise(SIGTERM);
     return;  // don't wait for signal handler
   }
 
   assert(!resolved.empty());
   const tcp::endpoint endpoint = *resolved.begin();
-  lgr.log<lvl::info>("Resolved endpoint {}",
-                     boost::lexical_cast<string>(endpoint));
+  lgr.info("Resolved endpoint {}", boost::lexical_cast<string>(endpoint));
 
   // open acceptor...
   boost::system::error_code acceptor_ec;
   acceptor.open(endpoint.protocol(), acceptor_ec);
   if (acceptor_ec) {
-    lgr.log<lvl::error>("Error opening endpoint {}: {}",
-                        boost::lexical_cast<string>(endpoint), acceptor_ec);
+    lgr.error("Error opening endpoint {}: {}",
+              boost::lexical_cast<string>(endpoint), acceptor_ec);
     std::raise(SIGTERM);
     return;  // don't wait for signal handler
   }
@@ -93,23 +98,217 @@ server::server(const string &address, const string &port,
   // ...with option to reuse the address (SO_REUSEADDR)
   acceptor.set_option(tcp::acceptor::reuse_address(true), acceptor_ec);
   if (acceptor_ec) {
-    lgr.log<lvl::error>("Error setting SO_REUSEADDR: {}", acceptor_ec);
+    lgr.error("Error setting SO_REUSEADDR: {}", acceptor_ec);
     std::raise(SIGTERM);
     return;  // don't wait for signal handler
   }
 
   acceptor.bind(endpoint, acceptor_ec);
   if (acceptor_ec) {
-    lgr.log<lvl::error>("Error binding endpoint {}: {}",
-                        boost::lexical_cast<string>(endpoint), acceptor_ec);
+    lgr.error("Error binding endpoint {}: {}",
+              boost::lexical_cast<string>(endpoint), acceptor_ec);
     std::raise(SIGTERM);
     return;  // don't wait for signal handler
   }
 
   acceptor.listen(asio::socket_base::max_listen_connections, acceptor_ec);
   if (acceptor_ec) {
-    lgr.log<lvl::error>("Error listening  on endpoint {}: {}",
-                        boost::lexical_cast<string>(endpoint), acceptor_ec);
+    lgr.error("Error listening  on endpoint {}: {}",
+              boost::lexical_cast<string>(endpoint), acceptor_ec);
+    std::raise(SIGTERM);
+    return;  // don't wait for signal handler
+  }
+  do_accept();
+}
+
+server::server(const string &address, const string &port,
+               const uint32_t n_threads, const string &methylome_dir,
+               const uint32_t max_live_methylomes, logger &lgr,
+               std::error_code &ec) :
+  n_threads{n_threads},
+#if defined(SIGQUIT)
+  signals(ioc, SIGINT, SIGTERM, SIGQUIT),
+#else
+  signals(ioc, SIGINT, SIGTERM),
+#endif
+  acceptor(ioc), handler(methylome_dir, max_live_methylomes), lgr{lgr} {
+  // io_context ios uses default constructor
+
+  // ADS: already exists as "ioc"
+  // boost::asio::io_context io_context;
+
+  // Initialise server before becoming a daemon. If process is started
+  // from a shell, this means errors will be reported back to user.
+
+  // ADS: already done; we are here!
+
+  // udp_daytime_server server(io_context);
+
+  do_daemon_await_stop();  // start waiting for signals
+
+  // ^^ ADS: above
+
+  // Register signal handlers so that the daemon may be shut down. You may
+  // also want to register for other signals, such as SIGHUP to trigger a
+  // re-read of a configuration file.
+
+  // boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+  // signals.async_wait([&](boost::system::error_code /*ec*/, int /*signo*/) {
+  //   io_context.stop();
+  // });
+
+  // Inform the io_context that we are about to become a daemon. The
+  // io_context cleans up any internal resources, such as threads, that may
+  // interfere with forking.
+
+  // ADS: we are about to fork; clean up threads (what else?)
+  ioc.notify_fork(boost::asio::io_context::fork_prepare);
+
+  // Fork the process and have the parent exit. If the process was started
+  // from a shell, this returns control to the user. Forking a new process is
+  // also a prerequisite for the subsequent call to setsid().
+  if (const pid_t pid = fork()) {
+    if (pid > 0) {
+      // We're in the parent process and need to exit.
+      //
+      // When the exit() function is used, the program terminates without
+      // invoking local variables' destructors. Only global variables are
+      // destroyed. As the io_context object is a local variable, this means
+      // we do not have to call:
+      //
+      //   io_context.notify_fork(boost::asio::io_context::fork_parent);
+      //
+      // However, this line should be added before each call to exit() if
+      // using a global io_context object. An additional call:
+      //
+      //   io_context.notify_fork(boost::asio::io_context::fork_prepare);
+      //
+      // should also precede the second fork().
+      std::exit(EXIT_SUCCESS);
+    }
+    else {
+      ec = std::make_error_code(std::errc(errno));
+      syslog(LOG_ERR | LOG_USER, "%s",
+             format("First fork failed: {}", ec).data());
+      return;
+    }
+  }
+
+  // Make the process a new session leader. This detaches it from the
+  // terminal.
+  setsid();
+
+  // Process inherits working dir from parent. Could be on a mounted
+  // filesystem, which means that the daemon would prevent filesystem
+  // from being unmounted. Changing to root dir avoids this problem.
+
+  // chdir("/");
+  chdir(fs::current_path().root_path().string().data());
+
+  // File mode creation mask is inherited from parent process. Don't
+  // want to restrict perms on created files, so the mask is cleared.
+  umask(0);
+
+  // A second fork ensures the process cannot acquire a controlling terminal.
+  if (pid_t pid = fork()) {
+    if (pid > 0) {
+      std::exit(EXIT_SUCCESS);
+    }
+    else {
+      ec = std::make_error_code(std::errc(errno));
+      syslog(LOG_ERR | LOG_USER, "%s",
+             format("Second fork failed: {}", ec).data());
+      return;
+    }
+  }
+
+  // Close the standard streams. This decouples the daemon from the terminal
+  // that started it.
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  // We don't want the daemon to have any standard input.
+  static constexpr auto dev_null = "/dev/null";
+  if (open(dev_null, O_RDONLY) < 0) {
+    ec = std::make_error_code(std::errc(errno));
+    const auto msg = format("Unable to open {}: {}", dev_null, ec);
+    syslog(LOG_ERR | LOG_USER, "%s", msg.data());
+    return;
+  }
+
+  // Send standard output to a log file.
+  const char *output = "/tmp/mc16.daemon.out";
+  const int flags = O_WRONLY | O_CREAT | O_APPEND;
+  const mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+  if (open(output, flags, mode) < 0) {
+    ec = std::make_error_code(std::errc(errno));
+    const auto msg = format("Unable to open output file {}: {}", output, ec);
+    syslog(LOG_ERR | LOG_USER, "%s", msg.data());
+    return;
+  }
+
+  // Also send standard error to the same log file.
+  if (dup(1) < 0) {
+    ec = std::make_error_code(std::errc(errno));
+    const auto msg = format("Unable to dup output descriptor: {}", ec);
+    syslog(LOG_ERR | LOG_USER, "%s", msg.data());
+    return;
+  }
+
+  // Inform the io_context that we have finished becoming a daemon. The
+  // io_context uses this opportunity to create any internal file descriptors
+  // that need to be private to the new process.
+  // io_context.notify_fork(boost::asio::io_context::fork_child);
+
+  ioc.notify_fork(boost::asio::io_context::fork_child);
+
+  // io_context 'ioc' can now be used normally
+  syslog(LOG_INFO | LOG_USER, "Daemon started.");
+
+  tcp::resolver resolver(ioc);
+  boost::system::error_code resolver_ec;
+  const auto resolved = resolver.resolve(address, port, resolver_ec);
+  if (resolver_ec) {
+    lgr.error("{} {}:{}", resolver_ec, address, port);
+    std::raise(SIGTERM);
+    return;  // don't wait for signal handler
+  }
+
+  assert(!resolved.empty());
+  const tcp::endpoint endpoint = *resolved.begin();
+  lgr.info("Resolved endpoint {}", boost::lexical_cast<string>(endpoint));
+
+  // open acceptor...
+  boost::system::error_code acceptor_ec;
+  acceptor.open(endpoint.protocol(), acceptor_ec);
+  if (acceptor_ec) {
+    lgr.error("Error opening endpoint {}: {}",
+              boost::lexical_cast<string>(endpoint), acceptor_ec);
+    std::raise(SIGTERM);
+    return;  // don't wait for signal handler
+  }
+
+  // ...with option to reuse the address (SO_REUSEADDR)
+  acceptor.set_option(tcp::acceptor::reuse_address(true), acceptor_ec);
+  if (acceptor_ec) {
+    lgr.error("Error setting SO_REUSEADDR: {}", acceptor_ec);
+    std::raise(SIGTERM);
+    return;  // don't wait for signal handler
+  }
+
+  acceptor.bind(endpoint, acceptor_ec);
+  if (acceptor_ec) {
+    lgr.error("Error binding endpoint {}: {}",
+              boost::lexical_cast<string>(endpoint), acceptor_ec);
+    std::raise(SIGTERM);
+    return;  // don't wait for signal handler
+  }
+
+  acceptor.listen(asio::socket_base::max_listen_connections, acceptor_ec);
+  if (acceptor_ec) {
+    lgr.error("Error listening  on endpoint {}: {}",
+              boost::lexical_cast<string>(endpoint), acceptor_ec);
     std::raise(SIGTERM);
     return;  // don't wait for signal handler
   }
@@ -152,8 +351,20 @@ server::do_await_stop() -> void {
   // capture brings 'this' into search for names
   signals.async_wait(
     [this](const boost::system::error_code ec, const int signo) {
-      lgr.log<lvl::info>("Received signal {} ({})", strsignal(signo),
-                         ec.message());
+      lgr.error("Received signal {} ({})", strsignal(signo), ec);
+      // stop server by cancelling all outstanding async ops; when all
+      // have finished, the call to io_context::run() will finish
+      ioc.stop();
+    });
+}
+
+auto
+server::do_daemon_await_stop() -> void {
+  // capture brings 'this' into search for names
+  signals.async_wait(
+    [this](const boost::system::error_code ec, const int signo) {
+      syslog(LOG_INFO | LOG_USER, "Daemon stopped.");
+      lgr.error("Received signal {} ({})", strsignal(signo), ec);
       // stop server by cancelling all outstanding async ops; when all
       // have finished, the call to io_context::run() will finish
       ioc.stop();
