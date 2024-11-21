659f4c81154086e1afbe8e2e75bb605dcf5e7b06
Andrew D Smith <andrewds@usc.edu>
2024-11-21 09:39:28 -0800

    src/utilities.*pp: moving the write_bins function to the header so it can be templated

--- src/utilities.hpp
+++ src/utilities.hpp
@@ -33,6 +33,7 @@
 #include "methylome.hpp"
 
 #include <algorithm>
+#include <cassert>
 #include <cerrno>
 #include <chrono>
 #include <cmath>
@@ -163,8 +164,55 @@ write_bedgraph(std::ostream &out, const cpg_index &index,
 
 auto
 write_bins(std::ostream &out, const std::uint32_t bin_size,
-           const cpg_index &index,
-           const std::vector<counts_res_cov> &results) -> std::error_code;
+           const cpg_index &index, const auto &results) -> std::error_code {
+  static constexpr auto buf_size{512};
+  static constexpr auto delim{'\t'};
+
+  using counts_res_type =
+    typename std::remove_cvref_t<decltype(results)>::value_type;
+
+  std::array<char, buf_size> buf{};
+  const auto buf_beg = buf.data();
+  const auto buf_end = buf.data() + buf_size;
+
+  auto res = std::cbegin(results);
+
+  const auto zipped = std::views::zip(index.chrom_size, index.chrom_order);
+  for (const auto [chrom_size, chrom_name] : zipped) {
+    std::ranges::copy(chrom_name, buf_beg);
+    buf[size(chrom_name)] = delim;
+    for (std::uint32_t bin_beg = 0; bin_beg < chrom_size; bin_beg += bin_size) {
+      const auto bin_end = std::min(bin_beg + bin_size, chrom_size);
+      std::to_chars_result tcr{buf_beg + std::size(chrom_name) + 1,
+                               std::errc()};
+#if defined(__GNUG__) and not defined(__clang__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic error "-Wstringop-overflow=0"
+#endif
+      tcr = std::to_chars(tcr.ptr, buf_end, bin_beg);
+      *tcr.ptr++ = delim;
+      tcr = std::to_chars(tcr.ptr, buf_end, bin_end);
+      *tcr.ptr++ = delim;
+      tcr = std::to_chars(tcr.ptr, buf_end, res->n_meth);
+      *tcr.ptr++ = delim;
+      tcr = std::to_chars(tcr.ptr, buf_end, res->n_unmeth);
+      if constexpr (std::is_same<counts_res_type, counts_res_cov>::value) {
+        *tcr.ptr++ = delim;
+        tcr = std::to_chars(tcr.ptr, buf_end, res->n_covered);
+      }
+      *tcr.ptr++ = '\n';
+#if defined(__GNUG__) and not defined(__clang__)
+#pragma GCC diagnostic pop
+#endif
+      out.write(buf_beg, std::ranges::distance(buf_beg, tcr.ptr));
+      if (!out)
+        return std::make_error_code(std::errc(errno));
+      ++res;
+    }
+  }
+  assert(res == std::cend(results));
+  return {};
+}
 
 [[nodiscard]] inline auto
 duration(const auto start, const auto stop) {
