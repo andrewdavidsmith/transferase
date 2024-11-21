43f11964a3dfe3f34aa280094ab0afcc359a3bd3
Andrew D Smith <andrewds@usc.edu>
2024-11-21 09:43:25 -0800

    src/bins.cpp: adding functionality to get bins with coverage or without and adding cli for remote queries

--- src/bins.cpp
+++ src/bins.cpp
@@ -24,122 +24,223 @@
 #include "bins.hpp"
 #include "cpg_index.hpp"
 #include "genomic_interval.hpp"
+#include "logger.hpp"
 #include "methylome.hpp"
 #include "mxe_error.hpp"
 #include "utilities.hpp"
 
 #include <boost/program_options.hpp>
 
-#include <algorithm>
-#include <array>
 #include <chrono>
 #include <cstdint>
 #include <format>
 #include <fstream>
 #include <iostream>
+#include <memory>
 #include <print>
-#include <ranges>
 #include <string>
 #include <tuple>
 #include <vector>
 
-using std::array;
-using std::cbegin;
-using std::cend;
-using std::cerr;
-using std::cout;
-using std::min;
 using std::ofstream;
-using std::print;
-using std::println;
 using std::string;
 using std::tuple;
-using std::uint32_t;
 using std::vector;
 
-namespace rg = std::ranges;
-namespace vs = std::views;
-using hr_clock = std::chrono::high_resolution_clock;
+template <typename counts_res_type>
+[[nodiscard]] static inline auto
+do_local_bins(const string &meth_file, const cpg_index &index,
+              const std::uint32_t bin_size)
+  -> tuple<vector<counts_res_type>, std::error_code> {
+  methylome meth{};
+  if (const auto meth_read_err = meth.read(meth_file, index.n_cpgs_total)) {
+    logger::instance().error("Error: {} ({})", meth_read_err, meth_file);
+    return {{}, meth_read_err};
+  }
+  if constexpr (std::is_same<counts_res_type, counts_res_cov>::value)
+    return {std::move(meth.get_bins_cov(bin_size, index)), {}};
+  else
+    return {std::move(meth.get_bins(bin_size, index)), {}};
+}
 
-namespace po = boost::program_options;
-using po::value;
+template <typename counts_res_type>
+static auto
+do_bins(const string &accession, const cpg_index &index,
+        const std::uint32_t bin_size, const string &hostname,
+        const string &port, const string &meth_file, std::ostream &out,
+        const bool write_scores, [[maybe_unused]] const bool remote_mode)
+  -> std::error_code {
+
+  using hr_clock = std::chrono::high_resolution_clock;
+
+  const auto bins_start{hr_clock::now()};
+  const auto [results, bins_err] =
+    do_local_bins<counts_res_type>(meth_file, index, bin_size);
+  const auto bins_stop{hr_clock::now()};
+  logger::instance().debug("Elapsed time for bins query: {:.3}s",
+                           duration(bins_start, bins_stop));
+
+  if (bins_err)  // ADS: error messages already logged
+    return std::make_error_code(std::errc::invalid_argument);
+
+  const auto output_start{hr_clock::now()};
+  write_bins(out, bin_size, index, results);
+  const auto output_stop{hr_clock::now()};
+  // ADS: elapsed time for output will include conversion to scores
+  logger::instance().debug("Elapsed time for output: {:.3}s",
+                           duration(output_start, output_stop));
+  return {};
+}
 
 auto
 bins_main(int argc, char *argv[]) -> int {
-  static const auto description = std::format("{}", "bins");
+  static constexpr auto usage_format =
+    "Usage: mxe lookup {} [options]\n\nOption groups";
+  static constexpr mxe_log_level default_log_level = mxe_log_level::info;
+  static constexpr auto default_port = "5000";
+  static const auto command = "bins";
 
-  bool verbose{};
+  bool count_covered{};
+  bool write_scores{};
 
   string index_file{};
   string meth_file{};
-  string output_file{};
+  string outfile{};
+  std::uint32_t bin_size{};
+  mxe_log_level log_level{};
+  string hostname{};
+  string port{};
+  string accession{};
 
-  uint32_t bin_size{};
+  string subcmd;
 
-  po::options_description desc(description);
+  namespace po = boost::program_options;
+
+  po::options_description subcmds;
+  subcmds.add_options()
+    // clang-format off
+    ("subcmd", po::value(&subcmd))
+    ("subargs", po::value<vector<string>>())
+    // clang-format on
+    ;
+  // positional; one for "subcmd" and the rest else parser throws
+  po::positional_options_description p;
+  p.add("subcmd", 1).add("subargs", -1);
+
+  po::variables_map vm_subcmd;
+  po::store(po::command_line_parser(argc, argv)
+              .options(subcmds)
+              .positional(p)
+              .allow_unregistered()
+              .run(),
+            vm_subcmd);
+  po::notify(vm_subcmd);
+
+  if (subcmd != "local" && subcmd != "remote") {
+    std::println("Usage: mxe lookup [local|remote] [options]");
+    return EXIT_FAILURE;
+  }
+  const bool remote_mode = subcmd == "remote";
+
+  po::options_description general("General");
   // clang-format off
-  desc.add_options()
+  general.add_options()
     ("help,h", "produce help message")
-    ("index,x", value(&index_file)->required(), "index file")
-    ("bin,b", value(&bin_size)->required(), "size of bins")
-    ("meth,m", value(&meth_file)->required(), "methylation file")
-    ("output,o", value(&output_file)->required(), "output file")
-    ("verbose,v", po::bool_switch(&verbose), "print more run info")
+    ("index,x", po::value(&index_file)->required(), "index file")
+    ("bin-size,b", po::value(&bin_size)->required(), "size of bins")
+    ("log-level,v", po::value(&log_level)->default_value(default_log_level),
+     "log level {debug,info,warning,error,critical}")
+    ;
+  po::options_description output("Output");
+  output.add_options()
+    ("output,o", po::value(&outfile)->required(), "output file")
+    ("covered", po::bool_switch(&count_covered), "count covered sites per bin")
+    ("score", po::bool_switch(&write_scores), "weighted methylation bedgraph format")
+    ;
+  po::options_description remote("Remote");
+  remote.add_options()
+    ("hostname,s", po::value(&hostname)->required(), "server hostname")
+    ("port,p", po::value(&port)->default_value(default_port), "port")
+    ("accession,a", po::value(&accession)->required(), "methylome accession")
+    ;
+  po::options_description local("Local");
+  local.add_options()
+    ("methylome,m", po::value(&meth_file)->required(), "local methylome file")
     ;
   // clang-format on
+
+  po::options_description all(std::format(usage_format, subcmd));
+  all.add(general).add(output);
+  all.add(remote_mode ? remote : local);
+
   try {
     po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::store(po::parse_command_line(argc - 1, argv + 1, all), vm);
     if (vm.count("help")) {
-      desc.print(std::cout);
+      all.print(std::cout);
       return EXIT_SUCCESS;
     }
     po::notify(vm);
   }
   catch (po::error &e) {
-    println(cerr, "{}", e.what());
-    desc.print(std::cout);
+    std::println("{}", e.what());
+    all.print(std::cout);
     return EXIT_FAILURE;
   }
 
-  if (verbose)
-    print("index: {}\n"
-          "methylome: {}\n"
-          "output: {}\n"
-          "bin_size: {}\n",
-          index_file, meth_file, output_file, bin_size);
+  logger &lgr = logger::instance(
+    std::make_shared<std::ostream>(std::cout.rdbuf()), command, log_level);
+  if (!lgr) {
+    std::println("Failure initializing logging: {}.", lgr.get_status());
+    return EXIT_FAILURE;
+  }
+
+  // ADS: log the command line arguments (assuming right log level)
+  vector<tuple<string, string>> args_to_log{
+    {"Index", index_file},
+    {"Binsize", std::format("{}", bin_size)},
+    {"Output", outfile},
+    {"Covered", std::format("{}", count_covered)},
+    {"Bedgraph", std::format("{}", write_scores)},
+  };
+  vector<tuple<string, string>> remote_args{
+    {"Hostname:port", std::format("{}:{}", hostname, port)},
+    {"Accession", accession},
+  };
+  vector<tuple<string, string>> local_args{
+    {"Methylome", meth_file},
+  };
+  log_args<mxe_log_level::info>(args_to_log);
+  log_args<mxe_log_level::info>(remote_mode ? remote_args : local_args);
 
   cpg_index index{};
   if (const auto index_read_err = index.read(index_file); index_read_err) {
-    println(cerr, "Error: {} ({})", index_read_err, index_file);
+    lgr.error("Failed to read cpg index: {} ({})", index_file, index_read_err);
     return EXIT_FAILURE;
   }
 
-  if (verbose)
-    println("index:\n{}", index);
+  if (log_level == mxe_log_level::debug)
+    lgr.debug("Number of CpGs in index: {}", index.n_cpgs_total);
 
   methylome meth{};
   const auto meth_read_err = meth.read(meth_file, index.n_cpgs_total);
   if (meth_read_err) {
-    println(cerr, "Failed to read methylome: {}", meth_read_err);
+    lgr.error("Failed to read methylome: {}", meth_read_err);
     return EXIT_FAILURE;
   }
 
-  ofstream out(output_file);
+  ofstream out(outfile);
   if (!out) {
-    println(cerr, "Failed to open output file: {}", output_file);
+    lgr.error("Failed to open output file: {}", outfile);
     return EXIT_FAILURE;
   }
 
-  const auto get_bins_start{hr_clock::now()};
-  const auto results = meth.get_bins_cov(bin_size, index);
-  const auto get_bins_stop{hr_clock::now()};
+  const auto bins_err =
+    count_covered
+      ? do_bins<counts_res_cov>(accession, index, bin_size, hostname, port,
+                                meth_file, out, write_scores, remote_mode)
+      : do_bins<counts_res>(accession, index, bin_size, hostname, port,
+                            meth_file, out, write_scores, remote_mode);
 
-  if (verbose)
-    println("Elapsed time to get bins: {:.3}s",
-            duration(get_bins_start, get_bins_stop));
-
-  write_bins(out, bin_size, index, results);
-
-  return EXIT_SUCCESS;
+  return bins_err == std::errc() ? EXIT_SUCCESS : EXIT_FAILURE;
 }
