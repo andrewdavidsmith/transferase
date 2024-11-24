5b9dd1ee07b265a72f0e0b190ddfb2c2b3295b68
Andrew D Smith <andrewds@usc.edu>
2024-11-23 19:04:58 -0800

    src/compress.cpp: moved the methylome_metadata struct to its own sources, now outputting metadata to file

--- src/compress.cpp
+++ src/compress.cpp
@@ -22,96 +22,37 @@
  */
 
 #include "compress.hpp"
+
 #include "cpg_index.hpp"
 #include "genomic_interval.hpp"
+#include "logger.hpp"
 #include "methylome.hpp"
+#include "methylome_metadata.hpp"
 #include "mxe_error.hpp"
 #include "utilities.hpp"
 
-#include <config.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <zlib.h>
 
-#include <boost/asio.hpp>  // boost::asio::ip::host_name();
 #include <boost/program_options.hpp>
 
 #include <algorithm>
 #include <array>
 #include <cassert>
 #include <charconv>
-#include <chrono>
-#include <filesystem>
 #include <iostream>
 #include <limits>
 #include <numeric>
 #include <print>
 #include <string>
 #include <tuple>
-#include <unordered_map>
 #include <utility>
 #include <vector>
 
-using std::from_chars;
-using std::max;
-using std::pair;
 using std::println;
 using std::size;
 using std::string;
-using std::unordered_map;
-using std::vector;
-
-namespace po = boost::program_options;
-using po::value;
-
-template <typename T> using num_lim = std::numeric_limits<T>;
-
-// ADS: this should be moved into methylome.hpp
-struct methylome_metadata {
-  std::string version{};
-  std::string host{};
-  std::int64_t user{};
-  std::chrono::time_point<std::chrono::utc_clock> creation_time{};
-  std::uint64_t methylome_adler{};
-  std::uint64_t index_adler{};
-
-  static auto init(const std::string &index_filename,
-                   const std::string &methylome_filename)
-    -> std::tuple<methylome_metadata, std::error_code> {
-    std::error_code err;
-    const auto index_adler = get_adler(index_filename, err);
-    if (err)
-      return {{}, err};
-
-    const auto methylome_adler = get_adler(methylome_filename, err);
-    if (err)
-      return {{}, err};
-
-    boost::system::error_code boost_err;
-    const auto host = boost::asio::ip::host_name(boost_err);
-    if (boost_err)
-      return {{}, err};
-
-    const auto creation_time = std::chrono::utc_clock::now();
-
-    return {
-      {VERSION, host, getuid(), creation_time, methylome_adler, index_adler},
-      {}};
-  }
-
-  auto tostring() const -> std::string {
-    std::ostringstream oss;
-    println(oss,
-            "version: {}\n"
-            "host: {}\n"
-            "user: {}\n"
-            "creation_time: \"{}\"\n"
-            "methylome_adler: {}\n"
-            "index_adler: {}",
-            version, host, user, creation_time, methylome_adler, index_adler);
-    return oss.str();
-  }
-};
 
 enum class compress_err {
   // clang-format off
@@ -132,24 +73,18 @@ struct compress_err_cat : std::error_category {
   }
   auto message(const int condition) const -> std::string override {
     using std::string_literals::operator""s;
+    // clang-format off
     switch (condition) {
-    case 0:
-      return "ok"s;
-    case 1:
-      return "failed to open methylome file"s;
-    case 2:
-      return "failed to parse xcounts header"s;
-    case 3:
-      return "failed to find chromosome in xcounts header"s;
-    case 4:
-      return "inconsistent chromosome order"s;
-    case 5:
-      return "incorrect chromosome size"s;
-    case 6:
-      return "failed to generate methylome file"s;
-    case 7:
-      return "failed to write methylome file"s;
+    case 0: return "ok"s;
+    case 1: return "failed to open methylome file"s;
+    case 2: return "failed to parse xcounts header"s;
+    case 3: return "failed to find chromosome in xcounts header"s;
+    case 4: return "inconsistent chromosome order"s;
+    case 5: return "incorrect chromosome size"s;
+    case 6: return "failed to generate methylome file"s;
+    case 7: return "failed to write methylome file"s;
     }
+    // clang-format on
     std::abort();  // unreacheable
   }
 };
@@ -164,7 +99,7 @@ make_error_code(compress_err e) {
 }
 
 struct meth_file {
-  [[nodiscard]] auto open(const string &filename) -> std::error_code {
+  [[nodiscard]] auto open(const std::string &filename) -> std::error_code {
     in = gzopen(filename.data(), "rb");
     if (in == nullptr)
       return compress_err::xcounts_file_open_failure;
@@ -177,7 +112,7 @@ struct meth_file {
     return len;
   }
 
-  [[nodiscard]] auto getline(string &line) -> bool {
+  [[nodiscard]] auto getline(std::string &line) -> bool {
     line.clear();
     if (pos == len && !read())
       return false;
@@ -190,17 +125,17 @@ struct meth_file {
     return true;
   }
 
-  static constexpr int32_t buf_size = 4 * 128 * 1024;
+  static constexpr std::int32_t buf_size = 4 * 128 * 1024;
   gzFile in{};
-  int32_t pos{};
-  int32_t len{};
-  std::array<uint8_t, buf_size> buf;
+  std::int32_t pos{};
+  std::int32_t len{};
+  std::array<std::uint8_t, buf_size> buf;
 };
 
 static inline auto
-skip_absent_cpgs(const uint64_t end_pos, const cpg_index::vec &idx,
-                 uint32_t &cpg_idx_in) -> uint32_t {
-  const uint32_t first_idx = cpg_idx_in;
+skip_absent_cpgs(const std::uint64_t end_pos, const cpg_index::vec &idx,
+                 std::uint32_t &cpg_idx_in) -> std::uint32_t {
+  const std::uint32_t first_idx = cpg_idx_in;
   while (cpg_idx_in < size(idx) && idx[cpg_idx_in] < end_pos)
     ++cpg_idx_in;
   return cpg_idx_in - first_idx;
@@ -208,8 +143,8 @@ skip_absent_cpgs(const uint64_t end_pos, const cpg_index::vec &idx,
 
 // add all cpg sites for chroms in the given range
 static inline auto
-add_all_cpgs(const uint32_t prev_ch_id, const uint32_t ch_id,
-             const cpg_index &idx) -> uint32_t {
+add_all_cpgs(const std::uint32_t prev_ch_id, const std::uint32_t ch_id,
+             const cpg_index &idx) -> std::uint32_t {
   auto n_cpgs_total = 0;
   for (auto i = prev_ch_id + 1; i < ch_id; ++i)
     n_cpgs_total += size(idx.positions[i]);
@@ -217,7 +152,7 @@ add_all_cpgs(const uint32_t prev_ch_id, const uint32_t ch_id,
 }
 
 static inline auto
-get_ch_id(const cpg_index &ci, const string &chrom_name) -> int32_t {
+get_ch_id(const cpg_index &ci, const std::string &chrom_name) -> std::int32_t {
   const auto ch_id = ci.chrom_index.find(chrom_name);
   if (ch_id == cend(ci.chrom_index))
     return -1;
@@ -227,15 +162,15 @@ get_ch_id(const cpg_index &ci, const string &chrom_name) -> int32_t {
 // ADS: this function is tied to the specifics of dnmtools::xcounts
 // code, and likely needs a review
 static auto
-verify_header_line(const cpg_index &idx, int32_t &n_chroms_seen,
-                   const string &line) -> std::error_code {
+verify_header_line(const cpg_index &idx, std::int32_t &n_chroms_seen,
+                   const std::string &line) -> std::error_code {
   // ignore the version line and the header end line
   if (line.substr(0, 9) == "#DNMTOOLS" || size(line) == 1)
     return compress_err::ok;
 
   // parse the chrom and its size
-  string chrom;
-  uint64_t chrom_size{};
+  std::string chrom;
+  std::uint64_t chrom_size{};
   std::istringstream iss{line};
   if (!(iss >> chrom >> chrom_size))
     return compress_err::xcounts_file_header_failure;
@@ -264,46 +199,45 @@ verify_header_line(const cpg_index &idx, int32_t &n_chroms_seen,
 }
 
 static auto
-process_cpg_sites(const string &infile, const string &outfile,
+process_cpg_sites(const std::string &infile, const std::string &outfile,
                   const cpg_index &index, const bool zip) -> std::error_code {
   meth_file mf{};
   std::error_code err = mf.open(infile);
   if (err != compress_err::ok) {
-    println("Error reading xcounts file: {}", infile);
+    std::println("Error reading xcounts file: {}", infile);
     return err;
   }
 
   methylome::vec cpgs(index.n_cpgs_total, {0, 0});
 
-  uint32_t cpg_idx_out{};  // index of current output cpg position
-  uint32_t cpg_idx_in{};   // index of current input cpg position
-  int32_t prev_ch_id = -1;
-  uint64_t pos = num_lim<uint64_t>::max();
+  std::uint32_t cpg_idx_out{};  // index of current output cpg position
+  std::uint32_t cpg_idx_in{};   // index of current input cpg position
+  std::int32_t prev_ch_id = -1;
+  std::uint64_t pos = std::numeric_limits<std::uint64_t>::max();
 
-  vector<cpg_index::vec>::const_iterator positions{};
+  std::vector<cpg_index::vec>::const_iterator positions{};
 
-  unordered_map<string, int32_t> header_chroms_index;
-
-  string line;
-  int32_t n_chroms_seen{};
+  std::string line;
+  std::int32_t n_chroms_seen{};
   while (mf.getline(line)) {
     if (line[0] == '#') {
       // consistency check between reference used for the index and
       // reference used for the methylome
       if (err = verify_header_line(index, n_chroms_seen, line); err) {
-        println("Error parsing xcounts header line: {} ({})", line, err);
+        std::println("Error parsing xcounts header line: {} ({})", line, err);
         return err;
       }
       continue;  // ADS: early loop exit
     }
-    if (!std::isdigit(line[0])) {           // check for new chromosome
-      if (pos != num_lim<uint64_t>::max())  // check not first iteration
+    if (!std::isdigit(line[0])) {  // check for new chromosome
+      // check not first iteration
+      if (pos != std::numeric_limits<std::uint64_t>::max())
         // add cpgs before those in the input
         cpg_idx_out += (size(*positions) - cpg_idx_in);
 
-      const int32_t ch_id = get_ch_id(index, line);
+      const std::int32_t ch_id = get_ch_id(index, line);
       if (ch_id < 0) {
-        println("Failed to find chromosome in index: {}", line);
+        std::println("Failed to find chromosome in index: {}", line);
         return compress_err::xcounts_file_chromosome_not_found;
       }
 
@@ -315,11 +249,11 @@ process_cpg_sites(const string &infile, const string &outfile,
       prev_ch_id = ch_id;
     }
     else {
-      uint32_t pos_step = 0, n_meth = 0, n_unmeth = 0;
+      std::uint32_t pos_step = 0, n_meth = 0, n_unmeth = 0;
       const auto end_line = line.data() + size(line);
-      auto res = from_chars(line.data(), end_line, pos_step);
-      res = from_chars(res.ptr + 1, end_line, n_meth);
-      res = from_chars(res.ptr + 1, end_line, n_unmeth);
+      auto res = std::from_chars(line.data(), end_line, pos_step);
+      res = std::from_chars(res.ptr + 1, end_line, n_meth);
+      res = std::from_chars(res.ptr + 1, end_line, n_unmeth);
       // ADS: check for errors here
 
       const auto curr_pos = pos + pos_step;
@@ -339,7 +273,7 @@ process_cpg_sites(const string &infile, const string &outfile,
   cpg_idx_out += add_all_cpgs(prev_ch_id, size(index.positions), index);
 
   if (cpg_idx_out != index.n_cpgs_total) {
-    println("Failed to generate mxe methylome");
+    std::println("Failed to generate mxe methylome");
     return compress_err::methylome_compression_failure;
   }
 
@@ -347,7 +281,7 @@ process_cpg_sites(const string &infile, const string &outfile,
   m.cpgs = std::move(cpgs);
   const auto meth_write_err = m.write(outfile, zip);
   if (meth_write_err) {
-    println("Error: {} ({})", meth_write_err, outfile);
+    std::println("Error: {} ({})", meth_write_err, outfile);
     return compress_err::methylome_file_write_failure;
   }
 
@@ -358,74 +292,94 @@ process_cpg_sites(const string &infile, const string &outfile,
 
 auto
 compress_main(int argc, char *argv[]) -> int {
-  static const auto description = "compress";
+  static constexpr mxe_log_level default_log_level = mxe_log_level::info;
+  static constexpr auto command = "compress";
 
-  bool verbose{};
-  bool zip{};
+  std::string methylation_input{};
+  std::string methylome_output{};
+  std::string metadata_output{};
+  std::string index_file{};
+  mxe_log_level log_level{};
+  bool zip{false};
 
-  string methylation_input{};
-  string methylation_output{};
-  string index_file{};
+  namespace po = boost::program_options;
 
-  po::options_description desc(description);
+  po::options_description desc(std::format("Usage: mxe {} [options]", command));
   desc.add_options()
     // clang-format off
     ("help,h", "produce help message")
-    ("meth,m", value(&methylation_input)->required(),
+    ("meth,m", po::value(&methylation_input)->required(),
      "methylation input file")
-    ("index,x", value(&index_file)->required(), "index file")
-    ("output,o", value(&methylation_output)->required(),
-     "methylation output file")
+    ("index,x", po::value(&index_file)->required(), "index file")
+    ("output,o", po::value(&methylome_output)->required(),
+     "methylome output file")
+    ("meta-out", po::value(&metadata_output),
+     "metadata output (defaults to output.json)")
     ("zip,z", po::bool_switch(&zip), "zip the output")
-    ("verbose,v", po::bool_switch(&verbose), "print more run info")
+    ("log-level,v", po::value(&log_level)->default_value(default_log_level),
+     "log level {debug,info,warning,error,critical}")
     // clang-format on
     ;
   try {
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
-    if (vm.count("help")) {
+    if (vm.count("help") || argc == 1) {
       desc.print(std::cout);
       return EXIT_SUCCESS;
     }
     po::notify(vm);
   }
   catch (po::error &e) {
-    println("{}", e.what());
+    std::println("{}", e.what());
     desc.print(std::cout);
     return EXIT_FAILURE;
   }
 
-  if (verbose)
-    print("methylome file: {}\n"
-          "index file: {}\n"
-          "output file: {}\n"
-          "zip: {}\n",
-          methylation_input, methylation_output, index_file, zip);
+  auto &lgr = logger::instance(shared_from_cout(), command, log_level);
+  if (!lgr) {
+    std::println("Failure initializing logging: {}.", lgr.get_status());
+    return EXIT_FAILURE;
+  }
+
+  if (metadata_output.empty())
+    metadata_output = std::format("{}.json", methylome_output);
+
+  std::vector<std::tuple<std::string, std::string>> args_to_log{
+    // clang-format off
+    {"Methylation", methylation_input},
+    {"Index", index_file},
+    {"Methylome output", methylome_output},
+    {"Metadata output", metadata_output},
+    {"Zip", std::format("{}", zip)},
+    // clang-format on
+  };
+  log_args<mxe_log_level::info>(args_to_log);
 
   cpg_index index{};
   if (const auto index_read_err = index.read(index_file); index_read_err) {
-    println("Error: {} ({})", index_read_err, index_file);
+    lgr.error("Error reading cpg index {}: {}", index_read_err, index_file);
     return EXIT_FAILURE;
   }
-
-  if (verbose)
-    println("{}", index);
 
   std::error_code err =
-    process_cpg_sites(methylation_input, methylation_output, index, zip);
+    process_cpg_sites(methylation_input, methylome_output, index, zip);
   if (err) {
-    println("Error : {}", err);
+    lgr.error("Error generating methylome: {}", err);
     return EXIT_FAILURE;
   }
 
-  const auto [mm, metadata_err] =
-    methylome_metadata::init(index_file, methylation_output);
+  const auto [metadata, metadata_err] =
+    methylome_metadata::init(index_file, methylome_output, zip);
   if (metadata_err) {
-    println("Error : {}", metadata_err);
+    lgr.error("Error initializing metadata: {}", metadata_err);
     return EXIT_FAILURE;
   }
 
-  println("{}", mm.tostring());
+  err = methylome_metadata::write(metadata, metadata_output);
+  if (err) {
+    lgr.error("Error writing metadata: {} ({})", err, metadata_output);
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
