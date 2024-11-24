8f805d7fe97812df1a283a6eec7352c9295e9d9a
Andrew D Smith <andrewds@usc.edu>
2024-11-24 13:04:57 -0800

    src/methylome_results_types.hpp: forgot to add this file in last PR

---
+++ src/methylome_results_types.hpp
@@ -0,0 +1,61 @@
+/* MIT License
+ *
+ * Copyright (c) 2024 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SRC_METHYLOME_RESULTS_TYPES_HPP_
+#define SRC_METHYLOME_RESULTS_TYPES_HPP_
+
+#include <cstdint>
+#include <format>
+#include <string>
+
+struct counts_res {
+  std::uint32_t n_meth{};
+  std::uint32_t n_unmeth{};
+};
+
+struct counts_res_cov {
+  std::uint32_t n_meth{};
+  std::uint32_t n_unmeth{};
+  std::uint32_t n_covered{};
+};
+
+template <> struct std::formatter<counts_res> : std::formatter<std::string> {
+  auto format(const counts_res &cr, std::format_context &ctx) const {
+    return std::formatter<std::string>::format(
+      std::format(R"({{"n_meth": {}, "n_unmeth": {}}})", cr.n_meth,
+                  cr.n_unmeth),
+      ctx);
+  }
+};
+
+template <>
+struct std::formatter<counts_res_cov> : std::formatter<std::string> {
+  auto format(const counts_res_cov &cr, std::format_context &ctx) const {
+    return std::formatter<std::string>::format(
+      std::format(R"({{"n_meth": {}, "n_unmeth": {}, "n_covered": {}}})",
+                  cr.n_meth, cr.n_unmeth, cr.n_covered),
+      ctx);
+  }
+};
+
+#endif  // SRC_METHYLOME_RESULTS_TYPES_HPP_
