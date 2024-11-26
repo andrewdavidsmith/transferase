17e8392b8bbd7d3f2c7565f68330c56ade3f63b0
Andrew D Smith <andrewds@usc.edu>
2024-11-26 12:28:25 -0800

    src/cpg_index_meta.cpp: adding source files for working with the cpg index metadata

---
+++ src/cpg_index_meta.cpp
@@ -0,0 +1,147 @@
+/* MIT License
+ *
+ * Copyright (c) 2024 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "cpg_index_meta.hpp"
+
+#include "cpg_index.hpp"
+#include "utilities.hpp"
+
+#include <config.h>
+
+#include <boost/asio.hpp>  // boost::asio::ip::host_name();
+#include <boost/json.hpp>
+
+#include <chrono>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <system_error>
+#include <tuple>
+
+[[nodiscard]] auto
+cpg_index_meta::write(const cpg_index_meta &cm,
+                      const std::string &json_filename) -> std::error_code {
+  std::ofstream out(json_filename);
+  if (!out)
+    return std::make_error_code(std::errc(errno));
+  if (!(out << boost::json::value_from(cm)))
+    return std::make_error_code(std::errc(errno));
+  return {};
+}
+
+[[nodiscard]] static auto
+get_username() -> std::tuple<std::string, std::error_code> {
+  static constexpr auto username_maxsize = 128;
+  std::array<char, username_maxsize> buf;
+  if (getlogin_r(buf.data(), username_maxsize))
+    return {std::string(), std::make_error_code(std::errc(errno))};
+  return {std::string(buf.data()), {}};
+}
+
+[[nodiscard]] static auto
+get_time_as_string() -> std::string {
+  const auto now{std::chrono::system_clock::now()};
+  const std::chrono::year_month_day ymd{
+    std::chrono::floor<std::chrono::days>(now)};
+  const std::chrono::hh_mm_ss hms{
+    std::chrono::floor<std::chrono::seconds>(now) -
+    std::chrono::floor<std::chrono::days>(now)};
+  return std::format("{:%F} {:%T}", ymd, hms);
+}
+
+[[nodiscard]] auto
+cpg_index_meta::tostring() const -> std::string {
+  std::ostringstream o;
+  if (!(o << boost::json::value_from(*this)))
+    o.clear();
+  return o.str();
+}
+
+[[nodiscard]] auto
+cpg_index_meta::init(const std::string &index_filename)
+  -> std::tuple<cpg_index_meta, std::error_code> {
+  // ADS: (todo) should there be a better way to get the "compression"
+  // status?
+  cpg_index_meta cm;
+  std::error_code err;
+  cm.index_hash = get_adler(index_filename, err);
+  if (err)
+    return {{}, err};
+
+  boost::system::error_code boost_err;
+  cm.host = boost::asio::ip::host_name(boost_err);
+  if (boost_err)
+    return {{}, err};
+
+  cm.assembly = get_assembly_from_filename(index_filename, err);
+  if (err)
+    return {{}, err};
+
+  std::tie(cm.user, err) = get_username();
+
+  cm.version = VERSION;
+  cm.creation_time = get_time_as_string();
+
+  cpg_index ci;
+  err = ci.read(index_filename);
+
+  cm.n_cpgs = ci.n_cpgs_total;
+  cm.chrom_order = ci.chrom_order;
+  cm.chrom_index = ci.chrom_index;
+  cm.chrom_size = ci.chrom_size;
+  cm.chrom_offset = ci.chrom_offset;
+
+  return {cm, err};
+}
+
+[[nodiscard]] auto
+cpg_index_meta::read(const std::string &json_filename)
+  -> std::tuple<cpg_index_meta, std::error_code> {
+  std::ifstream in(json_filename);
+  if (!in)
+    return {cpg_index_meta{}, std::make_error_code(std::errc(errno))};
+
+  std::error_code ec;
+  const auto filesize = std::filesystem::file_size(json_filename, ec);
+  if (ec)
+    return {cpg_index_meta{}, ec};
+
+  std::string payload(filesize, '\0');
+  if (!in.read(payload.data(), filesize))
+    return {cpg_index_meta{}, std::make_error_code(std::errc(errno))};
+
+  cpg_index_meta cim;
+  boost::json::parse_into(cim, payload, ec);
+  if (ec)
+    return {cpg_index_meta{}, cpg_index_meta_error::failure_parsing_json};
+
+  return {cim, cpg_index_meta_error::ok};
+}
+
+auto
+get_default_cpg_index_meta_filename(const std::string &indexfile)
+  -> std::string {
+  return std::format("{}.json", indexfile);
+}
---
+++ src/cpg_index_meta.hpp
@@ -0,0 +1,146 @@
+/* MIT License
+ *
+ * Copyright (c) 2024 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SRC_CPG_INDEX_META_HPP_
+#define SRC_CPG_INDEX_META_HPP_
+
+#include <boost/describe.hpp>  // for BOOST_DESCRIBE_STRUCT
+
+#include <cstdint>
+#include <format>
+#include <string>
+#include <system_error>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+enum class cpg_index_meta_error : std::uint32_t {
+  ok = 0,
+  version_not_found = 1,
+  host_not_found = 2,
+  user_not_found = 3,
+  creation_time_not_found = 4,
+  chrom_names_not_found = 5,
+  index_hash_not_found = 6,
+  assembly_not_found = 7,
+  n_cpgs_not_found = 8,
+  failure_parsing_json = 9,
+  inconsistent = 10,
+};
+
+// register cpg_index_meta_error as error code enum
+template <>
+struct std::is_error_code_enum<cpg_index_meta_error> : public std::true_type {};
+
+// category to provide text descriptions
+struct cpg_index_meta_error_category : std::error_category {
+  const char *name() const noexcept override { return "cpg_index_meta_error"; }
+  std::string message(int code) const override {
+    using std::string_literals::operator""s;
+    // clang-format off
+    switch (code) {
+    case 0: return "ok"s;
+    case 1: return "verion not found"s;
+    case 2: return "host not found"s;
+    case 3: return "user not found"s;
+    case 4: return "creation_time not found"s;
+    case 5: return "chrom names not found"s;
+    case 6: return "index_hash not found"s;
+    case 7: return "assembly not found"s;
+    case 8: return "n_cpgs not found"s;
+    case 9: return "failure parsing methylome metadata json"s;
+    case 10: return "inconsistent metadata"s;
+    }
+    // clang-format on
+    std::unreachable();  // hopefully this is unreacheable
+  }
+};
+
+inline std::error_code
+make_error_code(cpg_index_meta_error e) {
+  static auto category = cpg_index_meta_error_category{};
+  return std::error_code(std::to_underlying(e), category);
+}
+
+struct cpg_index_meta {
+  static constexpr auto filename_extension{"cpg_idx.json"};
+
+  std::string version;
+  std::string host;
+  std::string user;
+  std::string creation_time;
+  std::uint64_t index_hash{};
+  std::string assembly;
+  std::uint32_t n_cpgs{};
+  std::unordered_map<std::string, std::int32_t> chrom_index;
+  std::vector<std::string> chrom_order;
+  std::vector<std::uint32_t> chrom_size;
+  std::vector<std::uint32_t> chrom_offset;
+
+  [[nodiscard]] static auto write(const cpg_index_meta &cm,
+                                  const std::string &json_filename)
+    -> std::error_code;
+
+  [[nodiscard]] static auto init(const std::string &index_filename)
+    -> std::tuple<cpg_index_meta, std::error_code>;
+
+  [[nodiscard]] static auto read(const std::string &json_filename)
+    -> std::tuple<cpg_index_meta, std::error_code>;
+
+  // [[nodiscard]] static auto write(const cpg_index_meta &cm,
+  //                                 const std::string &json_filename)
+  //   -> std::error_code;
+
+  [[nodiscard]] auto tostring() const -> std::string;
+};
+
+template <>
+struct std::formatter<cpg_index_meta> : std::formatter<std::string> {
+  auto format(const cpg_index_meta &cm, std::format_context &ctx) const {
+    return std::formatter<std::string>::format(cm.tostring(), ctx);
+  }
+};
+
+// clang-format off
+BOOST_DESCRIBE_STRUCT(cpg_index_meta, (),
+(
+ version,
+ host,
+ user,
+ creation_time,
+ index_hash,
+ assembly,
+ n_cpgs,
+ chrom_index,
+ chrom_order,
+ chrom_size,
+ chrom_offset
+))
+// clang-format on
+
+auto
+get_default_cpg_index_meta_filename(const std::string &indexfile)
+  -> std::string;
+
+#endif  // SRC_CPG_INDEX_META_HPP_
