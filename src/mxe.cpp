13a129ea5c157b33ec5b8b69da9fe499d56bbb68
Andrew D Smith <andrewds@usc.edu>
2024-11-27 16:43:48 -0800

    src/mxe.cpp: Changing the lookup command to intervals in bettter contrast to bins

--- src/mxe.cpp
+++ src/mxe.cpp
@@ -26,12 +26,12 @@
 
 #include "command_bins.hpp"
 #include "command_check.hpp"
+#include "command_compress.hpp"
 #include "command_format.hpp"
 #include "command_index.hpp"
-#include "command_lookup.hpp"
+#include "command_intervals.hpp"
 #include "command_merge.hpp"
 #include "command_server.hpp"
-#include "command_zip.hpp"
 
 #include <config.h>
 
@@ -62,33 +62,35 @@ namespace po = boost::program_options;
 using po::value;
 
 typedef std::function<int(int, char **)> main_fun;
-const std::tuple<string_view, main_fun, string_view> commands[] = {
+const std::tuple<std::string_view, main_fun, std::string_view> commands[] = {
   // clang-format off
   {"index", command_index_main, "make an index for a reference genome"},
   {"format", command_format_main, "format a methylome file"},
   {"check", command_check_main, "perform checks on methylome and index files"},
-  {"lookup", command_lookup_main, "get methylation levels in each interval"},
+  {"intervals", command_intervals_main, "get methylation levels in each interval"},
   {"merge", command_merge_main, "merge a set of mxe format methylomes"},
-  {"zip", command_zip_main, "make an mxe format methylome smaller"},
+  {"compress", command_compress_main, "make an mxe format methylome smaller"},
   {"bins", command_bins_main, "get methylation levels in each bin"},
   {"server", command_server_main, "run a server to respond to lookup queries"},
   // clang-format on
 };
 
 static auto
-format_help(const string &description, rg::input_range auto &&cmds) {
+format_help(const std::string &description,
+            std::ranges::input_range auto &&cmds) {
   static constexpr auto sep_width = 4;
-  const auto cmds_comma = cmds | vs::elements<0> | vs::join_with(',');
-  const auto cmds_line = string(cbegin(cmds_comma), cend(cmds_comma));
+  const auto cmds_comma =
+    cmds | std::views::elements<0> | std::views::join_with(',');
+  const std::string cmds_line(std::cbegin(cmds_comma), std::cend(cmds_comma));
   std::println("usage: {} {{{}}}\n\n"
                "version: {}\n",
                description, cmds_line, VERSION);
   std::println("commands:\n"
                "  {{{}}}",
                cmds_line);
-  const auto sz =
-    rg::max(vs::transform(cmds | vs::elements<0>, &string_view::size));
-  rg::for_each(cmds, [sz, w = sep_width](const auto &cmd) {
+  const auto sz = std::ranges::max(std::views::transform(
+    cmds | std::views::elements<0>, &std::string_view::size));
+  std::ranges::for_each(cmds, [sz, w = sep_width](const auto &cmd) {
     std::println("    {:{}}{}", get<0>(cmd), sz + w, get<2>(cmd));
   });
 }
@@ -97,12 +99,12 @@ int
 main(int argc, char *argv[]) {
   static constexpr auto program = "mxe";
 
-  string command;
+  std::string command;
   po::options_description arguments;
   arguments.add_options()
     // clang-format off
     ("command", value(&command))
-    ("subargs", value<vector<string>>())
+    ("subargs", value<vector<std::string>>())
     // clang-format on
     ;
   // positional; one for "command" and the rest in "subargs"
@@ -122,7 +124,7 @@ main(int argc, char *argv[]) {
   }
   po::notify(vm);
 
-  const auto cmd_itr = rg::find_if(
+  const auto cmd_itr = std::ranges::find_if(
     commands, [&command](const auto c) { return get<0>(c) == command; });
 
   if (cmd_itr == cend(commands)) {
