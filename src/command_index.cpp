0fb2dde14e9a2f8c9c2a54ec137a86983a211140
Andrew D Smith <andrewds@usc.edu>
2024-12-01 21:58:45 -0800

    src/command_index.cpp: checking for proper output filename and added better cli messages

--- src/command_index.cpp
+++ src/command_index.cpp
@@ -23,6 +23,30 @@
 
 #include "command_index.hpp"
 
+static constexpr auto about = R"(
+make an index for a given reference genome
+)";
+
+static constexpr auto description = R"(
+The index is used to accelerate searches within methylomes and must be
+created from the same reference genome that was used originally to map
+the reads and generate the single-CpG methylation levels. The order of
+chromosomes within the reference genome is not relevant as long as
+each chromosome is correct. The index is in two files. The index data
+is a binary file with size just over 100MB for the human genome and it
+should have the extension '.cpg_idx'. The index metadata is a small
+JSON format file (on a single line) that can easily be examined with
+any JSON formatter (e.g., jq or json_pp).  These two files should
+reside in the same directory and typically only the index data file is
+specified when it is used.
+)";
+
+static constexpr auto examples = R"(
+Examples:
+
+mxe index -v debug -x hg38.cpg_idx -g hg38.fa
+)";
+
 #include "cpg_index.hpp"
 #include "cpg_index_meta.hpp"
 #include "logger.hpp"
@@ -32,6 +56,7 @@
 #include <boost/program_options.hpp>
 
 #include <chrono>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <print>
@@ -42,22 +67,26 @@
 auto
 command_index_main(int argc, char *argv[]) -> int {
   static constexpr auto command = "index";
+  static const auto usage =
+    std::format("Usage: mxe {} [options]\n", strip(command));
+  static const auto about_msg =
+    std::format("mxe {}: {}", strip(command), strip(about));
+  static const auto description_msg =
+    std::format("{}\n{}", strip(description), strip(examples));
 
   std::string genome_filename{};
   std::string index_file{};
-  std::string metadata_output{};
   mxe_log_level log_level{};
 
   namespace po = boost::program_options;
 
-  po::options_description desc(std::format("Usage: mxe {} [options]", command));
+  po::options_description desc("Options");
   desc.add_options()
     // clang-format off
     ("help,h", "produce help message")
     ("genome,g", po::value(&genome_filename)->required(), "genome_file")
-    ("index,x", po::value(&index_file)->required(), "output file")
-    ("meta", po::value(&metadata_output),
-     "metadata output (defaults to output.json)")
+    ("index,x", po::value(&index_file)->required(),
+     std::format("output file (must end in {})", cpg_index::filename_extension).data())
     ("log-level,v", po::value(&log_level)->default_value(logger::default_level),
      "log level {debug,info,warning,error,critical}")
     // clang-format on
@@ -66,14 +95,18 @@ command_index_main(int argc, char *argv[]) -> int {
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     if (vm.count("help") || argc == 1) {
+      std::println("{}\n{}", about_msg, usage);
       desc.print(std::cout);
+      std::println("\n{}", description_msg);
       return EXIT_SUCCESS;
     }
     po::notify(vm);
   }
   catch (po::error &e) {
-    std::println(std::cerr, "{}", e.what());
-    desc.print(std::cerr);
+    std::println("{}", e.what());
+    std::println("{}\n{}", about_msg, usage);
+    desc.print(std::cout);
+    std::println("\n{}", description_msg);
     return EXIT_FAILURE;
   }
 
@@ -83,8 +116,14 @@ command_index_main(int argc, char *argv[]) -> int {
     return EXIT_FAILURE;
   }
 
-  if (metadata_output.empty())
-    metadata_output = get_default_cpg_index_meta_filename(index_file);
+  const auto extension_found = std::filesystem::path(index_file).extension();
+  if (extension_found != cpg_index::filename_extension) {
+    lgr.error("Required filename extension {} (given: {})",
+              cpg_index::filename_extension, extension_found);
+    return EXIT_FAILURE;
+  }
+
+  const auto metadata_output = get_default_cpg_index_meta_filename(index_file);
 
   std::vector<std::tuple<std::string, std::string>> args_to_log{
     // clang-format off
@@ -98,12 +137,12 @@ command_index_main(int argc, char *argv[]) -> int {
   const auto constr_start = std::chrono::high_resolution_clock::now();
   const auto [index, cim, err] = initialize_cpg_index(genome_filename);
   const auto constr_stop = std::chrono::high_resolution_clock::now();
-  lgr.debug("Index construction time: {:.3}s",
-            duration(constr_start, constr_stop));
   if (err) {
     lgr.error("Error constructing index: {}", err);
     return EXIT_FAILURE;
   }
+  lgr.debug("Index construction time: {:.3}s",
+            duration(constr_start, constr_stop));
 
   if (const auto write_err = index.write(index_file); write_err) {
     lgr.error("Error writing cpg index {}: {}", index_file, write_err);
