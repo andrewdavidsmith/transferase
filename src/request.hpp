de44dfff337a2326cbcc9992a24805481e798b2a
Andrew D Smith <andrewds@usc.edu>
2024-12-12 14:58:34 -0800

    src/request.hpp: moved request_error stuff here from mxe_error.hpp and added nodiscards

--- src/request.hpp
+++ src/request.hpp
@@ -34,6 +34,58 @@
 #include <utility>  // pair<>
 #include <vector>
 
+enum class request_error : std::uint32_t {
+  ok = 0,
+  header_parse_error_accession = 1,
+  header_parse_error_methylome_size = 2,
+  header_parse_error_request_type = 3,
+  lookup_parse_error_n_intervals = 4,
+  lookup_error_reading_offsets = 5,
+  lookup_error_offsets = 6,
+  bins_error_assembly = 7,
+  bins_error_bin_size = 8,
+  noref_error_chroms_n_bytes = 9,
+  noref_error_n_intervals = 10,
+};
+
+// register request_error as error code enum
+template <>
+struct std::is_error_code_enum<request_error> : public std::true_type {};
+
+// category to provide text descriptions
+struct request_error_category : std::error_category {
+  auto
+  name() const noexcept -> const char * override {
+    return "request_error";
+  }
+  auto
+  message(int code) const -> std::string override {
+    using std::string_literals::operator""s;
+    // clang-format off
+    switch (code) {
+    case 0: return "ok"s;
+    case 1: return "header parse error accession"s;
+    case 2: return "header parse error methylome size"s;
+    case 3: return "header parse error request type"s;
+    case 4: return "lookup parse error n_intervals"s;
+    case 5: return "lookup error reading offsets"s;
+    case 6: return "lookup error offsets"s;
+    case 7: return "bins error assembly"s;
+    case 8: return "bins error bin size"s;
+    case 9: return "noref error chroms_n_bytes"s;
+    case 10: return "noref error n_intervals"s;
+    }
+    // clang-format on
+    std::unreachable();  // hopefully this is unreacheable
+  }
+};
+
+inline auto
+make_error_code(request_error e) -> std::error_code {
+  static auto category = request_error_category{};
+  return std::error_code(std::to_underlying(e), category);
+}
+
 static constexpr std::uint32_t request_header_buf_size = 256;  // full request
 typedef std::array<char, request_header_buf_size> request_header_buffer;
 
@@ -43,26 +95,41 @@ struct request_header {
     counts_cov = 1,
     bin_counts = 2,
     bin_counts_cov = 3,
-    n_request_types = 4,
+    counts_noref = 4,
+    counts_noref_cov = 5,
+    n_request_types = 6,
   };
   std::string accession;
   std::uint32_t methylome_size{};
   request_type rq_type{};
+
   auto
+  operator<=>(const request_header &) const = default;
+
+  [[nodiscard]] auto
   summary() const -> std::string;
-  auto
+
+  [[nodiscard]] auto
   is_valid_type() const -> bool {
     return rq_type < request_type::n_request_types;
   }
-  auto
+
+  [[nodiscard]] auto
   is_intervals_request() const -> bool {
     return rq_type == request_type::counts ||
            rq_type == request_type::counts_cov;
   }
-  auto
+
+  [[nodiscard]] auto
   is_bins_request() const -> bool {
     return rq_type == request_type::bin_counts ||
            rq_type == request_type::bin_counts_cov;
+  }
+
+  [[nodiscard]] auto
+  is_intervals_noref_request() const -> bool {
+    return rq_type == request_type::counts_noref ||
+           rq_type == request_type::counts_noref_cov;
   }
 };
 
@@ -87,58 +154,62 @@ struct std::formatter<request_header> : std::formatter<std::string> {
   }
 };
 
-auto
+[[nodiscard]] auto
 to_chars(char *first, char *last,
          const request_header &header) -> mxe_to_chars_result;
 
-auto
+[[nodiscard]] auto
 from_chars(const char *first, const char *last,
            request_header &header) -> mxe_from_chars_result;
 
-auto
+[[nodiscard]] auto
 compose(request_header_buffer &buf,
         const request_header &header) -> compose_result;
 
-auto
+[[nodiscard]] auto
 parse(const request_header_buffer &buf, request_header &header) -> parse_result;
 
 struct request {
   typedef std::pair<std::uint32_t, std::uint32_t> offset_type;
-
   std::uint32_t n_intervals{};
   std::vector<offset_type> offsets;
 
-  auto
+  [[nodiscard]] auto
   summary() const -> std::string;
 
-  auto
+  [[nodiscard]] auto
   get_offsets_n_bytes() const -> std::uint32_t {
-    return sizeof(offset_type) * size(offsets);
+    return sizeof(decltype(offsets)::value_type) * size(offsets);
   }
-  auto
+  [[nodiscard]] auto
   get_offsets_data() -> char * {
     return reinterpret_cast<char *>(offsets.data());
   }
+  auto
+  operator<=>(const request &) const = default;
 };
 
-auto
+// returns an mxe_to_chars_result which is a pair
+[[nodiscard]] auto
 to_chars(char *first, char *last, const request &req) -> mxe_to_chars_result;
 
-auto
+[[nodiscard]] auto
 from_chars(const char *first, const char *last,
            request &req) -> mxe_from_chars_result;
 
 struct bins_request {
   std::uint32_t bin_size{};
-  auto
+  [[nodiscard]] auto
   summary() const -> std::string;
+  auto
+  operator<=>(const bins_request &) const = default;
 };
 
-auto
+[[nodiscard]] auto
 to_chars(char *first, char *last,
          const bins_request &req) -> mxe_to_chars_result;
 
-auto
+[[nodiscard]] auto
 from_chars(const char *first, const char *last,
            bins_request &req) -> mxe_from_chars_result;
 
