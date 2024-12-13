dee90f2516ffbf4271c61a23b44266d1d079fe4e
Andrew D Smith <andrewds@usc.edu>
2024-12-12 20:00:45 -0800

    src/utilities.hpp: gixing headers based on iwyu; changing the name mxe_to_chars_result to compose_result; similar for mxe_from_chars_result to parse_result; moving all the code for writing output in the form of intervals or bins out of this file and into genomic_interval_output.hpp

--- src/utilities.hpp
+++ src/utilities.hpp
@@ -28,191 +28,25 @@
   Functions declared here are used by multiple source files
  */
 
-#include "cpg_index_meta.hpp"
-#include "genomic_interval.hpp"
-#include "methylome_results_types.hpp"  // counts_res and counts_res_cov
-
-#include <zlib.h>
-
-#include <algorithm>
-#include <cassert>
-#include <cerrno>
 #include <chrono>
-#include <cstdint>
 #include <filesystem>
 #include <format>
-#include <fstream>
-#include <iostream>
-#include <limits>
-#include <ranges>
+#include <iterator>  // for std::size
 #include <string>
 #include <string_view>
 #include <system_error>
 #include <tuple>
-#include <utility>
-#include <vector>
+#include <variant>  // IWYU pragma: keep
 
-struct mxe_to_chars_result {
+struct compose_result {
   char *ptr{};
   std::error_code error{};
 };
-typedef mxe_to_chars_result compose_result;
 
-struct mxe_from_chars_result {
+struct parse_result {
   const char *ptr{};
   std::error_code error{};
 };
-typedef mxe_from_chars_result parse_result;
-
-auto
-write_intervals(std::ostream &out, const cpg_index_meta &cim,
-                const std::vector<genomic_interval> &gis,
-                std::ranges::input_range auto &&results) -> std::error_code {
-  static constexpr auto buf_size{512};
-  static constexpr auto delim{'\t'};
-
-  std::array<char, buf_size> buf{};
-  const auto buf_end = buf.data() + buf_size;
-
-  using counts_res_type =
-    typename std::remove_cvref_t<decltype(results)>::value_type;
-  using gis_res = std::pair<const genomic_interval &, counts_res_type>;
-  const auto same_chrom = [](const gis_res &a, const gis_res &b) {
-    return a.first.ch_id == b.first.ch_id;
-  };
-
-  for (const auto &chunk :
-       std::views::zip(gis, results) | std::views::chunk_by(same_chrom)) {
-    const auto ch_id = get<0>(chunk.front()).ch_id;
-    const std::string chrom{cim.chrom_order[ch_id]};
-    std::ranges::copy(chrom, buf.data());
-    buf[size(chrom)] = delim;
-    for (const auto &[gi, single_result] : chunk) {
-      std::to_chars_result tcr{buf.data() + size(chrom) + 1, std::errc()};
-#if defined(__GNUG__) and not defined(__clang__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic error "-Wstringop-overflow=0"
-#endif
-      tcr = std::to_chars(tcr.ptr, buf_end, gi.start);
-      *tcr.ptr++ = delim;
-      tcr = std::to_chars(tcr.ptr, buf_end, gi.stop);
-      *tcr.ptr++ = delim;
-      tcr = std::to_chars(tcr.ptr, buf_end, single_result.n_meth);
-      *tcr.ptr++ = delim;
-      tcr = std::to_chars(tcr.ptr, buf_end, single_result.n_unmeth);
-      if constexpr (std::is_same<counts_res_type, counts_res_cov>::value) {
-        *tcr.ptr++ = delim;
-        tcr = std::to_chars(tcr.ptr, buf_end, single_result.n_covered);
-      }
-      *tcr.ptr++ = '\n';
-#if defined(__GNUG__) and not defined(__clang__)
-#pragma GCC diagnostic pop
-#endif
-      out.write(buf.data(), std::ranges::distance(buf.data(), tcr.ptr));
-      if (!out)
-        return std::make_error_code(std::errc(errno));
-    }
-  }
-  return {};
-}
-
-auto
-write_bedgraph(std::ostream &out, const cpg_index_meta &cim,
-               const std::vector<genomic_interval> &gis,
-               std::ranges::input_range auto &&scores) -> std::error_code {
-  static constexpr auto score_precision{6};
-  static constexpr auto buf_size{512};
-  static constexpr auto delim{'\t'};
-
-  std::array<char, buf_size> buf{};
-  const auto buf_end = buf.data() + buf_size;
-
-  using gis_score = std::pair<const genomic_interval &, const double>;
-  const auto same_chrom = [](const gis_score &a, const gis_score &b) {
-    return a.first.ch_id == b.first.ch_id;
-  };
-
-  for (const auto &chunk :
-       std::views::zip(gis, scores) | std::views::chunk_by(same_chrom)) {
-    const auto ch_id = get<0>(chunk.front()).ch_id;
-    const std::string chrom{cim.chrom_order[ch_id]};
-    std::ranges::copy(chrom, buf.data());
-    buf[size(chrom)] = delim;
-    for (const auto &[gi, single_score] : chunk) {
-      std::to_chars_result tcr{buf.data() + size(chrom) + 1, std::errc()};
-#if defined(__GNUG__) and not defined(__clang__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic error "-Wstringop-overflow=0"
-#endif
-      tcr = std::to_chars(tcr.ptr, buf_end, gi.start);
-      *tcr.ptr++ = delim;
-      tcr = std::to_chars(tcr.ptr, buf_end, gi.stop);
-      *tcr.ptr++ = delim;
-      // ADS: below the format is intended to mimick default for cout
-      tcr = std::to_chars(tcr.ptr, buf_end, single_score,
-                          std::chars_format::general, score_precision);
-      *tcr.ptr++ = '\n';
-#if defined(__GNUG__) and not defined(__clang__)
-#pragma GCC diagnostic pop
-#endif
-      out.write(buf.data(), std::ranges::distance(buf.data(), tcr.ptr));
-      if (!out)
-        return std::make_error_code(std::errc(errno));
-    }
-  }
-  return {};
-}
-
-auto
-write_bins(std::ostream &out, const std::uint32_t bin_size,
-           const cpg_index_meta &cim, const auto &results) -> std::error_code {
-  static constexpr auto buf_size{512};
-  static constexpr auto delim{'\t'};
-
-  using counts_res_type =
-    typename std::remove_cvref_t<decltype(results)>::value_type;
-
-  std::array<char, buf_size> buf{};
-  const auto buf_end = buf.data() + buf_size;
-
-  auto results_itr = std::cbegin(results);
-
-  const auto zipped = std::views::zip(cim.chrom_size, cim.chrom_order);
-  for (const auto [chrom_size, chrom_name] : zipped) {
-    std::ranges::copy(chrom_name, buf.data());
-    buf[size(chrom_name)] = delim;
-    for (std::uint32_t bin_beg = 0; bin_beg < chrom_size; bin_beg += bin_size) {
-      const auto bin_end = std::min(bin_beg + bin_size, chrom_size);
-      std::to_chars_result tcr{buf.data() + std::size(chrom_name) + 1,
-                               std::errc()};
-#if defined(__GNUG__) and not defined(__clang__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic error "-Wstringop-overflow=0"
-#endif
-      tcr = std::to_chars(tcr.ptr, buf_end, bin_beg);
-      *tcr.ptr++ = delim;
-      tcr = std::to_chars(tcr.ptr, buf_end, bin_end);
-      *tcr.ptr++ = delim;
-      tcr = std::to_chars(tcr.ptr, buf_end, results_itr->n_meth);
-      *tcr.ptr++ = delim;
-      tcr = std::to_chars(tcr.ptr, buf_end, results_itr->n_unmeth);
-      if constexpr (std::is_same<counts_res_type, counts_res_cov>::value) {
-        *tcr.ptr++ = delim;
-        tcr = std::to_chars(tcr.ptr, buf_end, results_itr->n_covered);
-      }
-      *tcr.ptr++ = '\n';
-#if defined(__GNUG__) and not defined(__clang__)
-#pragma GCC diagnostic pop
-#endif
-      out.write(buf.data(), std::ranges::distance(buf.data(), tcr.ptr));
-      if (!out)
-        return std::make_error_code(std::errc(errno));
-      ++results_itr;
-    }
-  }
-  assert(results_itr == std::cend(results));
-  return {};
-}
 
 [[nodiscard]] inline auto
 duration(const auto start, const auto stop) {
