1f3ab7992c6248f01422102b0831c84846190633
Andrew D Smith <andrewds@usc.edu>
2024-12-16 11:10:08 -0800

    src/genomic_interval.hpp: moving genomic_interval_code here from xfrase_error and adding the intervals_sorted and intervals_valid functions here

--- src/genomic_interval.hpp
+++ src/genomic_interval.hpp
@@ -24,17 +24,21 @@
 #ifndef SRC_GENOMIC_INTERVAL_HPP_
 #define SRC_GENOMIC_INTERVAL_HPP_
 
+#include <algorithm>
 #include <cstdint>
 #include <format>
+#include <ranges>
 #include <string>
 #include <system_error>
+#include <tuple>
+#include <utility>  // std::unreachable
 #include <vector>
 
 struct cpg_index_meta;
-struct genomic_interval_load_ret;
 
 struct genomic_interval {
-  std::int32_t ch_id{-1};
+  static constexpr auto not_a_chrom{-1};
+  std::int32_t ch_id{not_a_chrom};
   std::uint32_t start{};
   std::uint32_t stop{};
 
@@ -42,22 +46,69 @@ struct genomic_interval {
   operator<=>(const genomic_interval &) const = default;
 
   [[nodiscard]] static auto
-  load(const cpg_index_meta &index,
-       const std::string &filename) -> genomic_interval_load_ret;
+  load(const cpg_index_meta &index, const std::string &filename)
+    -> std::tuple<std::vector<genomic_interval>, std::error_code>;
 };
 
-struct genomic_interval_load_ret {
-  std::vector<genomic_interval> gis;
-  std::error_code ec;
+// ADS: Sorted intervals have chromosomes together but the order on
+// chroms is arbitrary; then they are ordered by first coord position
+// and second position is not relevant.
+[[nodiscard]] auto
+intervals_sorted(const cpg_index_meta &cim,
+                 const std::vector<genomic_interval> &gis) -> bool;
+
+[[nodiscard]] inline auto
+intervals_valid(const auto &g) -> bool {
+  return std::ranges::all_of(g, [](const auto x) { return x.start <= x.stop; });
 };
 
 template <>
 struct std::formatter<genomic_interval> : std::formatter<std::string> {
   auto
   format(const genomic_interval &gi, std::format_context &ctx) const {
-    return std::formatter<std::string>::format(
-      std::format("{}\t{}\t{}", gi.ch_id, gi.start, gi.stop), ctx);
+    return std::format_to(ctx.out(), "{}\t{}\t{}", gi.ch_id, gi.start, gi.stop);
   }
 };
+
+// genomic_interval errors
+
+enum class genomic_interval_code : std::uint32_t {
+  ok = 0,
+  error_parsing_bed_line = 1,
+  chrom_name_not_found_in_index = 2,
+  interval_past_chrom_end_in_index = 3,
+};
+
+// register genomic_interval_code as error code enum
+template <>
+struct std::is_error_code_enum<genomic_interval_code> : public std::true_type {
+};
+
+// category to provide text descriptions
+struct genomic_interval_category : std::error_category {
+  auto
+  name() const noexcept -> const char * override {
+    return "genomic_interval";
+  }
+  auto
+  message(int code) const -> std::string override {
+    using std::string_literals::operator""s;
+    // clang-format off
+    switch (code) {
+    case 0: return "ok"s;
+    case 1: return "error parsing BED line"s;
+    case 2: return "chrom name not found in index"s;
+    case 3: return "interval past chrom end in index"s;
+    }
+    // clang-format on
+    std::unreachable();  // hopefully
+  }
+};
+
+inline auto
+make_error_code(genomic_interval_code e) -> std::error_code {
+  static auto category = genomic_interval_category{};
+  return std::error_code(std::to_underlying(e), category);
+}
 
 #endif  // SRC_GENOMIC_INTERVAL_HPP_
