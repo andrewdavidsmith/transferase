06181375f6f850f67f3c1a17b48d46681206accc
Andrew D Smith <andrewds@usc.edu>
2024-12-27 13:17:30 -0800

    test/cpg_index_data_test.cpp: adding several tests and fixing names of tests

--- test/cpg_index_data_test.cpp
+++ test/cpg_index_data_test.cpp
@@ -25,6 +25,7 @@
 
 #include <cpg_index.hpp>
 #include <cpg_index_metadata.hpp>  // IWYU pragma: keep
+#include <utilities.hpp>
 
 #include <gtest/gtest.h>
 
@@ -34,23 +35,76 @@
 #include <vector>
 
 TEST(cpg_index_data_test, basic_assertions) {
-  cpg_index_data ci;
-  EXPECT_EQ(ci.positions, std::vector<cpg_index_data::vec>());
-  EXPECT_EQ(ci.hash(), 1);
+  cpg_index_data data;
+  EXPECT_EQ(data.positions, std::vector<cpg_index_data::vec>());
+  EXPECT_EQ(data.hash(), 1);
 }
 
-TEST(read_cpg_index_data_test, valid_index_file) {
-  static constexpr auto index_dir{"data"};
-  static constexpr auto assembly{"tProrsus1"};
+TEST(cpg_index_data_test, compose_cpg_index_data_filename_test) {
+  static constexpr auto index_directory = "data/lutions/methylomes";
+  static constexpr auto genome_name = "eFlareon";
+  static constexpr auto expected_filename =
+    "data/lutions/methylomes/eFlareon.cpg_idx";
+  const auto filename =
+    compose_cpg_index_data_filename(index_directory, genome_name);
+  EXPECT_EQ(filename, expected_filename);
+}
+
+TEST(cpg_index_data_test, valid_read) {
+  static constexpr auto dirname{"data"};
+  static constexpr auto genome_name{"pAntiquusx"};
   std::error_code ec;
-  const auto index = cpg_index::read(index_dir, assembly, ec);
+  [[maybe_unused]] const auto meta =
+    cpg_index_metadata::read(dirname, genome_name, ec);
   EXPECT_FALSE(ec);
-  EXPECT_GT(std::size(index.meta.chrom_order), 0);
-  EXPECT_GT(std::size(index.meta.chrom_size), 0);
-  EXPECT_GT(std::size(index.data.positions), 0);
+  [[maybe_unused]] const auto data =
+    cpg_index_data::read(dirname, genome_name, meta, ec);
+  EXPECT_FALSE(ec);
 }
 
-TEST(read_cpg_index_data_test, invalid_index_file) {
+TEST(cpg_index_data_test, valid_write) {
+  const std::filesystem::path output_file{
+    generate_temp_filename("file", cpg_index_data::filename_extension)};
+  cpg_index_data data;
+  data.positions.push_back({1, 2, 3, 4, 5});
+  std::error_code ec = data.write(output_file);
+  EXPECT_EQ(ec, std::error_code{});
+  const auto outfile_exists = std::filesystem::exists(output_file, ec);
+  EXPECT_EQ(ec, std::error_code{});
+  EXPECT_TRUE(outfile_exists);
+
+  std::filesystem::remove(output_file, ec);
+  EXPECT_EQ(ec, std::error_code{});
+}
+
+TEST(cpg_index_data_test, valid_round_trip) {
+  static constexpr auto dirname{"data"};
+  static constexpr auto genome_name{"pAntiquusx"};
+  std::error_code ec;
+  [[maybe_unused]] const auto meta =
+    cpg_index_metadata::read(dirname, genome_name, ec);
+  EXPECT_FALSE(ec);
+  auto data = cpg_index_data::read(dirname, genome_name, meta, ec);
+  EXPECT_FALSE(ec);
+
+  const std::filesystem::path output_file{
+    generate_temp_filename("file", cpg_index_data::filename_extension)};
+  ec = data.write(output_file);
+  EXPECT_FALSE(ec);
+  const auto outfile_exists = std::filesystem::exists(output_file, ec);
+  EXPECT_FALSE(ec);
+  EXPECT_TRUE(outfile_exists);
+
+  auto data2 = cpg_index_data::read(output_file, meta, ec);
+  EXPECT_FALSE(ec);
+
+  EXPECT_EQ(data.positions, data2.positions);
+
+  std::filesystem::remove(output_file, ec);
+  EXPECT_FALSE(ec);
+}
+
+TEST(cpg_index_data_test, invalid_read) {
   static constexpr auto index_dir{"data"};
   static constexpr auto assembly{"invalid_index_file"};
   std::error_code ec;
@@ -61,22 +115,14 @@ TEST(read_cpg_index_data_test, invalid_index_file) {
   EXPECT_EQ(std::size(index.data.positions), 0);
 }
 
-TEST(cpg_index_data_write_test, valid_write) {
-  cpg_index_data index;
-  // Fill index with test data
-  index.positions.push_back({1, 2, 3});
-  const auto ec = index.write("test_index_file.cpg_idx");
-  EXPECT_FALSE(ec);
-}
-
-TEST(cpg_index_data_get_offsets_with_chrom_test, valid_offsets) {
+TEST(cpg_index_data_test, valid_get_query_within_chrom) {
   cpg_index_data index;
   index.positions.push_back({1, 2, 3, 4, 5});
   std::vector<std::pair<std::uint32_t, std::uint32_t>> queries = {
     {1, 3},
     {4, 5},
   };
-  const auto offsets = index.get_offsets_within_chrom(0, queries);
+  const auto offsets = index.get_query_within_chrom(0, queries);
   std::vector<std::pair<std::uint32_t, std::uint32_t>> expected = {
     {0, 2},
     {3, 4},
