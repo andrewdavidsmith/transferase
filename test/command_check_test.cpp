e46a925d5686b88b226acee62ffd001bf9e952fd
Andrew D Smith <andrewds@usc.edu>
2024-12-27 19:20:29 -0800

    test/command_check_test.cpp: adding tests for the config command

---
+++ test/command_check_test.cpp
@@ -0,0 +1,68 @@
+/* MIT License
+ *
+ * Copyright (c) 2024 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <command_check.hpp>
+
+#include "unit_test_utils.hpp"
+
+#include <gtest/gtest.h>
+
+#include <cstdlib>  // for EXIT_SUCCESS
+#include <string>
+
+TEST(command_check_test, basic_test) {
+  static constexpr auto methylome_directory = "data/lutions/methylomes";
+  static constexpr auto index_directory = "data/lutions/indexes";
+  static constexpr auto genome_name = "eJolteon";
+  using std::literals::string_view_literals::operator""sv;
+  static constexpr auto methylome_names = std::array{
+    "eJolteon_brain"sv,
+    "eJolteon_tail"sv,
+    "eJolteon_ear"sv,
+  };
+
+  // Define command line arguments
+  const char *command_argv[] = {
+    // clang-format off
+    "check",
+    "-v",
+    "error",
+    "-x",
+    index_directory,
+    "-g",
+    genome_name,
+    "-d",
+    methylome_directory,
+    "-m",
+    methylome_names[0].data(),
+    methylome_names[1].data(),
+    methylome_names[2].data(),
+    // clang-format on
+  };
+  const int command_argc = sizeof(command_argv) / sizeof(command_argv[0]);
+
+  // Run the main function
+  const int result =
+    command_check_main(command_argc, const_cast<char **>(command_argv));
+  EXPECT_EQ(result, EXIT_SUCCESS);
+}
