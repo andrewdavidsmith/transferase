6874efc1dc5cbce00894879b66ef2b62e2ecbb33
Andrew D Smith <andrewds@usc.edu>
2024-12-27 16:54:35 -0800

    cmake/packaging.cmake: only doing Debian stuff if the system is linux

--- cmake/packaging.cmake
+++ cmake/packaging.cmake
@@ -33,39 +33,41 @@ set(CPACK_STRIP_FILES ON)
 ## For STGZ
 set(CPACK_STGZ_HEADER_FILE "${CMAKE_SOURCE_DIR}/modules/CPack.STGZ_Header.sh.in")
 
-## For Debian
-set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Andrew D Smith <andrewds@usc.edu>")
-# Get the system processor architecture
-message(STATUS "System Architecture: ${CMAKE_SYSTEM_PROCESSOR}")
-# Check if the architecture is x86_64 and set to amd64 for Debian packaging
-if(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
-  set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64")
-elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
-  set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "arm64")
-else()
-  set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "${CMAKE_SYSTEM_PROCESSOR}")
-endif()
+## Only make the Debian installer if building on Linux
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+  ## For Debian
+  set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Andrew D Smith <andrewds@usc.edu>")
+  # Get the system processor architecture
+  message(STATUS "System Architecture: ${CMAKE_SYSTEM_PROCESSOR}")
+  # Check if the architecture is x86_64 and set to amd64 for Debian packaging
+  if(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+    set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64")
+  elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+    set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "arm64")
+  else()
+    set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "${CMAKE_SYSTEM_PROCESSOR}")
+  endif()
 
-# Print the architecture being used for the Debian package
-message(STATUS "Debian package architecture: ${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}")
-# Run `ldd --version` to get glibc version
-execute_process(
-  COMMAND sh -c "ldd --version | head -1 | awk '{print \$NF}'"
-  OUTPUT_VARIABLE GLIBC_VERSION
-  ERROR_VARIABLE LDD_ERROR
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-if(GLIBC_VERSION AND NOT LDD_ERROR)
-  message(STATUS "Detected glibc version: ${GLIBC_VERSION}")
-else()
-  message(FATAL_ERROR "Failed to detect glibc version")
+  # Print the architecture being used for the Debian package
+  message(STATUS "Debian package architecture: ${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}")
+  # Run `ldd --version` to get glibc version
+  execute_process(
+    COMMAND sh -c "ldd --version | head -1 | awk '{print \$NF}'"
+    OUTPUT_VARIABLE GLIBC_VERSION
+    ERROR_VARIABLE LDD_ERROR
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  if(GLIBC_VERSION AND NOT LDD_ERROR)
+    message(STATUS "Detected glibc version: ${GLIBC_VERSION}")
+  else()
+    message(FATAL_ERROR "Failed to detect glibc version")
+  endif()
+  # Set dependency libc6 with the version constraint
+  set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>= ${GLIBC_VERSION})")
 endif()
-# Set dependency libc6 with the version constraint
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>= ${GLIBC_VERSION})")
 
 ### For Source
 # Only package source from a clean clone
 set(CPACK_SOURCE_IGNORE_FILES build .git .github)
 set(CPACK_SOURCE_GENERATOR "TGZ")
-
 include(CPack)
