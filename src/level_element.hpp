20d4e537771292177dc9ebc846a4e8158bc1a95d
Andrew D Smith <andrewds@usc.edu>
2025-01-02 13:47:26 -0800

    src/level_element.hpp: adding this source file with a classes to represent the tuples of elements for each interval or bin in a response

---
+++ src/level_element.hpp
@@ -0,0 +1,71 @@
+/* MIT License
+ *
+ * Copyright (c) 2024 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SRC_LEVEL_ELEMENT_HPP_
+#define SRC_LEVEL_ELEMENT_HPP_
+
+#include <cstdint>
+#include <format>
+#include <string>
+
+namespace xfrase {
+
+struct level_element_t {
+  std::uint32_t n_meth{};
+  std::uint32_t n_unmeth{};
+  auto
+  operator<=>(const level_element_t &) const = default;
+};
+
+struct level_element_covered_t {
+  std::uint32_t n_meth{};
+  std::uint32_t n_unmeth{};
+  std::uint32_t n_covered{};
+  auto
+  operator<=>(const level_element_covered_t &) const = default;
+};
+
+}  // namespace xfrase
+
+template <>
+struct std::formatter<xfrase::level_element_t> : std::formatter<std::string> {
+  auto
+  format(const xfrase::level_element_t &l, std::format_context &ctx) const {
+    return std::format_to(ctx.out(), R"({{"n_meth": {}, "n_unmeth": {}}})",
+                          l.n_meth, l.n_unmeth);
+  }
+};
+
+template <>
+struct std::formatter<xfrase::level_element_covered_t>
+  : std::formatter<std::string> {
+  auto
+  format(const xfrase::level_element_covered_t &l,
+         std::format_context &ctx) const {
+    return std::format_to(
+      ctx.out(), R"({{"n_meth": {}, "n_unmeth": {}, "n_covered": {}}})",
+      l.n_meth, l.n_unmeth, l.n_covered);
+  }
+};
+
+#endif  // SRC_LEVEL_ELEMENT_HPP_
