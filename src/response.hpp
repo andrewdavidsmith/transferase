2bc59f76bbadb0ee6ac45c74303d751ccde3135c
Andrew D Smith <andrewds@usc.edu>
2025-01-13 15:20:46 -0800

    src/response.hpp: replaced the response_size var from response_header class to have both rows and cols since we now deal with multiple methylomes in requests; removed the response class entirely; expanded the behavior of the response_payload class so it can be converted to and from vectors of level_containers

--- src/response.hpp
+++ src/response.hpp
@@ -29,6 +29,7 @@
 #include <array>
 #include <cstddef>  // for std::byte
 #include <cstdint>
+#include <cstring>
 #include <iterator>  // for std::size
 #include <string>
 #include <system_error>
@@ -42,35 +43,39 @@ typedef std::array<char, response_header_buffer_size> response_header_buffer;
 
 struct response_header {
   std::error_code status{};
-  std::uint32_t response_size{};
-  // ADS: doing the strange stuff below for cpplint...
-  // clang-format off
-  [[nodiscard]] auto error() const -> bool { return (status) ? true : false; }
-  [[nodiscard]] auto summary() const -> std::string;
-  // clang-format on
+  std::uint32_t cols{};
+  std::uint32_t rows{};
+  [[nodiscard]] auto
+  error() const noexcept -> bool {
+    return (status) ? true : false;
+  }
+  [[nodiscard]] auto
+  summary() const noexcept -> std::string;
 };
 
 [[nodiscard]] auto
 compose(char *first, const char *last,
-        const response_header &hdr) -> std::error_code;
+        const response_header &hdr) noexcept -> std::error_code;
 
 [[nodiscard]] auto
 parse(const char *first, const char *last,
-      response_header &hdr) -> std::error_code;
+      response_header &hdr) noexcept -> std::error_code;
 
 [[nodiscard]] auto
 compose(response_header_buffer &buf,
-        const response_header &hdr) -> std::error_code;
+        const response_header &hdr) noexcept -> std::error_code;
 
 [[nodiscard]] auto
 parse(const response_header_buffer &buf,
-      response_header &hdr) -> std::error_code;
+      response_header &hdr) noexcept -> std::error_code;
 
 struct response_payload {
-  std::vector<std::byte> payload;
+  typedef std::byte value_type;
+  std::vector<value_type> payload;
 
   response_payload() = default;
-  // ADS: update this to prevent a copy
+  explicit response_payload(const std::size_t n_bytes) noexcept :
+    payload{n_bytes} {}
   explicit response_payload(std::vector<std::byte> &&payload) noexcept :
     payload{std::move(payload)} {}
 
@@ -82,17 +87,85 @@ struct response_payload {
   response_payload &operator=(response_payload &&) noexcept = default;
   // clang-format on
 
+  template <typename lvl_elem>
+  [[nodiscard]] static auto
+  from_levels(const std::vector<level_container<lvl_elem>> &levels,
+              std::error_code &error) -> response_payload {
+    // ADS: slower than needed; copy happening here is not needed
+    error = std::error_code{};  // clear this in case it was set
+    const auto tot_bytes =
+      std::size(levels) * sizeof(lvl_elem) * size(levels[0]);
+    response_payload rp(tot_bytes);
+    std::size_t byte_offset{};
+    for (const auto &lvl : levels) {
+      auto dest = rp.data_at(byte_offset, error);
+      if (error)
+        return {};
+      std::memcpy(dest, lvl.data(), lvl.get_n_bytes());
+      byte_offset += lvl.get_n_bytes();
+    }
+    return rp;
+  }
+
+  template <typename lvl_elem>
+  [[nodiscard]] auto
+  to_levels(const response_header &hdr, std::error_code &error) const
+    -> std::vector<level_container<lvl_elem>> {
+    // ADS: slower than needed; copy happening here is not needed
+    error = std::error_code{};  // clear this in case it was set
+    std::vector<level_container<lvl_elem>> result;
+    std::size_t byte_offset{};
+    for (auto col_id = 0u; col_id < hdr.cols; ++col_id) {
+      auto source = data_at(byte_offset, error);
+      if (error)
+        return {};
+      level_container<lvl_elem> lvl(hdr.rows);
+      std::memcpy(lvl.data(), source, lvl.get_n_bytes());
+      result.emplace_back(std::move(lvl));
+      byte_offset += lvl.get_n_bytes();
+    }
+    return result;
+  }
+
+  [[nodiscard]] auto
+  data() noexcept -> char * {
+    // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
+    return reinterpret_cast<char *>(payload.data());
+  }
+
+  [[nodiscard]] auto
+  data() const noexcept -> const char * {
+    // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
+    return reinterpret_cast<const char *>(payload.data());
+  }
+
+  [[nodiscard]] auto
+  data_at(const std::size_t byte_offset,
+          std::error_code &error) noexcept -> char * {
+    if (byte_offset >= std::size(payload) * sizeof(value_type)) {
+      error = std::make_error_code(std::errc::result_out_of_range);
+      return nullptr;
+    }
+    error = std::error_code{};
+    // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
+    return reinterpret_cast<char *>(payload.data()) + byte_offset;
+  }
+
+  [[nodiscard]] auto
+  data_at(const std::size_t byte_offset,
+          std::error_code &error) const noexcept -> const char * {
+    if (byte_offset >= std::size(payload) * sizeof(value_type)) {
+      error = std::make_error_code(std::errc::result_out_of_range);
+      return nullptr;
+    }
+    error = std::error_code{};
+    // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
+    return reinterpret_cast<const char *>(payload.data()) + byte_offset;
+  }
+
   [[nodiscard]] auto
   n_bytes() const -> std::uint32_t {
     return std::size(payload);
-  }
-};
-
-template <typename level_element> struct response {
-  level_container<level_element> levels;
-  [[nodiscard]] auto
-  get_levels_n_bytes() const -> std::uint32_t {
-    return sizeof(level_element) * size(levels);
   }
 };
 
