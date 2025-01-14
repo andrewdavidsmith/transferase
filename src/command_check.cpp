0ec0f986de439c1f87b8ed001402cf68db095ac6
Andrew D Smith <andrewds@usc.edu>
2025-01-13 21:56:14 -0800

    src/command_check.cpp: updated to reflect methylome::global_levels now templated

--- src/command_check.cpp
+++ src/command_check.cpp
@@ -162,8 +162,8 @@ command_check_main(int argc,
       return EXIT_FAILURE;
     }
 
-    lgr.info("Methylome global levels: {}", meth.global_levels());
-    lgr.info("Methylome sites covered: {}", meth.global_levels_covered());
+    lgr.info("Methylome methylation levels: {}",
+             meth.global_levels<transferase::level_element_covered_t>());
 
     const auto methylome_consistency = meth.is_consistent();
     lgr.info("Methylome data and metadata are consistent: {}",
