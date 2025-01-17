46fa8b142a29aa9cc07e796cd04f41b5582087e9
Andrew D Smith <andrewds@usc.edu>
2025-01-16 20:21:28 -0800

    merging the config file and cli options

--- src/command_config_argset.hpp
+++ src/command_config_argset.hpp
@@ -51,7 +51,7 @@ struct command_config_argset : argset_base<command_config_argset> {
   }
 
   [[nodiscard]] static auto
-  get_default_config_dir_impl() -> std::string {
+  get_default_config_dir() -> std::string {
     std::error_code ec;
     const auto config_dir = get_transferase_config_dir_default(ec);
     if (ec)
@@ -68,26 +68,21 @@ struct command_config_argset : argset_base<command_config_argset> {
   std::string log_filename{};
   std::string genomes{};
   log_level_t log_level{};
-  std::string client_config_file{};
   bool quiet{};
 
   auto
   log_options_impl() const {}
 
   [[nodiscard]] auto
-  set_common_opts_impl() -> boost::program_options::options_description {
-    return {};
-  }
-
-  [[nodiscard]] auto
-  set_cli_only_opts_impl() -> boost::program_options::options_description {
+  set_opts_impl() -> boost::program_options::options_description {
     namespace po = boost::program_options;
     using po::value;
-    boost::program_options::options_description opts("Command line only");
+    boost::program_options::options_description opts("Options");
+    skip_parsing_config_file = true;
     // clang-format off
     opts.add_options()
       ("help,h", "print this message and exit")
-      ("config-file,c", value(&client_config_file)
+      ("config-file,c", value(&config_file)
        ->value_name("[arg]")->default_value(get_default_config_file(), ""),
        "name of configuration file; see help for default")
       ("hostname,s", value(&hostname)->required(), "transferase server hostname")
@@ -99,7 +94,7 @@ struct command_config_argset : argset_base<command_config_argset> {
       ("methylome-dir,d", value(&methylome_dir),
        "name of a directory to search for methylomes (default: not specified)")
       ("log-level,v", value(&log_level)->default_value(log_level_default, ""),
-       "log level {debug,info,warning,error,critical}")
+       "{debug, info, warning, error, critical}")
       ("log-file,l", value(&log_filename)->value_name("console"),
        "log file name")
       ("quiet,q", po::bool_switch(&quiet), "only print error info to terminal")
