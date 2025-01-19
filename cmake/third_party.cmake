71d80f965b9f88e0247494698596f5c7519fd4a0
Andrew D Smith <andrewds@usc.edu>
2025-01-18 19:18:18 -0800

    cmake/third_party.cmake: big changes here. The python and pybind11 stuff is moved here. The zlib stuff is moved here and for python it's now cloned and built because usually it doesn't install as position independent

--- cmake/third_party.cmake
+++ cmake/third_party.cmake
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2024 Andrew Smith
+# Copyright (c) 2024-2025 Andrew Smith
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -20,19 +20,71 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-message(STATUS "Locating required third-party packages")
+message(STATUS "Locating REQUIRED third-party packages")
 
 find_package(Threads REQUIRED)
 
 set(BOOST_COMPONENTS program_options context)
-## Building python lib needs -fPIC and Boost.json has an issue with
-## this so BOOST_JSON_NO_LIB will be passed to sources and turn on
-## header-only for Boost.json
-if(NOT BUILD_PYTHON)
-  list(APPEND BOOST_COMPONENTS json)
-else()
+
+# Boost.JSON
+if(BUILD_PYTHON)
+  ## Building python lib needs -fPIC and Boost.json has an issue with
+  ## this so BOOST_JSON_NO_LIB will be passed to sources and turn on
+  ## header-only for Boost.json
   set(BOOST_JSON_NO_LIB on)
+else()
+  list(APPEND BOOST_COMPONENTS json)
 endif()
+
+# ZLib
+if(BUILD_PYTHON)
+  # Typically ZLib isn't built with -fPIC, but it's small so we can
+  # clone and build it if needed
+  message(STATUS "Configuring to clone ZLib for Python API")
+  include(ExternalProject)
+  ExternalProject_Add(
+    ZLIB
+    GIT_REPOSITORY https://github.com/madler/zlib.git
+    GIT_TAG master
+    CMAKE_ARGS
+    -DZLIB_BUILD_EXAMPLES=off
+    -DSKIP_INSTALL_FILES=on
+    -DCMAKE_POSITION_INDEPENDENT_CODE=on
+    -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/src/zlib
+    -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS_RELEASE_INIT}
+    -DCMAKE_BUILD_TYPE=Release
+  )
+  # Include the built zlib headers and link against the built zlib library
+  set(ZLIB_INCLUDE_DIR "${PROJECT_BINARY_DIR}/src/zlib/include")
+  set(ZLIB_LIBRARY "${PROJECT_BINARY_DIR}/src/zlib/lib/libz.a")
+
+  # Create the CMake target for the built zlib
+  add_library(ZLIB_IMPORTED INTERFACE)
+  set_target_properties(ZLIB_IMPORTED PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES ${ZLIB_INCLUDE_DIR}
+    INTERFACE_LINK_LIBRARIES ${ZLIB_LIBRARY}
+  )
+
+  # This alias means we don't need to worry where zlib came from
+  add_library(ZLIB::ZLIB ALIAS ZLIB_IMPORTED)
+else()
+  find_package(ZLIB REQUIRED)
+endif()
+
+# Python and Pybind11
+if(BUILD_PYTHON)
+  # Python3
+  find_package(Python3 REQUIRED COMPONENTS Interpreter Development)
+  message(STATUS "Python3 include dir: ${Python3_INCLUDE_DIRS}")
+  message(STATUS "Python3 libraries: ${Python3_LIBRARIES}")
+  message(STATUS "Python3 version: ${Python3_VERSION}")
+  message(STATUS "Python3 SOABI: ${Python3_SOABI}")
+
+  # Pybind11
+  find_package(pybind11 REQUIRED)
+endif()
+
+# Boost components
 find_package(
   Boost
   1.85.0 REQUIRED
@@ -40,10 +92,10 @@ find_package(
   ${BOOST_COMPONENTS}
 )
 
-find_package(ZLIB REQUIRED)
+message(STATUS "Locating OPTIONAL third-party packages")
 
-message(STATUS "Locating optional third-party packages")
-
+# Curses
+include(FindCurses)  # This should be in the modules dir
 set(CURSES_NEED_NCURSES TRUE)
 find_package(Curses)
 if(CURSES_FOUND AND CURSES_HAVE_NCURSES_H)
@@ -53,5 +105,5 @@ if(CURSES_FOUND AND CURSES_HAVE_NCURSES_H)
   message(STATUS "Found ncurses header: ${CURSES_HAVE_NCURSES_H}")
   message(STATUS "Found ncurses libraries: ${CURSES_LIBRARIES}")
 else()
-  message(STATUS "NCurses not found; 'select' command will not be built")
+  message(STATUS "NCurses not found: 'select' command will not be built")
 endif()
