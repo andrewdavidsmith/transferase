91b005a973fb0c9b5e9c20299ce01f15d1fa2d0b
Andrew D Smith <andrewds@usc.edu>
2025-01-20 17:13:59 -0800

    src/command_query.cpp: includes the code for both bins and intervals queries

--- src/command_query.cpp
+++ src/command_query.cpp
@@ -21,33 +21,40 @@
  * SOFTWARE.
  */
 
-#include "command_intervals.hpp"
+#include "command_query.hpp"
 
 static constexpr auto about = R"(
-summarize methylation levels in each of a set of genomic intervals
+query methylation levels in genomic intervals or bins
 )";
 
 static constexpr auto description = R"(
-The intervals command accepts a set of genomic intervals and a
-methylome, and it generates a summary of the methylation levels in
-each interval. This command runs in two modes, local and remote. The
-local mode is for analyzing data on your local storage: either your
-own data or data that you downloaded. The remote mode is for analyzing
-methylomes in a remote database on a server. Depending on the mode you
-select, the options you must specify will differ.
+The query command accepts either a set of genomic intervals or a bin
+size, along with a set of methylome names. It generates a summary of
+the methylation levels in each interval/bin, for each methylome. This
+command runs in two modes, local and remote. The local mode is for
+analyzing data on your local storage: either your own data or data
+that you downloaded. The remote mode is for analyzing methylomes in a
+remote database on a server. Depending on the mode you select, the
+options you must specify will differ.
 )";
 
 static constexpr auto examples = R"(
 Examples:
 
-xfr intervals -s example.com -x index_dir -g hg38 -m methylome_name \
+xfr query -s example.com -x index_dir -g hg38 -m methylome_name \
     -o output.bed -i input.bed
 
-xfr intervals -c config_file.toml -g hg38 -m methylome_name \
+xfr query -c config_file.toml -g hg38 -m methylome_name \
     -o output.bed -i input.bed
 
-xfr intervals --local -x index_dir -g hg38 -d methylome_dir \
+xfr query --local -x index_dir -g hg38 -d methylome_dir \
     -m methylome_name -o output.bed -i input.bed
+
+xfr query -x index_dir -g hg38 -s example.com -m SRX012345 \
+    -o output.bed -b 5000
+
+xfr query --local -d methylome_dir -x index_dir -g hg38 \
+    -m methylome_name -o output.bed -b 1000
 )";
 
 #include "arguments.hpp"
@@ -70,14 +77,14 @@ xfr intervals --local -x index_dir -g hg38 -d methylome_dir \
 #include <cstdlib>  // for EXIT_FAILURE, EXIT_SUCCESS
 #include <filesystem>
 #include <format>
-#include <iterator>  // for std::size
+#include <iterator>  // for std::size, for std::cbegin, std::cend
 #include <print>
 #include <ranges>  // for std::views, std::ranges::view...
 #include <string>
 #include <string_view>
 #include <system_error>
 #include <tuple>
-#include <variant>
+#include <variant>  // for std::tuple
 #include <vector>
 
 [[nodiscard]] static inline auto
@@ -91,7 +98,7 @@ format_methylome_names_brief(const std::string &methylome_names)
 
 namespace transferase {
 
-struct intervals_argset : argset_base<intervals_argset> {
+struct query_argset : argset_base<query_argset> {
   static constexpr auto default_config_filename =
     "transferase_client_config.toml";
 
@@ -104,8 +111,6 @@ struct intervals_argset : argset_base<intervals_argset> {
     return std::filesystem::path{config_dir} / default_config_filename;
   }
 
-  static constexpr auto hostname_default{""};
-  static constexpr auto port_default{"5000"};
   static constexpr auto log_level_default{log_level_t::info};
   std::string hostname;
   std::string port;
@@ -115,6 +120,7 @@ struct intervals_argset : argset_base<intervals_argset> {
   transferase::log_level_t log_level{};
 
   bool local_mode{};
+  std::uint32_t bin_size{};
   std::string intervals_file{};
   std::string methylome_names{};
   std::string genome_name{};
@@ -134,6 +140,7 @@ struct intervals_argset : argset_base<intervals_argset> {
         {"log_filename", std::format("{}", log_filename)},
         {"log_level", std::format("{}", log_level)},
         {"local_mode", std::format("{}", local_mode)},
+        {"bin_size", std::format("{}", bin_size)},
         {"methylome_names", format_methylome_names_brief(methylome_names)},
         {"intervals_file", std::format("{}", intervals_file)},
         {"write_scores", std::format("{}", write_scores)},
@@ -155,7 +162,8 @@ struct intervals_argset : argset_base<intervals_argset> {
        po::value(&config_file)->default_value(get_default_config_file(), ""),
        "use specified config file")
       ("local", po::bool_switch(&local_mode), "run in local mode")
-      ("intervals,i", po::value(&intervals_file)->required(), "intervals file")
+      ("bin-size,b", po::value(&bin_size), "size of genomic bins")
+      ("intervals,i", po::value(&intervals_file), "intervals file")
       ("genome,g", po::value(&genome_name)->required(), "genome name")
       ("methylomes,m", po::value(&methylome_names)->required(),
        "methylome names (comma separated)")
@@ -164,16 +172,15 @@ struct intervals_argset : argset_base<intervals_argset> {
        "count covered sites for each interval")
       ("score", po::bool_switch(&write_scores),
        "output weighted methylation in bedgraph format")
-      ("hostname,s", value(&hostname)->default_value(hostname_default),
-       "server hostname")
-      ("port,p", value(&port)->default_value(port_default, ""), "server port")
-      ("methylome-dir,d", value(&methylome_dir)->required(),
+      ("hostname,s", po::value(&hostname), "server hostname")
+      ("port,p", po::value(&port), "server port")
+      ("methylome-dir,d", po::value(&methylome_dir),
        "methylome directory (local mode only)")
-      ("index-dir,x", value(&index_dir)->required(),
+      ("index-dir,x", po::value(&index_dir),
        "genome index directory")
-      ("log-level,v", value(&log_level)->default_value(log_level_default),
+      ("log-level,v", po::value(&log_level)->default_value(log_level_default),
        "{debug, info, warning, error, critical}")
-      ("log-file,l", value(&log_filename)->value_name("[arg]"),
+      ("log-file,l", po::value(&log_filename)->value_name("[arg]"),
        "log file name (defaults: print to screen)")
       // clang-format on
       ;
@@ -182,7 +189,7 @@ struct intervals_argset : argset_base<intervals_argset> {
 };
 
 // clang-format off
-BOOST_DESCRIBE_STRUCT(intervals_argset, (), (
+BOOST_DESCRIBE_STRUCT(query_argset, (), (
   hostname,
   port,
   methylome_dir,
@@ -203,18 +210,135 @@ split_comma(const auto &s) {
          std::ranges::to<std::vector<std::string>>();
 }
 
+/// Read query intervals, check that they are sorted and valid
+[[nodiscard]] static auto
+read_intervals(const transferase::genome_index &index,
+               const std::string &intervals_file, std::error_code &error)
+  -> std::vector<transferase::genomic_interval> {
+  auto &lgr = transferase::logger::instance();
+  const auto intervals =
+    transferase::genomic_interval::read(index, intervals_file, error);
+  if (error) {
+    lgr.error("Error reading intervals file {}: {}", intervals_file, error);
+    return {};
+  }
+  if (!transferase::genomic_interval::are_sorted(intervals)) {
+    lgr.error("Intervals not sorted: {}", intervals_file);
+    error = std::make_error_code(std::errc::invalid_argument);
+    return {};
+  }
+  if (!transferase::genomic_interval::are_valid(intervals)) {
+    lgr.error("Intervals not valid: {} (negative size found)", intervals_file);
+    error = std::make_error_code(std::errc::invalid_argument);
+    return {};
+  }
+  lgr.info("Number of intervals: {}", std::size(intervals));
+  return intervals;
+}
+
+[[nodiscard]] static auto
+do_intervals_query(const auto &args, const transferase::genome_index &index,
+                   const transferase::methylome_interface &interface,
+                   const std::vector<std::string> &methylome_names,
+                   const transferase::request_type_code &request_type) {
+  auto &lgr = transferase::logger::instance();
+  // Read query intervals and validate them
+  std::error_code error;
+  const auto intervals = read_intervals(index, args.intervals_file, error);
+  if (error)
+    return error;  // error message handled already
+
+  // Convert intervals into query
+  const auto format_query_start{std::chrono::high_resolution_clock::now()};
+  auto query = index.make_query(intervals);
+  const auto format_query_stop{std::chrono::high_resolution_clock::now()};
+  lgr.debug("Elapsed time to prepare query: {:.3}s",
+            duration(format_query_start, format_query_stop));
+
+  const auto req = transferase::request{request_type, index.get_hash(),
+                                        std::size(intervals), methylome_names};
+
+  const auto query_start{std::chrono::high_resolution_clock::now()};
+
+  using transferase::level_element_covered_t;
+  using transferase::level_element_t;
+  const auto results =
+    args.count_covered
+      ? interface.get_levels<level_element_covered_t>(req, query, error)
+      : interface.get_levels<level_element_t>(req, query, error);
+  if (error) {
+    lgr.error("Error obtaining levels: {}", error);
+    return error;
+  }
+  const auto query_stop{std::chrono::high_resolution_clock::now()};
+  lgr.debug("Elapsed time for query: {:.3}s",
+            duration(query_start, query_stop));
+
+  const auto outmgr = transferase::intervals_output_mgr{
+    args.output_file, intervals, index, args.write_scores};
+
+  const auto output_start{std::chrono::high_resolution_clock::now()};
+  error = write_output(outmgr, results);
+  const auto output_stop{std::chrono::high_resolution_clock::now()};
+  lgr.debug("Elapsed time for output: {:.3}s",
+            duration(output_start, output_stop));
+  if (error) {
+    lgr.error("Error writing output: {}", error);
+    return error;
+  }
+  return std::error_code{};
+}
+
+[[nodiscard]] static auto
+do_bins_query(const auto &args, const transferase::genome_index &index,
+              const transferase::methylome_interface &interface,
+              const std::vector<std::string> &methylome_names,
+              const transferase::request_type_code &request_type) {
+  auto &lgr = transferase::logger::instance();
+  // Read query intervals and validate them
+  const auto req = transferase::request{request_type, index.get_hash(),
+                                        args.bin_size, methylome_names};
+  std::error_code error;
+  const auto query_start{std::chrono::high_resolution_clock::now()};
+  using transferase::level_element_covered_t;
+  using transferase::level_element_t;
+  const auto results =
+    args.count_covered
+      ? interface.get_levels<level_element_covered_t>(req, index, error)
+      : interface.get_levels<level_element_t>(req, index, error);
+  if (error) {
+    lgr.error("Error obtaining levels: {}", error);
+    return error;
+  }
+  const auto query_stop{std::chrono::high_resolution_clock::now()};
+  lgr.debug("Elapsed time for query: {:.3}s",
+            duration(query_start, query_stop));
+
+  const auto outmgr = transferase::bins_output_mgr{
+    args.output_file, args.bin_size, index, args.write_scores};
+
+  const auto output_start{std::chrono::high_resolution_clock::now()};
+  error = write_output(outmgr, results);
+  const auto output_stop{std::chrono::high_resolution_clock::now()};
+  lgr.debug("Elapsed time for output: {:.3}s",
+            duration(output_start, output_stop));
+  if (error) {
+    lgr.error("Error writing output: {}", error);
+    return error;
+  }
+  return std::error_code{};
+}
+
 auto
-command_intervals_main(
-  int argc, char *argv[])  // NOLINT(cppcoreguidelines-avoid-c-arrays)
-  -> int {
-  static constexpr auto command = "intervals";
-  static const auto usage = std::format("Usage: xfr intervals [options]\n");
+command_query_main(int argc, char *argv[]) -> int {  // NOLINT
+  static constexpr auto command = "query";
+  static const auto usage = std::format("Usage: xfr {} [options]\n", command);
   static const auto about_msg =
     std::format("xfr {}: {}", rstrip(command), rstrip(about));
   static const auto description_msg =
     std::format("{}\n{}", rstrip(description), rstrip(examples));
 
-  transferase::intervals_argset args;
+  transferase::query_argset args;
   const auto ecc = args.parse(argc, argv, usage, about_msg, description_msg);
   if (ecc == argument_error_code::help_requested)
     return EXIT_SUCCESS;
@@ -234,6 +358,19 @@ command_intervals_main(
     lgr.error("Error: local mode requires a methylomes directory");
     return EXIT_FAILURE;
   }
+  if (args.index_dir.empty()) {
+    lgr.error(
+      "Error: specify index directory on command line or in config file");
+    return EXIT_FAILURE;
+  }
+  if (args.bin_size == 0 && args.intervals_file.empty()) {
+    lgr.error("Error: specify either a non-zer bin size or an intervals file");
+    return EXIT_FAILURE;
+  }
+  if (args.bin_size > 0 && !args.intervals_file.empty()) {
+    lgr.error("Error: specify only one of bin size or intervals file");
+    return EXIT_FAILURE;
+  }
 
   std::error_code ec;
   const auto index =
@@ -244,75 +381,26 @@ command_intervals_main(
     return EXIT_FAILURE;
   }
 
-  // Read query intervals and validate them
-  const auto intervals =
-    transferase::genomic_interval::read(index, args.intervals_file, ec);
-  if (ec) {
-    lgr.error("Error reading intervals file: {} ({})", args.intervals_file, ec);
-    return EXIT_FAILURE;
-  }
-  if (!transferase::genomic_interval::are_sorted(intervals)) {
-    lgr.error("Intervals not sorted: {}", args.intervals_file);
-    return EXIT_FAILURE;
-  }
-  if (!transferase::genomic_interval::are_valid(intervals)) {
-    lgr.error("Intervals not valid: {} (negative size found)",
-              args.intervals_file);
-    return EXIT_FAILURE;
-  }
-  lgr.info("Number of intervals: {}", std::size(intervals));
-
-  // Convert intervals into query
-  const auto format_query_start{std::chrono::high_resolution_clock::now()};
-  auto query = index.make_query(intervals);
-  const auto format_query_stop{std::chrono::high_resolution_clock::now()};
-  lgr.debug("Elapsed time to prepare query: {:.3}s",
-            duration(format_query_start, format_query_stop));
-
-  using transferase::request_type_code;
-  const auto request_type = args.count_covered
-                              ? request_type_code::intervals_covered
-                              : request_type_code::intervals;
-
-  const auto methylome_names = split_comma(args.methylome_names);
-  const auto req = transferase::request{request_type, index.get_hash(),
-                                        std::size(intervals), methylome_names};
-
-  const auto resource = transferase::methylome_interface{
+  const auto interface = transferase::methylome_interface{
     .directory = args.local_mode ? args.methylome_dir : std::string{},
     .hostname = args.hostname,
     .port_number = args.port,
   };
-
-  const auto intervals_start{std::chrono::high_resolution_clock::now()};
-
   using transferase::level_element_covered_t;
   using transferase::level_element_t;
-  const auto results =
-    args.count_covered
-      ? resource.get_levels<level_element_covered_t>(req, query, ec)
-      : resource.get_levels<level_element_t>(req, query, ec);
-  if (ec) {
-    lgr.error("Error obtaining levels: {}", ec);
-    return EXIT_FAILURE;
-  }
+  using transferase::request_type_code;
+  const auto methylome_names = split_comma(args.methylome_names);
+  const bool intervals_query = (args.bin_size == 0);
+  const auto request_type =
+    intervals_query ? (args.count_covered ? request_type_code::intervals_covered
+                                          : request_type_code::intervals)
+                    : (args.count_covered ? request_type_code::bins_covered
+                                          : request_type_code::bins);
+  const auto error =
+    intervals_query
+      ? do_intervals_query(args, index, interface, methylome_names,
+                           request_type)
+      : do_bins_query(args, index, interface, methylome_names, request_type);
 
-  const auto intervals_stop{std::chrono::high_resolution_clock::now()};
-  lgr.debug("Elapsed time for query: {:.3}s",
-            duration(intervals_start, intervals_stop));
-
-  const auto outmgr = transferase::intervals_output_mgr{
-    args.output_file, intervals, index, args.write_scores};
-
-  const auto output_start{std::chrono::high_resolution_clock::now()};
-  ec = write_output(outmgr, results);
-  const auto output_stop{std::chrono::high_resolution_clock::now()};
-  lgr.debug("Elapsed time for output: {:.3}s",
-            duration(output_start, output_stop));
-  if (ec) {
-    lgr.error("Error writing output: {}", ec);
-    return EXIT_FAILURE;
-  }
-
-  return EXIT_SUCCESS;
+  return error ? EXIT_FAILURE : EXIT_SUCCESS;
 }
