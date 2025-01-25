2fc230d597de01196626f5cf836eaddcc2129d8a
Andrew D Smith <andrewds@usc.edu>
2025-01-25 14:41:53 -0800

    src/command_config_argset.hpp: added a config variable for a labels dir; dropped the required attribute from the hostname in configuration; added a force_download option for the index files (they don't update by default unless those on the server are newer; added functions to set the default index and labels dirs

--- src/command_config_argset.hpp
+++ src/command_config_argset.hpp
@@ -41,10 +41,20 @@ struct command_config_argset : argset_base<command_config_argset> {
   static constexpr auto default_config_filename =
     "transferase_client_config.toml";
 
+  [[nodiscard]] auto
+  set_index_dir_default() {
+    index_dir = get_index_dir_default();
+  }
+
+  [[nodiscard]] auto
+  set_labels_dir_default() {
+    labels_dir = get_labels_dir_default();
+  }
+
   [[nodiscard]] static auto
   get_default_config_file_impl() -> std::string {
     std::error_code ec;
-    const auto config_dir = get_transferase_config_dir_default(ec);
+    const auto config_dir = get_config_dir_default(ec);
     if (ec)
       return {};
     return std::filesystem::path(config_dir) / default_config_filename;
@@ -53,7 +63,7 @@ struct command_config_argset : argset_base<command_config_argset> {
   [[nodiscard]] static auto
   get_default_config_dir() -> std::string {
     std::error_code ec;
-    const auto config_dir = get_transferase_config_dir_default(ec);
+    const auto config_dir = get_config_dir_default(ec);
     if (ec)
       return {};
     return config_dir;
@@ -66,9 +76,12 @@ struct command_config_argset : argset_base<command_config_argset> {
   std::string index_dir{};
   std::string methylome_dir{};
   std::string log_filename{};
+  std::string labels_dir{};
   std::string genomes{};
   log_level_t log_level{};
+  bool update{};
   bool quiet{};
+  bool force_download{};
 
   auto
   log_options_impl() const {}
@@ -85,18 +98,23 @@ struct command_config_argset : argset_base<command_config_argset> {
       ("config-file,c", value(&config_file)
        ->value_name("[arg]")->default_value(get_default_config_file(), ""),
        "name of configuration file; see help for default")
-      ("hostname,s", value(&hostname)->required(), "transferase server hostname")
+      ("hostname,s", value(&hostname), "transferase server hostname")
       ("port,p", value(&port)->default_value(port_default), "transferase server port")
       ("genomes,g", value(&genomes),
-       "attept to download index files for these genomes (comma separated list, e.g. hg38,mm39)")
-      ("index-dir,x", value(&index_dir)->default_value(get_default_config_dir(), ""),
-       "name of a directory to store genome index files (default: configuration directory)")
+       "download index files and sample labels for these genomes "
+       "(comma separated list, e.g. hg38,mm39)")
+      ("index-dir,x", value(&index_dir),
+       "name of a directory to store genome index files")
       ("methylome-dir,d", value(&methylome_dir),
-       "name of a directory to search for methylomes (default: not specified)")
+       "name of a directory to search for methylomes")
+      ("labels-dir,L", value(&labels_dir),
+       "name of a directory to search for sample labels")
       ("log-level,v", value(&log_level)->default_value(log_level_default, ""),
        "{debug, info, warning, error, critical}")
       ("log-file,l", value(&log_filename)->value_name("console"),
        "log file name")
+      ("update,u", po::bool_switch(&update), "update sample labels")
+      ("force,f", po::bool_switch(&force_download), "force index file update")
       ("quiet,q", po::bool_switch(&quiet), "only print error info to terminal")
       ;
     // clang-format on
@@ -110,6 +128,7 @@ BOOST_DESCRIBE_STRUCT(command_config_argset, (), (
   port,
   index_dir,
   methylome_dir,
+  labels_dir,
   log_filename,
   log_level
 ))
