c80996b1cb98172d942abbde0a7310b2628172f7
Andrew D Smith <andrewds@usc.edu>
2025-01-25 16:13:43 -0800

    src/command_server.cpp: adding the set_hidden_impl function to the arg parser

--- src/command_server.cpp
+++ src/command_server.cpp
@@ -125,6 +125,11 @@ struct server_argset : argset_base<server_argset> {
   }
 
   [[nodiscard]] auto
+  set_hidden_impl() -> boost::program_options::options_description {
+    return {};
+  }
+
+  [[nodiscard]] auto
   set_opts_impl() -> boost::program_options::options_description {
     namespace po = boost::program_options;
     using po::value;
