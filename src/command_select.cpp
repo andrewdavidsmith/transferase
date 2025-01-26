3e8ae90c99d69f43f5e20142b15b010130c71d12
Andrew D Smith <andrewds@usc.edu>
2025-01-25 16:56:39 -0800

    src/command_select.cpp: reworking the interface for better navigation and adding both a multi-add and multi-remove mode and cleaning up the terminal before terminating on signal and outputting in plain text one methylome accession/name per line

--- src/command_select.cpp
+++ src/command_select.cpp
@@ -26,24 +26,24 @@
 #ifdef HAVE_NCURSES
 
 static constexpr auto about = R"(
-manually select methylomes based on metadata
+select methylomes based on metadata related to biological samples
 )";
 
 static constexpr auto description = R"(
 This command interacts with MethBase2 metadata files for experiments,
 allowing methylomes to be selected based on information about the
-associated biological samples. This command uses a textual user
-interface with list navigation. A genome must be specified as
-selection can only be done for one genome at a time. The output file
-format is in JSON but only lists methylome accessions that were
-selected. The purpose of this JSON file is to serve as input for
-transferase queries.
+associated biological samples. This command uses a text-based user
+interface with list navigation. A genome must be specified because the
+selection can only be done for one genome at a time. The selected
+methylomes are output to a text file with one methylome accession per
+line. The purpose of this file is to serve as input for transferase
+queries.
 )";
 
 static constexpr auto examples = R"(
 Examples:
 
-xfr select -o output_file.json -g hg38
+xfr select -o output_file.txt -g hg38
 )";
 
 #include "utilities.hpp"
@@ -57,7 +57,9 @@ xfr select -o output_file.json -g hg38
 #include <algorithm>
 #include <cctype>  // for std::isprint
 #include <cerrno>
+#include <csignal>
 #include <cstdint>
+#include <cstdio>  // for std::getchar
 #include <cstdlib>
 #include <filesystem>
 #include <format>
@@ -67,6 +69,7 @@ xfr select -o output_file.json -g hg38
 #include <map>
 #include <print>
 #include <ranges>
+#include <regex>
 #include <stdexcept>
 #include <string>
 #include <string_view>
@@ -74,8 +77,6 @@ xfr select -o output_file.json -g hg38
 #include <unordered_set>
 #include <utility>  // for std::pair
 #include <vector>
-
-using std::string_literals::operator""s;
 
 [[nodiscard]] auto
 load_data(const std::string &json_filename, std::error_code &error)
@@ -112,7 +113,8 @@ static auto
 mvprintw(const int x, const int y, const std::string &s) {
   const auto ret = mvprintw(x, y, "%s", s.data());  // NOLINT
   if (ret != OK)
-    throw std::runtime_error("Error updating display");
+    throw std::runtime_error(
+      std::format("Error updating display (writing: {})", s));
 }
 
 [[nodiscard]] static inline auto
@@ -140,7 +142,24 @@ do_select(const auto &filtered, const auto cursor_pos, auto &selected_keys) {
 }
 
 static inline auto
+do_add(const auto &filtered, const auto cursor_pos, auto &selected_keys) {
+  const auto curr_fltr = std::cbegin(filtered) + cursor_pos;
+  const auto itr = selected_keys.find(curr_fltr->first);
+  if (itr == std::cend(selected_keys))
+    selected_keys.insert(curr_fltr->first);
+}
+
+static inline auto
+do_remove(const auto &filtered, const auto cursor_pos, auto &selected_keys) {
+  const auto curr_fltr = std::cbegin(filtered) + cursor_pos;
+  const auto itr = selected_keys.find(curr_fltr->first);
+  if (itr != std::cend(selected_keys))
+    selected_keys.erase(curr_fltr->first);
+}
+
+static inline auto
 show_selected_keys(const auto &selected_keys) {
+  using std::string_literals::operator""s;
   clear();
   mvprintw(0, 0, "Selected keys: "s);
   if (selected_keys.empty())
@@ -155,10 +174,13 @@ show_selected_keys(const auto &selected_keys) {
 }
 
 static inline auto
-get_query(std::string &query) {
+get_query(std::string &query, std::regex &query_re) {
   static constexpr auto escape_key_code = 27;
   static constexpr auto enter_key_code = 10;
   query.clear();
+  clear();
+  mvprintw(1, 0, std::format("Search Query: {}", query));
+  refresh();
   while (true) {
     const auto query_ch = getch();
     if (query_ch == escape_key_code)
@@ -171,16 +193,42 @@ get_query(std::string &query) {
     mvprintw(1, 0, std::format("Search Query: {}", query));
     refresh();
   }
+  query_re = std::regex(query, std::regex_constants::icase);
+}
+
+[[nodiscard]] static inline auto
+confirm_quit(const auto &selected_keys) -> bool {
+  using std::string_literals::operator""s;
+  const auto message = std::format("Quit and save selection ({} items)? [y/n]",
+                                   std::size(selected_keys));
+  int confirmation{};
+  while (std::tolower(confirmation) != 'y' &&
+         std::tolower(confirmation) != 'n') {
+    erase();
+    mvprintw(0, 0, message);
+    refresh();
+    confirmation = std::getchar();
+  }
+  erase();
+  mvprintw(0, 0, message + std::string(" {}", confirmation));
+  refresh();
+  return (confirmation == 'y' || confirmation == 'Y');
 }
 
 auto
 main_loop(const std::vector<std::pair<std::string, std::string>> &data)
   -> std::vector<std::string> {
+  static constexpr auto extra_margin_space = 3;
   static constexpr auto escape_key_code = 27;
   static constexpr auto escape_delay{25};
-  static constexpr std::int32_t margin{5};  // should be max key width plus room
   static constexpr auto legend =
-    "q=Quit, Move=Arrows, p=Toggle multi-select, Space=Add/Remove, d=Display";
+    "q=Quit, Move=Arrows, a=Toggle multi-add, r=Toggle multi-remove, "
+    "Space=Add/Remove, v=View selected, Line={}/{}";
+
+  // margin must be max key width plus room
+  const auto key_sizes = std::views::transform(
+    data, [](const auto &s) -> std::int32_t { return std::size(s.first); });
+  const std::int32_t margin = std::ranges::max(key_sizes) + extra_margin_space;
 
   // Initialize ncurses
   initscr();
@@ -199,28 +247,23 @@ main_loop(const std::vector<std::pair<std::string, std::string>> &data)
 
   std::unordered_set<std::string> selected_keys;
   std::string query;
-  bool multi_mode = false;
+  std::regex query_re;
+  bool multi_add = false;
+  bool multi_remove = false;
   std::int32_t horiz_pos = 0;
   std::int32_t cursor_pos = 0;
   std::int32_t legend_height = 1;  // lines to keep at top of display
   std::vector<std::pair<std::string, std::string>> filtered;
 
   while (true) {
-    clear();
-
-    // Display the legend and query
-    if (query.empty())
-      mvprintw(0, 0, std::string{legend});
-    else
-      mvprintw(0, 0, legend + std::format("[{}]", query));
-
     // Filter data based on the query
-    const auto query_found = [&](const auto &x) {
+    const auto query_found = [&](const auto &x) -> bool {
       // ADS: need to take care of upper/lower case here
-      return x.second.find(query) != std::string::npos;
+      return std::regex_search(x.second, query_re);
     };
     filtered.clear();
-    std::ranges::copy_if(data, std::back_inserter(filtered), query_found);
+    if (!query.empty())
+      std::ranges::copy_if(data, std::back_inserter(filtered), query_found);
 
     if (filtered.empty()) {
       query.clear();
@@ -231,12 +274,23 @@ main_loop(const std::vector<std::pair<std::string, std::string>> &data)
     const std::int32_t n_filtered = std::size(filtered);
     // NOLINTEND (cppcoreguidelines-narrowing-conversions)
 
+    // Include the query in the legened if appropriate
+    const std::string current_legend =
+      query.empty() ? std::format(legend, cursor_pos + 1, n_filtered)
+                    : std::format(legend, cursor_pos + 1, n_filtered) +
+                        std::format(" [{}]", query);
+
     const auto disp_start =
       std::max(0, std::min(n_filtered + legend_height - LINES,
                            std::max(0, cursor_pos - LINES / 2)));
     const auto disp_end = std::min(n_filtered, disp_start + LINES);
 
     const auto to_show = get_to_show(filtered, disp_start, disp_end);
+
+    // Clear to prepare for redraw and display the legend
+    erase();  // performs better than using clear();
+    mvprintw(0, 0, current_legend);
+
     for (const auto [idx, entry] : std::views::enumerate(to_show)) {
       // Calculate the global data index
       const auto data_idx = disp_start + idx;
@@ -250,7 +304,7 @@ main_loop(const std::vector<std::pair<std::string, std::string>> &data)
 
       // highlight this item if it's at the cursor position
       if (data_idx == cursor_pos) {
-        if (multi_mode)
+        if (multi_add || multi_remove)
           attron(COLOR_PAIR(4));
         else
           attron(COLOR_PAIR(2));
@@ -269,16 +323,12 @@ main_loop(const std::vector<std::pair<std::string, std::string>> &data)
     // Handle user input
     const auto ch = getch();
     if (ch == 'q') {
-      if (query.empty())
+      if (confirm_quit(selected_keys))
         break;
-      else
-        query.clear();
     }
     else if (ch == escape_key_code) {  // ESC key to reset query
-      if (query.empty())
-        break;
-      else
-        query.clear();
+      query.clear();
+      cursor_pos = 0;
     }
     else if (ch == KEY_RIGHT) {  // Scroll right
       // NOLINTBEGIN (cppcoreguidelines-narrowing-conversions)
@@ -287,32 +337,84 @@ main_loop(const std::vector<std::pair<std::string, std::string>> &data)
       if (margin + width > COLS)
         horiz_pos = std::min(horiz_pos + 1, (margin + width) - COLS);
     }
-    else if (ch == KEY_LEFT)  // Scroll left
+    else if (ch == KEY_LEFT) {  // Scroll left
       horiz_pos = std::max(horiz_pos - 1, 0);
-
+    }
     else if (ch == KEY_DOWN) {
       cursor_pos = (cursor_pos + 1) % n_filtered;
-      if (multi_mode)
-        do_select(filtered, cursor_pos, selected_keys);
+      if (multi_add)
+        do_add(filtered, cursor_pos, selected_keys);
+      if (multi_remove)
+        do_remove(filtered, cursor_pos, selected_keys);
+    }
+    else if (ch == KEY_NPAGE) {
+      std::int32_t max_down =
+        std::min(cursor_pos + LINES - legend_height, n_filtered - 1);
+      if (multi_add)
+        for (std::int32_t i = cursor_pos; i < max_down; ++i)
+          do_add(filtered, i, selected_keys);
+      if (multi_remove)
+        for (std::int32_t i = cursor_pos; i < max_down; ++i)
+          do_remove(filtered, i, selected_keys);
+      cursor_pos = max_down;
+    }
+    else if (ch == KEY_END) {
+      if (multi_add)
+        for (std::int32_t i = cursor_pos; i < n_filtered; ++i)
+          do_add(filtered, i, selected_keys);
+      if (multi_remove)
+        for (std::int32_t i = cursor_pos; i < n_filtered; ++i)
+          do_remove(filtered, i, selected_keys);
+      cursor_pos = n_filtered - 1;
     }
     else if (ch == KEY_UP) {
       cursor_pos = (cursor_pos - 1 + n_filtered) % n_filtered;
-      if (multi_mode)
-        do_select(filtered, cursor_pos, selected_keys);
+      if (multi_add)
+        do_add(filtered, cursor_pos, selected_keys);
+      if (multi_remove)
+        do_remove(filtered, cursor_pos, selected_keys);
     }
-    else if (ch == ' ' || ch == '\n')  // Select/deselect current item
+    else if (ch == KEY_PPAGE) {
+      std::int32_t max_up = std::max(cursor_pos - (LINES - legend_height), 0);
+      if (multi_add)
+        for (std::int32_t i = cursor_pos; i >= max_up; --i)
+          do_add(filtered, i, selected_keys);
+      if (multi_remove)
+        for (std::int32_t i = cursor_pos; i >= max_up; --i)
+          do_remove(filtered, i, selected_keys);
+      cursor_pos = max_up;
+    }
+    else if (ch == KEY_HOME) {
+      if (multi_add)
+        for (std::int32_t i = cursor_pos; i >= 0; --i)
+          do_add(filtered, i, selected_keys);
+      if (multi_remove)
+        for (std::int32_t i = cursor_pos; i >= 0; --i)
+          do_remove(filtered, i, selected_keys);
+      cursor_pos = 0;
+    }
+    else if (ch == ' ' || ch == '\n') {  // Select/deselect current item
       do_select(filtered, cursor_pos, selected_keys);
-
-    else if (ch == 'd')  // Display selected keys
-      show_selected_keys(selected_keys);
-
-    else if (ch == 'p') {  // Toggle multi-select mode
-      multi_mode = !multi_mode;
-      if (multi_mode)
-        do_select(filtered, cursor_pos, selected_keys);
     }
-    else if (ch == '/')  // Start search query
-      get_query(query);
+    else if (ch == 'v') {  // Display selected keys
+      show_selected_keys(selected_keys);
+    }
+    else if (ch == 'a') {  // Toggle multi-select mode
+      multi_add = !multi_add;
+      multi_remove = false;
+      if (multi_add)
+        do_add(filtered, cursor_pos, selected_keys);
+    }
+    else if (ch == 'r') {  // Toggle multi-select mode
+      multi_remove = !multi_remove;
+      multi_add = false;
+      if (multi_remove)
+        do_remove(filtered, cursor_pos, selected_keys);
+    }
+    else if (ch == '/') {  // Start search query
+      get_query(query, query_re);
+      cursor_pos = 0;
+    }
   }
 
   endwin();  // End ncurses session
@@ -325,9 +427,29 @@ write_output(const auto &data, std::string &json_filename) -> std::error_code {
   std::ofstream out(json_filename);
   if (!out)
     return std::make_error_code(std::errc(errno));
-  if (!(out << boost::json::value_from(data)))
-    return std::make_error_code(std::errc(errno));
+  for (const auto &d : data)
+    std::println(out, "{}", d);
   return {};
+}
+
+static auto
+throwing_handler(int sig) {
+  clear();
+  refresh();
+  endwin();
+  throw std::runtime_error(std::format("received signal: {}", sig));
+}
+
+/// Register all signals that could disrupt the curses and leave the
+/// terminal in a bad state.
+static auto
+register_signals() {
+  (void)std::signal(SIGTERM, throwing_handler);
+  (void)std::signal(SIGSEGV, throwing_handler);
+  (void)std::signal(SIGINT, throwing_handler);
+  (void)std::signal(SIGINT, throwing_handler);
+  (void)std::signal(SIGABRT, throwing_handler);
+  (void)std::signal(SIGFPE, throwing_handler);
 }
 
 auto
@@ -352,7 +474,8 @@ command_select_main(int argc, char *argv[]) -> int {  // NOLINT
     ("help,h", "print this message and exit")
     ("genome,g", po::value(&genome_name)->required(), "use this genome (required)")
     ("output,o", po::value(&output_file)->required(), "output file (required)")
-    ("input,i", po::value(&input_file), "specify an input file")
+    ("input,i", po::value(&input_file),
+     "specify an input file")
     // clang-format on
     ;
   po::variables_map vm;
@@ -374,28 +497,34 @@ command_select_main(int argc, char *argv[]) -> int {  // NOLINT
     return EXIT_FAILURE;
   }
 
+  if (input_file.empty())
+    input_file = (std::filesystem::path{transferase::get_labels_dir_default()} /
+                  std::format("{}.json", genome_name))
+                   .string();
+
   try {
-    std::error_code error;
+    std::error_code error{};
     const auto data = load_data(input_file, error);
     if (error)
       throw std::runtime_error(
         std::format("Error reading input {}: {}", input_file, error.message()));
 
-    std::println(
-      "Type g then Enter to go to selection. Any other key to exit.");
-    const auto user_input = std::cin.get();
-    if (user_input != 'g') {
+    std::println("Number of items loaded: {}", std::size(data));
+    std::print("Type 'g' then Enter to proceed. Any other key to exit. ");
+    if (std::cin.get() != 'g') {
       std::println("Terminating on user request");
       return EXIT_SUCCESS;
     }
+    // ADS: register the signals so a handler can properly reset the
+    // terminal on exit
+    register_signals();
     const auto selected = main_loop(data);
-    if (!selected.empty()) {
+    if (!selected.empty())
       if (error = write_output(selected, output_file); error) {
         std::println("Error writing output {}: {}", output_file,
                      error.message());
         return EXIT_FAILURE;
       }
-    }
   }
   catch (std::runtime_error &e) {
     std::println("{}", e.what());
