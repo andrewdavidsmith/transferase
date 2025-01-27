cab68bc1a1e01e7598fc5af95db83e6da6aa220e
Andrew D Smith <andrewds@usc.edu>
2025-01-26 17:44:12 -0800

    src/command_config.cpp: refactoring to create a class for client_config that will allow the client configuration to be exposed as an object in APIs

--- src/command_config.cpp
+++ src/command_config.cpp
@@ -42,18 +42,18 @@ for the underlying biological samples.
 static constexpr auto examples = R"(
 Examples:
 
-xfr config -c my_config_file.toml -s example.com -p 5009 --genomes hg38,mm39
+xfr config -c my_config_dir -s example.com -p 5009 --genomes hg38,mm39
 )";
 
 #include "arguments.hpp"
 #include "client_config.hpp"
 #include "command_config_argset.hpp"
-#include "config_file_utils.hpp"  // write_config_file
 #include "download.hpp"
 #include "find_path_to_binary.hpp"
 #include "format_error_code.hpp"  // IWYU pragma: keep
 #include "genome_index_data.hpp"
 #include "genome_index_metadata.hpp"
+#include "logger.hpp"
 #include "utilities.hpp"
 
 #include <config.h>  // for VERSION, DATADIR, PROJECT_NAME
@@ -61,18 +61,14 @@ xfr config -c my_config_file.toml -s example.com -p 5009 --genomes hg38,mm39
 #include <boost/describe.hpp>  // for BOOST_DESCRIBE_STRUCT
 #include <boost/json.hpp>
 
-#include <algorithm>
 #include <cerrno>   // for errno
 #include <chrono>   // for std::chrono::operator-
 #include <cstdlib>  // for EXIT_FAILURE, EXIT_SUCCESS
 #include <filesystem>
 #include <format>
 #include <fstream>
-#include <iostream>
 #include <iterator>  // for std::cbegin
-#include <memory>    // std::make_shared
 #include <print>
-#include <ranges>
 #include <string>
 #include <string_view>
 #include <system_error>
@@ -114,19 +110,19 @@ get_remote_data_resources()
   -> std::tuple<std::vector<remote_data_resources>, std::error_code> {
   // ADS: this function prints error messages itself, so no need to
   // log based on error code returned
-
   static const auto exe_path = find_path_to_binary();
+  auto &lgr = transferase::logger::instance();
 
   std::error_code ec;
   const auto exe_dir_parent =
     std::filesystem::canonical(exe_path).parent_path().parent_path().string();
   bool is_dir = std::filesystem::is_directory(exe_dir_parent, ec);
   if (ec) {
-    std::println("Error: {} ({})", ec, exe_dir_parent);
+    lgr.error("Error: {} ({})", ec, exe_dir_parent);
     return {{}, ec};
   }
   if (!is_dir) {
-    std::println("Not a directory: {}", exe_dir_parent);
+    lgr.error("Not a directory: {}", exe_dir_parent);
     return {{}, std::make_error_code(std::errc::not_a_directory)};
   }
 
@@ -136,11 +132,11 @@ get_remote_data_resources()
   const auto data_dir = exe_dir_parent / data_path / std::string(PROJECT_NAME);
   is_dir = std::filesystem::is_directory(data_dir, ec);
   if (ec) {
-    std::println("Error: {} ({})", ec, data_dir.string());
+    lgr.error("Error: {} ({})", ec, data_dir.string());
     return {{}, ec};
   }
   if (!is_dir) {
-    std::println("Not a directory: {}", data_dir.string());
+    lgr.error("Not a directory: {}", data_dir.string());
     return {{}, std::make_error_code(std::errc::not_a_directory)};
   }
 
@@ -154,21 +150,21 @@ get_remote_data_resources()
   const auto filesize =
     static_cast<std::streamsize>(std::filesystem::file_size(json_file, ec));
   if (ec) {
-    std::println("Bad system config file: {}", json_file.string());
+    lgr.error("Bad system config file: {}", json_file.string());
     return {{}, ec};
   }
 
   std::string payload(filesize, '\0');
   if (!in.read(payload.data(), filesize)) {
-    std::println("Failure reading file: {}", json_file.string());
+    lgr.error("Failure reading file: {}", json_file.string());
     return {{}, std::make_error_code(std::errc(errno))};
   }
 
   std::vector<remote_data_resources> resources;
   boost::json::parse_into(resources, payload, ec);
   if (ec) {
-    std::println("Malformed JSON for remote resources {}: {}",
-                 json_file.string(), ec);
+    lgr.error("Malformed JSON for remote resources {}: {}", json_file.string(),
+              ec);
     return {{}, ec};
   }
   return std::make_tuple(std::move(resources), std::error_code{});
@@ -176,13 +172,14 @@ get_remote_data_resources()
 
 [[nodiscard]] static auto
 dl_err(const auto &hdr, const auto &ec, const auto &url) -> std::error_code {
-  std::println("Error downloading {}: ", url);
+  auto &lgr = transferase::logger::instance();
+  lgr.error("Error downloading {}: ", url);
   if (ec)
-    std::println("Error code: {}", ec);
+    lgr.error("Error code: {}", ec);
   const auto status_itr = hdr.find("Status");
   const auto reason_itr = hdr.find("Reason");
   if (status_itr != std::cend(hdr) && reason_itr != std::cend(hdr))
-    std::println("HTTP status: {} {}", status_itr->second, reason_itr->second);
+    lgr.error("HTTP status: {} {}", status_itr->second, reason_itr->second);
   return ec;
 }
 
@@ -206,19 +203,20 @@ check_is_outdated(const download_request &dr,
 }
 
 [[nodiscard]] static auto
-get_index_files(const bool quiet, const remote_data_resources &remote,
-                const std::string &genomes, const std::string &dirname,
+get_index_files(const remote_data_resources &remote,
+                const std::vector<std::string> &genomes,
+                const std::string &dirname,
                 const bool force_download) -> std::error_code {
-  for (const auto genome : std::views::split(genomes, ',')) {
-    const auto assem = std::string{std::cbegin(genome), std::cend(genome)};
-    const auto stem = remote.form_index_target_stem(assem);
+  auto &lgr = transferase::logger::instance();
+  for (const auto &genome : genomes) {
+    const auto stem = remote.form_index_target_stem(genome);
 
     const auto data_file =
       std::format("{}{}", stem, genome_index_data::filename_extension);
 
     const download_request dr{remote.host, remote.port, data_file, dirname};
     const auto local_index_file =
-      std::filesystem::path{dirname} / std::format("{}.cpg_idx", assem);
+      std::filesystem::path{dirname} / std::format("{}.cpg_idx", genome);
 
     std::error_code ec;
     const bool is_outdated = check_is_outdated(dr, local_index_file, ec);
@@ -226,8 +224,7 @@ get_index_files(const bool quiet, const remote_data_resources &remote,
       return ec;
 
     if (is_outdated || force_download) {
-      if (!quiet)
-        std::println("Download: {} to {}", remote.form_url(data_file), dirname);
+      lgr.info("Download: {} to \"{}\"", remote.form_url(data_file), dirname);
 
       const auto [data_hdr, data_err] =
         download({remote.host, remote.port, data_file, dirname});
@@ -236,38 +233,136 @@ get_index_files(const bool quiet, const remote_data_resources &remote,
 
       const auto meta_file =
         std::format("{}{}", stem, genome_index_metadata::filename_extension);
-      if (!quiet)
-        std::println("Download: {} to {}", remote.form_url(meta_file), dirname);
-      const auto [meta_hdr, meta_err] =
-        download({remote.host, remote.port, meta_file, dirname});
+      lgr.info("Download: {} to \"{}\"", remote.form_url(meta_file), dirname);
+      const auto [meta_hdr, meta_err] = download({
+        remote.host,
+        remote.port,
+        meta_file,
+        dirname,
+      });
       if (meta_err)
         return dl_err(meta_hdr, meta_err, remote.form_url(meta_file));
     }
-    else if (!force_download) {
-      if (!quiet)
-        std::println("Existing index is most recent: {}",
-                     local_index_file.string());
-    }
+    else if (!force_download)
+      lgr.info("Existing index is most recent: {}", local_index_file.string());
   }
   return {};
 }
 
 [[nodiscard]] static auto
-get_labels_files(const bool quiet, const remote_data_resources &remote,
-                 const std::string &genomes,
+get_labels_files(const remote_data_resources &remote,
+                 const std::vector<std::string> &genomes,
                  const std::string &dirname) -> std::error_code {
-  for (const auto genome : std::views::split(genomes, ',')) {
-    const auto assem = std::string{std::cbegin(genome), std::cend(genome)};
-    const auto stem = remote.form_labels_target_stem(assem);
+  auto &lgr = transferase::logger::instance();
+  for (const auto &genome : genomes) {
+    const auto stem = remote.form_labels_target_stem(genome);
     const auto labels_file = std::format("{}.json", stem);
-    if (!quiet)
-      std::println("Download: {} to {}", remote.form_url(labels_file), dirname);
-    const auto [data_hdr, data_err] =
-      download({remote.host, remote.port, labels_file, dirname});
+    lgr.info("Download: {} to {}", remote.form_url(labels_file), dirname);
+    const auto [data_hdr, data_err] = download(download_request{
+      remote.host,
+      remote.port,
+      labels_file,
+      dirname,
+    });
     if (data_err)
       return dl_err(data_hdr, data_err, remote.form_url(labels_file));
   }
   return {};
+}
+
+[[nodiscard]] static auto
+configure_client(const std::string &config_dir, const std::string &log_file,
+                 const transferase::log_level_t log_level,
+                 const std::string &index_dir, const std::string &labels_dir,
+                 const std::vector<std::string> &genomes,
+                 const bool force_download) -> std::error_code {
+  auto &lgr = transferase::logger::instance();
+
+  using transferase::client_config;
+  client_config config{};
+  std::error_code error;
+
+  config.init_config_dir(config_dir, error);
+  if (error) {
+    lgr.error("Failed to set config dir {}: {}", config_dir, error);
+    return error;
+  }
+
+  const std::string config_file_empty{};
+  config.init_config_file(config_file_empty, error);
+  if (error) {
+    lgr.error("Failed to set config file: {}", error);
+    return error;
+  }
+  config.log_level = log_level;
+  config.init_log_file(log_file, error);
+  if (error) {
+    lgr.error("Failed to set log file: {}", error);
+    return error;
+  }
+
+  // Setup the indexes dir and the labels dir whether or not the user
+  // has specified genomes to configure. This must be done prior to
+  // writing the config file or attempting to download any config
+  // data.
+  config.init_index_dir(index_dir, error);
+  if (error) {
+    lgr.error("Failed to set index directory {}: {}", config.index_dir, error);
+    return error;
+  }
+  config.init_labels_dir(labels_dir, error);
+  if (error) {
+    lgr.error("Failed to set labels directory {}: {}", config.labels_dir,
+              error);
+    return error;
+  }
+
+  lgr.info("Making config directories");
+  error = config.make_directories();
+  if (error) {
+    lgr.error("Failure creating directories: {}", error);
+    return error;
+  }
+
+  lgr.info("Writing configuration file");
+  error = config.write();
+  if (error) {
+    lgr.error("Error writing config file: {}", error);
+    return error;
+  }
+
+  if (!genomes.empty()) {
+    // Only attempt downloads if the user specifies one or more
+    // genomes. And if the user specifies genomes, but no index_dir
+    // or labels_dir, then make the defaults.
+    const auto [remotes, remote_err] = transferase::get_remote_data_resources();
+    if (remote_err) {
+      lgr.error("Error identifying remote server: {}", remote_err);
+      return remote_err;
+    }
+
+    // Do the downloads, attempting whatever remote resources are
+    // available
+    for (const auto &remote : remotes) {
+      lgr.info("Host for data files: {}:{}", remote.host, remote.port);
+      const auto index_err =
+        get_index_files(remote, genomes, config.index_dir, force_download);
+      if (index_err)
+        lgr.error("Error obtaining cpg index files: {}", index_err);
+
+      const auto labels_err =
+        get_labels_files(remote, genomes, config.labels_dir);
+      if (labels_err)
+        lgr.error("Error obtaining labels files: {}", labels_err);
+
+      // ADS: this is broken -- there is no proper check for whether a
+      // subsequent server should be attempted.
+      if (!index_err && !labels_err)
+        break;  // quit if we got what we want
+    }
+  }
+
+  return std::error_code{};
 }
 
 }  // namespace transferase
@@ -294,115 +389,38 @@ command_config_main(int argc,
   static const auto description_msg =
     std::format("{}\n{}", rstrip(description), rstrip(examples));
 
-  transferase::command_config_argset args;
+  namespace xfr = transferase;
+
+  xfr::command_config_argset args;
   auto ec = args.parse(argc, argv, usage, about_msg, description_msg);
   if (ec == argument_error_code::help_requested)
     return EXIT_SUCCESS;
   if (ec)
     return EXIT_FAILURE;
 
-  std::shared_ptr<std::ostream> log_file =
-    args.log_filename.empty()
-      ? std::make_shared<std::ostream>(std::cout.rdbuf())
-      : std::make_shared<std::ofstream>(args.log_filename, std::ios::app);
-
-  if (!args.quiet)
-    std::ranges::for_each(
-      std::vector<std::tuple<std::string, std::string>>{
-        // clang-format off
-        {"config_file", std::format("{}", args.config_file)},
-        {"hostname", std::format("{}", args.hostname)},
-        {"port", std::format("{}", args.port)},
-        {"log_filename", std::format("{}", args.log_filename)},
-        {"log_level", std::format("{}", args.log_level)},
-        // clang-format on
-      },
-      [](auto &&x) { std::println("{}: {}", std::get<0>(x), std::get<1>(x)); });
-
-  using transferase::client_config;
-  client_config config{};
-
-  if (args.config_file.empty()) {
-    config.config_file = client_config::get_config_file_default(ec);
-  }
-  else
-    ec = config.set_config_file(args.config_file);
-  if (ec) {
-    std::println("Bad config file {}: {}", config.config_file, ec);
+  auto &lgr = xfr::logger::instance(xfr::shared_from_cout(), command,
+                                    args.quiet ? xfr::log_level_t::error
+                                               : xfr::log_level_t::info);
+  if (!lgr) {
+    std::println("Failure initializing logging: {}.", lgr.get_status());
     return EXIT_FAILURE;
   }
 
-  if (!args.quiet)
-    std::println("Creating needed directories");
+  args.log_options();
 
-  ec = config.make_directories();
-  if (ec) {
-    std::println("Failure creating directories: {}", ec);
+  const auto genomes = split_comma(args.genomes);
+
+  // clang-format off
+  const auto error = configure_client(
+    args.config_dir,
+    args.log_file,
+    args.log_level,
+    args.index_dir,
+    args.labels_dir,
+    genomes,
+    args.force_download);
+  // clang-format on
+  if (error)
     return EXIT_FAILURE;
-  }
-
-  // Setup the indexes dir and the labels dir if the user has
-  // specified genomes to configure but not given locations for the
-  // files. This must be done prior to writing the config file.
-  if (!args.genomes.empty()) {
-    if (args.index_dir.empty()) {
-      config.index_dir = client_config::get_index_dir_default(ec);
-      if (ec) {
-        std::println("Failure setting default indexes dir: {}", ec);
-        return EXIT_FAILURE;
-      }
-    }
-    else
-      config.index_dir = args.index_dir;
-
-    if (args.labels_dir.empty()) {
-      config.labels_dir = client_config::get_labels_dir_default(ec);
-      if (ec) {
-        std::println("Failure setting default labels dir: {}", ec);
-        return EXIT_FAILURE;
-      }
-    }
-    else
-      config.labels_dir = args.labels_dir;
-  }
-
-  const auto config_write_err = write_config_file(args);
-  if (config_write_err) {
-    std::println("Error writing config file: {}", config_write_err);
-    return EXIT_FAILURE;
-  }
-
-  if (!args.genomes.empty()) {
-    // Only attempt downloads if the user specifies one or more
-    // genomes. And if the user specifies genomes, but no index_dir
-    // or labels_dir, then make the defaults.
-    const auto [remotes, remote_err] = transferase::get_remote_data_resources();
-    if (remote_err) {
-      std::println("Error identifying remote server: {}", remote_err);
-      return EXIT_FAILURE;
-    }
-
-    // Do the downloads, attempting whatever remote resources are
-    // available
-    for (const auto &remote : remotes) {
-      if (!args.quiet)
-        std::println("Host for data files: {}:{}", remote.host, remote.port);
-      const auto index_err =
-        get_index_files(args.quiet, remote, args.genomes, config.index_dir,
-                        args.force_download);
-      if (index_err)
-        std::println("Error obtaining cpg index files: {}", index_err);
-
-      const auto labels_err =
-        get_labels_files(args.quiet, remote, args.genomes, config.labels_dir);
-      if (labels_err)
-        std::println("Error obtaining labels files: {}", labels_err);
-
-      // ADS: this is broken -- there is no proper check for whether a
-      // subsequent server should be attempted.
-      if (!index_err && !labels_err)
-        break;  // quit if we got what we want
-    }
-  }
   return EXIT_SUCCESS;
 }
