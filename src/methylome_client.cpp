b6c2d89946b08d742ced8553906877e7c28651fd
Andrew D Smith <andrewds@usc.edu>
2025-02-02 11:21:34 -0800

    src/methylome_client.hcpp: adding a cpp file with a function to get default settings from a config file. Renaming the name server to client because it will make more sense for API users

---
+++ src/methylome_client.cpp
@@ -0,0 +1,113 @@
+/* MIT License
+ *
+ * Copyright (c) 2025 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "methylome_client.hpp"
+
+#include "client_config.hpp"
+
+#include <cctype>
+#include <fstream>
+#include <ranges>
+#include <string>
+#include <system_error>
+#include <tuple>
+
+[[nodiscard]] inline auto
+rlstrip(const std::string &s) noexcept -> std::string {
+  constexpr auto is_graph = [](const char c) { return std::isgraph(c); };
+  if (s.empty())
+    return {};
+  const auto start = std::ranges::find_if(s, is_graph);
+  auto stop = std::begin(std::ranges::find_last_if(s, is_graph));
+  if (stop != std::cend(s))
+    ++stop;
+  return std::string(start, stop);
+}
+
+namespace transferase {
+
+[[nodiscard]] auto
+split_equals(const std::string &line, std::error_code &error) noexcept
+  -> std::tuple<std::string, std::string> {
+  static constexpr auto delim = '=';
+  const auto eq_pos = line.find(delim);
+  if (eq_pos >= std::size(line)) {
+    error = methylome_client_error_code::error_reading_config_file;
+    return {std::string{}, std::string{}};
+  }
+  const std::string key{rlstrip(line.substr(0, eq_pos))};
+  const std::string value{rlstrip(line.substr(eq_pos + 1))};
+  if (std::size(key) == 0 || std::size(value) == 0) {
+    error = methylome_client_error_code::error_reading_config_file;
+    return {std::string{}, std::string{}};
+  }
+
+  return {key, value};
+}
+
+[[nodiscard]] auto
+methylome_client::get_default(std::error_code &error) noexcept
+  -> methylome_client {
+
+  const auto config_file = client_config::get_config_file_default(error);
+  if (error)
+    // error from get_config_file_default
+    return {};
+
+  // read the config file
+  std::ifstream in(config_file);
+  if (!in) {
+    error = std::make_error_code(std::errc(errno));
+    return {};
+  }
+
+  std::string hostname;
+  std::string port_number;
+
+  std::string line;
+  while (getline(in, line)) {
+    line = rlstrip(line);
+    if (line[0] == '#')
+      continue;
+    const auto [key, value] = split_equals(line, error);
+    if (error)
+      return {};
+    if (key == "hostname")
+      hostname = value;
+    if (key == "port")
+      port_number = value;
+  }
+
+  if (hostname.empty() || port_number.empty()) {
+    error = methylome_client_error_code::required_config_values_not_found;
+    return {};
+  }
+
+  methylome_client ms;
+  ms.hostname = hostname;
+  ms.port_number = port_number;
+
+  return ms;
+}
+
+}  // namespace transferase
---
+++ src/methylome_client.hpp
@@ -0,0 +1,188 @@
+/* MIT License
+ *
+ * Copyright (c) 2025 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SRC_METHYLOME_CLIENT_HPP_
+#define SRC_METHYLOME_CLIENT_HPP_
+
+#include "client.hpp"
+#include "genome_index.hpp"
+#include "methylome.hpp"
+#include "request.hpp"
+
+#include <boost/json.hpp>
+
+#include <string>
+#include <vector>
+
+namespace transferase {
+
+class methylome_client {
+public:
+  std::string hostname;
+  std::string port_number;
+  std::uint64_t index_hash{};
+
+  [[nodiscard]] static auto
+  get_default(std::error_code &error) noexcept -> methylome_client;
+
+#ifndef TRANSFERASE_NOEXCEPT
+  [[nodiscard]] static auto
+  get_default() -> methylome_client {
+    std::error_code error;
+    auto result = get_default(error);
+    if (error)
+      throw std::system_error(error);
+    return result;
+  }
+#endif
+
+  [[nodiscard]] auto
+  tostring() const noexcept -> std::string {
+    std::ostringstream o;
+    if (!(o << boost::json::value_from(*this)))
+      o.clear();
+    return o.str();
+  }
+
+  // intervals: takes a query
+  template <typename lvl_elem_t>
+  [[nodiscard]] auto
+  get_levels(const std::vector<std::string> &methylome_names,
+             const query_container &query, std::error_code &error)
+    const noexcept -> std::vector<level_container<lvl_elem_t>> {
+    static constexpr auto REQ_TYPE = request_type_code::intervals;
+    const auto req =
+      request{REQ_TYPE, index_hash, size(query), methylome_names};
+    return get_levels_impl<lvl_elem_t>(req, query, error);
+  }
+
+#ifndef TRANSFERASE_NOEXCEPT
+  // intervals: takes a query (throws)
+  template <typename lvl_elem_t>
+  auto
+  get_levels(const std::vector<std::string> &methylome_names,
+             const query_container &query) const
+    -> std::vector<level_container<lvl_elem_t>> {
+    std::error_code error;
+    auto result = get_levels<lvl_elem_t>(methylome_names, query, error);
+    if (error)
+      throw std::system_error(error);
+    return result;
+  }
+#endif
+
+  // bins: takes an index
+  template <typename lvl_elem_t>
+  [[nodiscard]] auto
+  get_levels(const std::vector<std::string> &methylome_names,
+             const std::uint32_t bin_size, std::error_code &error)
+    const noexcept -> std::vector<level_container<lvl_elem_t>> {
+    static constexpr auto REQ_TYPE = request_type_code::bins;
+    const auto req = request{REQ_TYPE, index_hash, bin_size, methylome_names};
+    return get_levels_impl<lvl_elem_t>(req, error);
+  }
+
+#ifndef TRANSFERASE_NOEXCEPT
+  // bins: takes an index (throws)
+  template <typename lvl_elem_t>
+  auto
+  get_levels(const std::vector<std::string> &methylome_names,
+             const std::uint32_t bin_size) const
+    -> std::vector<level_container<lvl_elem_t>> {
+    std::error_code error;
+    auto result = get_levels<lvl_elem_t>(methylome_names, bin_size, error);
+    if (error)
+      throw std::system_error(error);
+    return result;
+  }
+#endif
+
+private:
+  template <typename lvl_elem_t>
+  [[nodiscard]] auto
+  get_levels_impl(const request &req, const query_container &query,
+                  std::error_code &error) const noexcept
+    -> std::vector<level_container<lvl_elem_t>> {
+    intervals_client<lvl_elem_t> cl(hostname, port_number, req, query);
+    error = cl.run();
+    if (error)
+      return {};
+    return cl.take_levels(error);
+  }
+
+  template <typename lvl_elem_t>
+  [[nodiscard]] auto
+  get_levels_impl(const request &req, std::error_code &error) const noexcept
+    -> std::vector<level_container<lvl_elem_t>> {
+    bins_client<lvl_elem_t> cl(hostname, port_number, req);
+    error = cl.run();
+    if (error)
+      return {};
+    return cl.take_levels(error);
+  }
+};
+
+// clang-format off
+BOOST_DESCRIBE_STRUCT(methylome_client, (),
+(
+ hostname,
+ port_number,
+ index_hash
+))
+// clang-format on
+
+}  // namespace transferase
+
+/// @brief Enum for error codes related to methylome_client
+enum class methylome_client_error_code : std::uint8_t {
+  ok = 0,
+  error_reading_config_file = 1,
+  required_config_values_not_found = 2,
+};
+
+template <>
+struct std::is_error_code_enum<methylome_client_error_code>
+  : public std::true_type {};
+
+struct methylome_client_error_category : std::error_category {
+  // clang-format off
+  auto name() const noexcept -> const char * override { return "methylome_client"; }
+  auto message(int code) const noexcept -> std::string override {
+    using std::string_literals::operator""s;
+    switch (code) {
+    case 0: return "ok"s;
+    case 1: return "error reading default config file"s;
+    case 2: return "required config values not found"s;
+    }
+    std::unreachable();
+  }
+  // clang-format on
+};
+
+inline auto
+make_error_code(methylome_client_error_code e) noexcept -> std::error_code {
+  static auto category = methylome_client_error_category{};
+  return std::error_code(std::to_underlying(e), category);
+}
+
+#endif  // SRC_METHYLOME_CLIENT_HPP_
