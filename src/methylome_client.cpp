d6022eec8fa26fe1218a3e3e41d93830ba36de24
Andrew D Smith <andrewds@usc.edu>
2025-02-03 16:09:24 -0800

    src/methylome_client.cpp: moved much of the work out of this file and into utilities.cpp and client_config sources

--- src/methylome_client.cpp
+++ src/methylome_client.cpp
@@ -24,6 +24,7 @@
 #include "methylome_client.hpp"
 
 #include "client_config.hpp"
+#include "utilities.hpp"
 
 #include <algorithm>
 #include <cctype>  // for std::isgraph
@@ -35,81 +36,21 @@
 #include <system_error>
 #include <tuple>
 
-[[nodiscard]] inline auto
-rlstrip(const std::string &s) noexcept -> std::string {
-  constexpr auto is_graph = [](const unsigned char c) {
-    return std::isgraph(c);
-  };
-  const auto start = std::ranges::find_if(s, is_graph);
-  auto stop = std::begin(std::ranges::find_last_if(s, is_graph));
-  if (stop != std::cend(s))
-    ++stop;
-  return std::string(start, stop);
-}
-
 namespace transferase {
-
-[[nodiscard]] auto
-split_equals(const std::string &line, std::error_code &error) noexcept
-  -> std::tuple<std::string, std::string> {
-  static constexpr auto delim = '=';
-  const auto eq_pos = line.find(delim);
-  if (eq_pos >= std::size(line)) {
-    error = methylome_client_error_code::error_reading_config_file;
-    return {std::string{}, std::string{}};
-  }
-  const std::string key{rlstrip(line.substr(0, eq_pos))};
-  const std::string value{rlstrip(line.substr(eq_pos + 1))};
-  if (std::size(key) == 0 || std::size(value) == 0) {
-    error = methylome_client_error_code::error_reading_config_file;
-    return {std::string{}, std::string{}};
-  }
-
-  return {key, value};
-}
 
 [[nodiscard]] auto
 methylome_client::get_default(std::error_code &error) noexcept
   -> methylome_client {
-  const auto config_file = client_config::get_config_file_default(error);
+  const auto config = client_config::read(error);
   if (error)
     // error from get_config_file_default
     return {};
 
-  // read the config file
-  std::ifstream in(config_file);
-  if (!in) {
-    error = std::make_error_code(std::errc(errno));
-    return {};
-  }
+  methylome_client mc;
+  mc.hostname = config.hostname;
+  mc.port_number = config.port;
 
-  std::string hostname;
-  std::string port_number;
-
-  std::string line;
-  while (getline(in, line)) {
-    line = rlstrip(line);
-    if (line[0] == '#')
-      continue;
-    const auto [key, value] = split_equals(line, error);
-    if (error)
-      return {};
-    if (key == "hostname")
-      hostname = value;
-    if (key == "port")
-      port_number = value;
-  }
-
-  if (hostname.empty() || port_number.empty()) {
-    error = methylome_client_error_code::required_config_values_not_found;
-    return {};
-  }
-
-  methylome_client ms;
-  ms.hostname = hostname;
-  ms.port_number = port_number;
-
-  return ms;
+  return mc;
 }
 
 }  // namespace transferase
