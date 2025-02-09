c133a50db9de6eabba28bd54e07dbd5e19d83234
Andrew D Smith <andrewds@usc.edu>
2025-02-09 12:38:21 -0800

    python/transferase/client_config_python.hcpp: removing these because they are no longer needed with changes to client_config class

--- python/transferase/client_config_python.cpp
+++ python/transferase/client_config_python.cpp
@@ -26,6 +26,7 @@
 #include "bindings_utils.hpp"
 
 #include <client_config.hpp>  // IWYU pragma: keep
+#include <config_file_utils.hpp>
 #include <logger.hpp>
 #include <utilities.hpp>
 
@@ -38,15 +39,16 @@ namespace transferase {
 auto
 client_config_python::configure_python_system_config(
   const std::vector<std::string> &genomes,
-  const download_policy_t download_policy) const -> void {
-  const auto sys_conf_dir = find_system_config_dir();
-  configure(genomes, sys_conf_dir, download_policy);
+  const download_policy_t download_policy,
+  const std::string &config_dir) const -> void {
+  const auto sys_conf_dir = find_python_sys_config_dir();
+  configure(genomes, download_policy, config_dir, sys_conf_dir);
 }
 
 auto
-client_config_python::write_python(const std::string &directory) -> void {
+client_config_python::save_python(const std::string &directory) -> void {
   std::error_code error;
-  write(directory, error);
+  save(directory, error);
   if (error)
     throw std::system_error(error);
 }
@@ -54,25 +56,19 @@ client_config_python::write_python(const std::string &directory) -> void {
 [[nodiscard]] auto
 client_config_python::read_python(std::error_code &error) noexcept
   -> client_config_python {
-  const auto config_dir = get_config_dir_default(error);
+  const auto config_dir = get_default_config_dir(error);
   if (error)
-    // error from get_config_dir_default
+    // error from get_default_config_dir
     return {};
-
+  // Get the config filename
   const auto config_file = get_config_file(config_dir, error);
   if (error)
     return {};
-
-  const auto key_val = parse_config_file(config_file, error);
+  client_config_python config;
+  parse_config_file(config, config_file, error);
   if (error)
-    return {};
-
-  client_config_python c;
-  error = assign_members(key_val, c);
-  if (error)
-    return {};
-
-  return c;
+    return client_config_python{};
+  return config;
 }
 
 }  // namespace transferase
