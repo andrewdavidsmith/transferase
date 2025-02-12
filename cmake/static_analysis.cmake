048c10581ee23adf0ba7cf7e7d668f89c2118313
Andrew D Smith <andrewds@usc.edu>
2025-02-12 15:42:43 -0800

    cmake/static_analysis.cmake: allowing individual static analysis to be done

--- cmake/static_analysis.cmake
+++ cmake/static_analysis.cmake
@@ -23,63 +23,79 @@
 message(STATUS "Configuring build for static analysis")
 
 # cpplint: all options are in the config file
-find_program(FOUND_CPPLINT cpplint)
-if(FOUND_CPPLINT)
-  message(STATUS "Enabling cpplint analysis")
-  set(CMAKE_CXX_CPPLINT cpplint --quiet)
+if ("cpplint" IN_LIST STATIC_ANALYSIS_CHECKS)
+  find_program(FOUND_CPPLINT cpplint)
+  if(FOUND_CPPLINT)
+    message(STATUS "Enabling cpplint analysis")
+    set(CMAKE_CXX_CPPLINT cpplint --quiet)
+  else()
+    message(STATUS "Could not find cpplint; disabling cpplint")
+  endif()
 endif()
 
 # include-what-you-use: config is a mappings file
-find_program(FOUND_IWYU include-what-you-use)
-if(FOUND_IWYU)
-  message(STATUS "Enabling include-what-you-use analysis")
-  set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE
-    include-what-you-use
-    -Xiwyu
-    --comment_style=long
-    -Xiwyu
-    --quoted_includes_first
-    -Xiwyu
-    --mapping_file=${PROJECT_SOURCE_DIR}/iwyu.json
-  )
+if ("iwyu" IN_LIST STATIC_ANALYSIS_CHECKS)
+  find_program(FOUND_IWYU include-what-you-use)
+  if(FOUND_IWYU)
+    message(STATUS "Enabling include-what-you-use analysis")
+    set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE
+      include-what-you-use
+      -Xiwyu
+      --comment_style=long
+      -Xiwyu
+      --quoted_includes_first
+      -Xiwyu
+      --mapping_file=${PROJECT_SOURCE_DIR}/iwyu.json
+    )
+  else()
+    message(STATUS "Could not find iwyu; disabling iwyu")
+  endif()
 endif()
 
 # cppcheck: options on the command line as there is no config file
-find_program(FOUND_CPPCHECK cppcheck)
-if(FOUND_CPPCHECK)
-  message(STATUS "Enabling cppcheck analysis")
-  set(CMAKE_CXX_CPPCHECK
-    cppcheck
-    --quiet
-    --enable=all
-    --check-level=exhaustive
-    --inline-suppr
-    --suppressions-list=${PROJECT_SOURCE_DIR}/.cppcheck_suppress
-  )
+if ("cppcheck" IN_LIST STATIC_ANALYSIS_CHECKS)
+  find_program(FOUND_CPPCHECK cppcheck)
+  if(FOUND_CPPCHECK)
+    message(STATUS "Enabling cppcheck analysis")
+    set(CMAKE_CXX_CPPCHECK
+      cppcheck
+      --quiet
+      --enable=all
+      --check-level=exhaustive
+      --inline-suppr
+      --suppressions-list=${PROJECT_SOURCE_DIR}/.cppcheck_suppress
+    )
+  else()
+    message(STATUS "Could not find cppcheck; disabling cppcheck")
+  endif()
 endif()
 
 # clang-tidy: need to make sure version is at least 20
-find_program(CLANG_TIDY_EXECUTABLE NAMES clang-tidy)
-# Minimum required version
-set(MIN_CLANG_TIDY_VERSION "20.0.0")
-if(CLANG_TIDY_EXECUTABLE)
-  execute_process(
-    COMMAND
-    bash -c
-    "${CLANG_TIDY_EXECUTABLE} --version | grep version | tr -cd '0-9.\n'"
-    OUTPUT_VARIABLE CLANG_TIDY_VERSION
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-  # Compare the version numbers
-  if(CLANG_TIDY_VERSION VERSION_GREATER_EQUAL MIN_CLANG_TIDY_VERSION)
-    message(STATUS "Enabling clang-tidy (version: ${CLANG_TIDY_VERSION})")
-    set(CMAKE_CXX_CLANG_TIDY
-      clang-tidy
-      --quiet
-      --allow-no-checks
-      -p ${PROJECT_BINARY_DIR}
+if ("clang-tidy" IN_LIST STATIC_ANALYSIS_CHECKS)
+  find_program(CLANG_TIDY_EXECUTABLE NAMES clang-tidy)
+  # Minimum required version
+  set(MIN_CLANG_TIDY_VERSION "20.0.0")
+  if(CLANG_TIDY_EXECUTABLE)
+    execute_process(
+      COMMAND
+      bash -c
+      "${CLANG_TIDY_EXECUTABLE} --version | grep version | tr -cd '0-9.\n'"
+      OUTPUT_VARIABLE CLANG_TIDY_VERSION
+      OUTPUT_STRIP_TRAILING_WHITESPACE
     )
+    # Compare the version numbers
+    if(CLANG_TIDY_VERSION VERSION_GREATER_EQUAL MIN_CLANG_TIDY_VERSION)
+      message(STATUS "Enabling clang-tidy (version: ${CLANG_TIDY_VERSION})")
+      set(CMAKE_CXX_CLANG_TIDY
+        clang-tidy
+        --quiet
+        --allow-no-checks
+        -p ${PROJECT_BINARY_DIR}
+      )
+    else()
+      message(STATUS "Not enabling clang-tidy (min version not found")
+    endif()
   else()
-    message(STATUS "Not enabling clang-tidy (min version not found")
+    message(STATUS "Could not find clang-tidy; disabling clang-tidy")
   endif()
 endif()
