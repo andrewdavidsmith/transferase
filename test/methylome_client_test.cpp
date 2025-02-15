33833269aa7ab421271980e3eb973158bdb93898
Andrew D Smith <andrewds@usc.edu>
2025-02-14 20:51:12 -0800

    test/methylome_client_test.cpp: using a unique_ptr insted of methylome_client_remote because the default constructor is gone

--- test/methylome_client_test.cpp
+++ test/methylome_client_test.cpp
@@ -77,22 +77,22 @@ TEST_F(methylome_client_remote_mock, read_failure) {
 }
 
 TEST_F(methylome_client_remote_mock, read_success) {
-  methylome_client_remote client{};
-  EXPECT_NO_THROW({ client = methylome_client_remote(config_dir); });
-  EXPECT_EQ(client.config.hostname, hostname)
-    << client.config.tostring() << "\n";
-  EXPECT_EQ(client.config.port, port);
-  EXPECT_FALSE(client.config.index_dir.empty());
-  EXPECT_FALSE(client.config.metadata_file.empty());
+  std::unique_ptr<methylome_client_remote> client;
+  EXPECT_NO_THROW({ *client = methylome_client_remote(config_dir); });
+  EXPECT_EQ(client->config.hostname, hostname)
+    << client->config.tostring() << "\n";
+  EXPECT_EQ(client->config.port, port);
+  EXPECT_FALSE(client->config.index_dir.empty());
+  EXPECT_FALSE(client->config.metadata_file.empty());
 
-  EXPECT_EQ(std::size(client.config.meta.genome_to_methylomes),
+  EXPECT_EQ(std::size(client->config.meta.genome_to_methylomes),
             n_lutions_available)
-    << client.config.meta.tostring();
-  for (const auto &d : client.config.meta.genome_to_methylomes)
+    << client->config.meta.tostring();
+  for (const auto &d : client->config.meta.genome_to_methylomes)
     EXPECT_EQ(std::size(d.second), n_lutions_tissues)
-      << client.config.meta.tostring();
+      << client->config.meta.tostring();
 
-  EXPECT_EQ(std::size(client.config.meta.methylome_to_genome),
+  EXPECT_EQ(std::size(client->config.meta.methylome_to_genome),
             n_lutions_available * n_lutions_tissues)
-    << client.config.meta.tostring();
+    << client->config.meta.tostring();
 }
