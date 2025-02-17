ac40576cf1a5efac475c1ca53ea9e830f93f8361
Andrew D Smith <andrewds@usc.edu>
2025-02-16 16:05:37 -0800

    test/command_check_test.cpp: putting args in std::array

--- test/command_check_test.cpp
+++ test/command_check_test.cpp
@@ -42,8 +42,7 @@ TEST(command_check_test, basic_test) {
   };
 
   // Define command line arguments
-  // NOLINTBEGIN(cppcoreguidelines-avoid-c-arrays)
-  const char *command_argv[] = {
+  const auto argv = std::array{
     // clang-format off
     "check",
     "-v",
@@ -60,13 +59,10 @@ TEST(command_check_test, basic_test) {
     methylome_names[2].data(),
     // clang-format on
   };
-  // NOLINTEND(cppcoreguidelines-avoid-c-arrays)
-  const int command_argc = sizeof(command_argv) / sizeof(command_argv[0]);
+  const int argc = sizeof(argv) / sizeof(argv[0]);
 
   // Run the main function
-  // NOLINTBEGIN(cppcoreguidelines-pro-type-const-cast)
-  const int result =
-    command_check_main(command_argc, const_cast<char **>(command_argv));
-  // NOLINTEND(cppcoreguidelines-pro-type-const-cast)
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-const-cast)
+  const int result = command_check_main(argc, const_cast<char **>(argv.data()));
   EXPECT_EQ(result, EXIT_SUCCESS);
 }
