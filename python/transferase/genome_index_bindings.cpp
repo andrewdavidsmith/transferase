635d8576f0c0711f6e2a3353f86919c59f9fc720
Andrew D Smith <andrewds@usc.edu>
2025-02-20 16:45:23 -0800

    python/transferase/genome_index_bindings.cpp: change to variable names and docstring formatting

--- python/transferase/genome_index_bindings.cpp
+++ python/transferase/genome_index_bindings.cpp
@@ -34,33 +34,33 @@
 #include <string>
 #include <vector>
 
-namespace nb = nanobind;
-
 auto
-genome_index_bindings(nb::class_<transferase::genome_index> &cls) -> void {
+genome_index_bindings(nanobind::class_<transferase::genome_index> &cls)
+  -> void {
+  namespace nb = nanobind;
+  namespace xfr = transferase;
   using namespace nanobind::literals;  // NOLINT
   cls.def(nb::init<>())
-    .def("is_consistent", &transferase::genome_index::is_consistent)
-    .def("__hash__", &transferase::genome_index::get_hash)
-    .def("__repr__", &transferase::genome_index::tostring)
+    .def("is_consistent", &xfr::genome_index::is_consistent)
+    .def("__hash__", &xfr::genome_index::get_hash)
+    .def("__repr__", &xfr::genome_index::tostring)
     .def_static("read",
                 nb::overload_cast<const std::string &, const std::string &>(
-                  &transferase::genome_index::read),
+                  &xfr::genome_index::read),
                 R"doc(
     Read a a GenomeIndex object from a directory.
 
     Parameters
     ----------
 
-    directory (str): Directory where the genome_index files can be
-        found.
+    directory (str): Directory where the genome_index files can be found.
 
     genome_name (str): Read the index for the genome with this name.
     )doc",
                 "directory"_a, "genome_name"_a)
     .def("write",
          nb::overload_cast<const std::string &, const std::string &>(
-           &transferase::genome_index::write, nb::const_),
+           &xfr::genome_index::write, nb::const_),
          R"doc(
     Write this GenomeIndex to a directory.
 
@@ -69,35 +69,34 @@ genome_index_bindings(nb::class_<transferase::genome_index> &cls) -> void {
 
     directory (str): The directory in which to write the GenomeIndex.
 
-    genome_name (str): The name of the genome; determines filenames
-        written.
+    genome_name (str): The name of the genome; determines filenames written.
     )doc",
          "directory"_a, "name"_a)
-    .def("make_query", &transferase::genome_index::make_query, R"doc(
-    Construct a QueryContainer object for a given list of GenomicInterval
-    objects.
+    .def("make_query", &xfr::genome_index::make_query,
+         R"doc(
+    Construct a MQuery object for a given list of GenomicInterval objects.
 
     Parameters
     ----------
 
-    intervals (list[GenomicInterval]): A list of GenomicInterval
-        objects, assumed to be sorted within each chromosome.
+    intervals (list[GenomicInterval]): A list of GenomicInterval objects,
+        assumed to be sorted within each chromosome.
     )doc",
          "intervals"_a)
     .def_static("make_genome_index",
                 nb::overload_cast<const std::string &>(
-                  &transferase::genome_index::make_genome_index),
+                  &xfr::genome_index::make_genome_index),
                 R"doc(
     Create a genome index from a reference genome.
 
     Parameters
     ----------
 
-    genome_file (str): Filename for a reference genome in FASTA format
-        (can be gzipped).
+    genome_file (str): Filename for a reference genome in FASTA format (can be
+        gzipped).
     )doc",
                 "genome_file"_a)
-    .def_static("files_exist", &transferase::genome_index::files_exist,
+    .def_static("files_exist", &xfr::genome_index::files_exist,
                 R"doc(
     Check if genome index files exist in a directory.
 
@@ -111,7 +110,7 @@ genome_index_bindings(nb::class_<transferase::genome_index> &cls) -> void {
                 "directory"_a, "genome_name"_a)
     .def_static("list_genome_indexes",
                 nb::overload_cast<const std::string &>(
-                  &transferase::genome_index::list_genome_indexes),
+                  &xfr::genome_index::list_genome_indexes),
                 R"doc(
     Get a list of names of all genome indexes in a directory.
 
@@ -120,7 +119,16 @@ genome_index_bindings(nb::class_<transferase::genome_index> &cls) -> void {
 
     directory (str): Directory to list.
     )doc",
-                "directory"_a)
+                "directory"_a);
+  cls.doc() = R"doc(
+    A GenomeIndex is a data structure that corresponds to a reference genome.
+    The purpose of GenomeIndex objects is to accelerate retrieval of
+    methylation levels for genomic intervals. When stored on disk a
+    genomic_interval is in the for of two files: one a binary data file and
+    the other a JSON format metadata file. These should only be used directly
+    if you are working with your own data. Otherwise they will be handled
+    internally by other functions.
+    )doc"
     //
     ;
 }
