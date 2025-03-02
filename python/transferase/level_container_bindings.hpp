bd62e3245456685f29b649e91db6c8faa6c36a8a
Andrew D Smith <andrewds@usc.edu>
2025-03-01 20:07:53 -0800

    python/transferase/level_container_bindings.hpp: changing original level_container to level_container_md

--- python/transferase/level_container_bindings.hpp
+++ python/transferase/level_container_bindings.hpp
@@ -24,24 +24,26 @@
 #ifndef PYTHON_TRANSFERASE_LEVEL_CONTAINER_BINDINGS_HPP_
 #define PYTHON_TRANSFERASE_LEVEL_CONTAINER_BINDINGS_HPP_
 
+#include <level_container_md.hpp>
+
 #include <nanobind/nanobind.h>
 
 namespace transferase {
 struct level_element_covered_t;
 struct level_element_t;
-template <typename level_element_type> struct level_container;
 }  // namespace transferase
 
 namespace nb = nanobind;
 
 auto
 level_container_bindings(
-  nb::class_<transferase::level_container<transferase::level_element_t>> &cls)
-  -> void;
+  nb::class_<transferase::level_container_md<transferase::level_element_t>>
+    &cls) -> void;
 
 auto
 level_container_covered_bindings(
-  nb::class_<transferase::level_container<transferase::level_element_covered_t>>
-    &cls) -> void;
+  nb::class_<
+    transferase::level_container_md<transferase::level_element_covered_t>> &cls)
+  -> void;
 
 #endif  // PYTHON_TRANSFERASE_LEVEL_CONTAINER_BINDINGS_HPP_
