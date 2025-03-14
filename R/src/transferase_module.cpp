8f2d947ef351a97ae5c3c8832a6ea3ca93de7a15
Andrew D Smith <andrewds@usc.edu>
2025-03-13 22:06:44 -0700

    R/src/transferase_module.cpp: added code to find the transferase system config file relative to the R search path and added a MClientCovered class. Removed the genomic_interval and the genome_index as neither of those is currently used outside the query

--- R/src/transferase_module.cpp
+++ R/src/transferase_module.cpp
@@ -23,78 +23,133 @@
 
 #include "MClient.hpp"
 
-#include "genome_index.hpp"
-#include "genomic_interval.hpp"
+#include "level_element.hpp"
 #include "logger.hpp"
+#include "query_container.hpp"
+#include "system_config.hpp"  // for get_system_config_filename
 
 #include <Rcpp.h>
 
+#include <filesystem>
 #include <ranges>
 #include <string>
 #include <vector>
 
-RCPP_EXPOSED_CLASS_NODECL(transferase::genomic_interval)
-RCPP_EXPOSED_CLASS_NODECL(transferase::genome_index)
-
-auto
-read_genomic_interval(const transferase::genome_index &index,
-                      const std::string &filename) -> Rcpp::NumericMatrix {
-  static constexpr auto n_cols = 3u;
-  const auto intervals = transferase::genomic_interval::read(index, filename);
-  const auto n_rows = std::size(intervals);
-  Rcpp::NumericMatrix m(n_rows, n_cols);
-  for (auto row_id = 0u; row_id < n_rows; ++row_id) {
-    m(row_id, 0) = intervals[row_id].ch_id;
-    m(row_id, 1) = intervals[row_id].start;
-    m(row_id, 2) = intervals[row_id].stop;
+[[nodiscard]] static inline auto
+find_dir(const std::vector<std::string> &paths,
+         const std::string &filename) -> std::string {
+  for (const auto &p : paths) {
+    // Some of the paths given by the environment might not exist
+    const auto path_elem_exists = std::filesystem::exists(p);
+    if (!path_elem_exists)
+      continue;
+    const auto dir_itr = std::filesystem::recursive_directory_iterator(p);
+    for (const auto &dir_entry : dir_itr) {
+      const auto &curr_dir = dir_entry.path();
+      if (std::filesystem::is_directory(curr_dir)) {
+        const auto candidate = curr_dir / filename;
+        if (std::filesystem::exists(candidate))
+          return curr_dir.string();
+      }
+    }
   }
-  return m;
+  throw std::runtime_error(
+    std::format("Failed to locate system config file: {}", filename));
 }
 
-auto
-read_genomic_interval(const std::string &dirname,
-                      const std::string &genome) -> transferase::genome_index {
+[[nodiscard]] static inline auto
+get_package_paths() -> std::vector<std::string> {
+  static constexpr auto raw_lib_paths = ".libPaths";
+  const Rcpp::Function lib_paths_fun(raw_lib_paths);
+  const auto lib_paths = lib_paths_fun();
+  std::vector<std::string> results;
+  for (auto i = 0u; i < LENGTH(lib_paths); ++i) {
+    const auto curr_path = STRING_PTR(lib_paths)[i];
+    results.emplace_back(CHAR(curr_path), LENGTH(curr_path));
+  }
+  return results;
+}
+
+[[nodiscard]] static inline auto
+find_R_sys_config_dir() -> std::string {
+  const auto sys_conf_file = transferase::get_system_config_filename();
+  const auto package_paths = get_package_paths();
+  return find_dir(package_paths, sys_conf_file);
+}
+
+static auto
+configure_transferase(const std::vector<std::string> &genomes,
+                      const std::string &config_dir) -> void {
   try {
-    return transferase::genome_index::read(dirname, genome);
+    const auto download_policy = transferase::download_policy_t::update;
+    const auto sys_config_dir = find_R_sys_config_dir();
+    transferase::client_config cfg(config_dir, sys_config_dir);
+    cfg.install(genomes, download_policy, sys_config_dir);
   }
   catch (const std::exception &e) {
     Rcpp::Rcout << std::format("{}\n", e.what());
-    return {};
   }
 }
+
+static auto
+set_log_level(const std::string &log_level_name) -> void {
+  const auto itr = transferase::str_to_level.find(log_level_name);
+  if (itr == std::cend(transferase::str_to_level)) {
+    Rcpp::Rcout << std::format("Invalid log level: {}\n", log_level_name);
+    return;
+  }
+  transferase::logger::set_level(itr->second);
+}
+
+RCPP_EXPOSED_CLASS_NODECL(transferase::query_container)
+RCPP_EXPOSED_CLASS_NODECL(transferase::level_element_t)
+RCPP_EXPOSED_CLASS_NODECL(transferase::level_element_covered_t)
 
 RCPP_MODULE(transferase) {
   using namespace Rcpp;
 
   namespace xfr = transferase;
 
+  // ADS: this is not to set the log level, but instead to prevent a crash due
+  // to the log output not existing.
   [[maybe_unused]] auto &lgr =
     xfr::logger::instance(std::make_shared<std::ostream>(Rcpp::Rcout.rdbuf()),
                           "transferase", xfr::log_level_t::error);
 
-  Rcpp::class_<MClient>("MClient")
+  Rcpp::class_<MClient<xfr::level_element_t>>("MClient")
     .constructor()
     .constructor<std::string>()
-    .method("tostring", &MClient::tostring, "documentation")
-    .method("query_bins", &MClient::query_bins, "documentation")
-    .method("query_intervals", &MClient::query_intervals, "documentation")
-    //
-    ;
+    .method("tostring", &MClient<xfr::level_element_t>::tostring,
+            "documentation")
+    .method("format_query", &MClient<xfr::level_element_t>::format_query,
+            "documentation")
+    .method("query_bins", &MClient<xfr::level_element_t>::query_bins,
+            "documentation")
+    .method("query_intervals", &MClient<xfr::level_element_t>::query_intervals,
+            "documentation")
+    .method("query_preprocessed",
+            &MClient<xfr::level_element_t>::query_preprocessed,
+            "documentation");
 
-  Rcpp::class_<xfr::genomic_interval>("GenomicInterval")
+  Rcpp::class_<MClient<xfr::level_element_covered_t>>("MClientCovered")
     .constructor()
-    .field("ch_id", &xfr::genomic_interval::ch_id, "documentation")
-    .field("start", &xfr::genomic_interval::start, "documentation")
-    .field("stop", &xfr::genomic_interval::stop, "documentation")
-    //
-    ;
+    .constructor<std::string>()
+    .method("tostring", &MClient<xfr::level_element_covered_t>::tostring,
+            "documentation")
+    .method("format_query",
+            &MClient<xfr::level_element_covered_t>::format_query,
+            "documentation")
+    .method("query_bins", &MClient<xfr::level_element_covered_t>::query_bins,
+            "documentation")
+    .method("query_intervals",
+            &MClient<xfr::level_element_covered_t>::query_intervals,
+            "documentation")
+    .method("query_preprocessed",
+            &MClient<xfr::level_element_covered_t>::query_preprocessed,
+            "documentation");
 
-  Rcpp::class_<xfr::genome_index>("GenomeIndex")
-    .constructor()
-    .method("tostring", &xfr::genome_index::tostring, "documentation")
-    //
-    ;
+  Rcpp::class_<xfr::query_container>("QueryContainer").constructor();
 
-  function("read_genomic_interval", &read_genomic_interval, "documentation");
-  function("read_genome_index", &read_genomic_interval, "documentation");
+  Rcpp::function("transferase_config", &configure_transferase, "documentation");
+  Rcpp::function("transferase_set_log_level", &set_log_level, "documentation");
 }
