2e07c0d2ed1695eebdff57271f58372aeed0c269
Andrew D Smith <andrewds@usc.edu>
2025-03-14 23:45:24 -0700

    lib/server.hcpp: removing all the server error code stuff and putting it into server_error_code.hpp

--- lib/server.hpp
+++ lib/server.hpp
@@ -25,8 +25,9 @@
 #define LIB_SERVER_HPP_
 
 #include "request_handler.hpp"
+#include "server_error_code.hpp"
 
-#include <asio.hpp>
+#include "asio.hpp"
 
 #include <atomic>
 #include <cstdint>
@@ -75,51 +76,5 @@ struct server {
 };
 
 }  // namespace transferase
-
-enum class server_error_code : std::uint8_t {
-  // clang-format off
-  ok =                     0,
-  invalid_methylome_name = 1,
-  invalid_request_type =   2,
-  bin_size_too_small =     3,
-  too_many_intervals =     4,
-  invalid_index_hash =     5,
-  methylome_not_found =    6,
-  index_not_found =        7,
-  server_failure =         8,
-  bad_request =            9,
-  // clang-format on
-};
-
-template <>
-struct std::is_error_code_enum<server_error_code> : public std::true_type {};
-
-struct server_error_category : std::error_category {
-  // clang-format off
-  auto name() const noexcept -> const char * override {return "server_error_code";}
-  auto message(int code) const -> std::string override {
-    using std::string_literals::operator""s;
-    switch (code) {
-    case 0: return "ok"s;
-    case 1: return "invalid methylome name"s;
-    case 2: return "invalid request type"s;
-    case 3: return "bin size too small"s;
-    case 4: return "too many intervals"s;
-    case 5: return "invalid index hash"s;
-    case 6: return "methylome not found"s;
-    case 7: return "index not found"s;
-    case 8: return "server failure"s;
-    case 9: return "bad request"s;
-    }
-    std::unreachable();
-  }
-  // clang-format on
-};
-
-inline auto
-make_error_code(server_error_code e) -> std::error_code {
-  static auto category = server_error_category{};
-  return std::error_code(std::to_underlying(e), category);
-}
 
 #endif  // LIB_SERVER_HPP_
