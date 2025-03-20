e884a55d0220e8d48985b88a349d55d23e933235
Andrew D Smith <andrewds@usc.edu>
2025-03-19 18:10:18 -0700

    python/transferase/query_container_bindings.cpp: added a function to get the number of CpG sites in each query interval

--- python/transferase/query_container_bindings.cpp
+++ python/transferase/query_container_bindings.cpp
@@ -28,6 +28,7 @@
 #include <nanobind/nanobind.h>
 #include <nanobind/operators.h>   // IWYU pragma: keep
 #include <nanobind/stl/string.h>  // IWYU pragma: keep
+#include <nanobind/stl/vector.h>  // IWYU pragma: keep
 
 #include <format>
 #include <iterator>  // for std::size
@@ -42,19 +43,28 @@ auto
 query_container_bindings(nb::class_<transferase::query_container> &cls)
   -> void {
   namespace xfr = transferase;
-  cls.def(nb::init<>())
-    .def("__len__", &xfr::query_container::size)
-    .def(nanobind::self == nanobind::self)
-    .def(nanobind::self != nanobind::self)
-    .def("__repr__",
-         [](const xfr::query_container &self) -> std::string {
-           return std::format("<MQuery size={}>", std::size(self));
-         })
-    .doc() = R"doc(
+  cls.def(nb::init<>());
+  cls.def("__len__", &xfr::query_container::size);
+  cls.def(nanobind::self == nanobind::self);
+  cls.def(nanobind::self != nanobind::self);
+  cls.def("__repr__", [](const xfr::query_container &self) -> std::string {
+    return std::format("<MQuery size={}>", std::size(self));
+  });
+  cls.def("get_n_cpgs", &xfr::query_container::get_n_cpgs,
+          R"doc(
+    Get a list with the number of CpG sites in each intervals among those
+    represented by this MQuery object. Note: the number of CpG sites is purely
+    a function of the reference genome and the set of genomic intervals, and
+    not related to any particular methylome.
+    )doc");
+  cls.doc() = R"doc(
     A MQuery is a representation for a list of GenomicInterval objects that
-    has been packaged for use in a transferase query. You can't do anything
-    else with a MQuery, but it allows you to avoid repeating work if you want
-    to use the same set of GenomicIntervals in more than one query.
+    has been packaged for use in a transferase query. The only other thing you
+    can do with a MQuery object is get a list of the number of CpG sites per
+    query interval. The MQuery objects allow you to avoid repeating work if
+    you want to use the same set of GenomicIntervals in more than one
+    query. This makes repeated queries with different methylomes, but the same
+    genomic intervals, faster.
     )doc"
     //
     ;
