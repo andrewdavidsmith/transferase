114166c072c1c7b494d9df6aa0a7d58ff9b576d3
Andrew D Smith <andrewds@usc.edu>
2025-04-04 20:53:53 -0700

    renaming files for shorter names

---
+++ lib/local_client.hpp
@@ -0,0 +1,189 @@
+/* MIT License
+ *
+ * Copyright (c) 2025 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LIB_LOCAL_CLIENT_HPP_
+#define LIB_LOCAL_CLIENT_HPP_
+
+#include "client_base.hpp"
+
+#include "client_config.hpp"
+#include "genome_index.hpp"
+#include "genome_index_set.hpp"
+#include "level_container_md.hpp"
+#include "methylome.hpp"
+#include "query_container.hpp"
+#include "request.hpp"
+#include "request_type_code.hpp"
+
+#include <cassert>
+#include <cstdint>
+#include <format>    // for std::vector??
+#include <iterator>  // for std::size
+#include <memory>
+#include <string>
+#include <system_error>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <variant>  // for std::tuple
+#include <vector>
+
+// forward declarations
+namespace transferase {
+struct genomic_interval;
+struct level_element_covered_t;
+}  // namespace transferase
+
+namespace transferase {
+
+class local_client : public client_base<local_client> {
+public:
+  explicit local_client(const std::string &config_dir) :
+    client_base(config_dir) {
+    std::error_code error;
+    validate(error);
+    if (error)
+      throw std::system_error(error, "[Failed in local constructor]");
+  }
+
+  auto
+  validate(std::error_code &error) noexcept -> void;
+
+  // intervals: takes a query
+  template <typename lvl_elem_t>
+  [[nodiscard]] auto
+  get_levels(const std::vector<std::string> &methylome_names,
+             const query_container &query) const
+    -> level_container_md<lvl_elem_t> {
+    std::error_code error{};
+    auto result = get_levels_impl<lvl_elem_t>(methylome_names, query, error);
+    if (error)
+      throw std::system_error(error);
+    return result;
+  }
+
+  // intervals: takes a vector of genomic intervals
+  template <typename lvl_elem_t>
+  [[nodiscard]] auto
+  get_levels(const std::vector<std::string> &methylome_names,
+             const std::vector<genomic_interval> &intervals) const
+    -> level_container_md<lvl_elem_t> {
+    std::error_code error{};
+    const auto [genome_name, _] = methylome::get_genome_info(
+      config.get_methylome_dir(), methylome_names.at(0), error);
+    if (error)
+      throw std::system_error(error);
+    const auto index = indexes->get_genome_index(genome_name, error);
+    if (error)
+      throw std::system_error(error);
+    const auto query = index->make_query(intervals);
+    auto result = get_levels_impl<lvl_elem_t>(methylome_names, query, error);
+    if (error)
+      throw std::system_error(error);
+    return result;
+  }
+
+  // bins: takes an index
+  template <typename lvl_elem_t>
+  [[nodiscard]] auto
+  get_levels(const std::vector<std::string> &methylome_names,
+             const std::uint32_t bin_size) const
+    -> level_container_md<lvl_elem_t> {
+    std::error_code error{};
+    const auto [genome_name, _] = methylome::get_genome_info(
+      config.get_methylome_dir(), methylome_names.at(0), error);
+    if (error)
+      return {};
+    const auto index = indexes->get_genome_index(genome_name, error);
+    if (error)
+      return {};
+    auto result =
+      get_levels_impl<lvl_elem_t>(methylome_names, *index, bin_size, error);
+    if (error)
+      throw std::system_error(error);
+    return result;
+  }
+
+private:
+  template <typename lvl_elem_t>
+  [[nodiscard]] auto
+  get_levels_impl(const std::vector<std::string> &methylome_names,
+                  const query_container &query, std::error_code &error)
+    const noexcept -> level_container_md<lvl_elem_t> {
+    level_container_md<lvl_elem_t> results(std::size(query),
+                                           std::size(methylome_names));
+    bool first_methylome = true;
+    std::uint64_t index_hash = 0;
+    std::uint32_t col_id = 0;
+    for (const auto &methylome_name : methylome_names) {
+      const auto meth =
+        methylome::read(config.get_methylome_dir(), methylome_name, error);
+      if (error)
+        return {};
+      if (first_methylome) {
+        index_hash = meth.get_index_hash();
+        first_methylome = false;
+      }
+      else if (index_hash != meth.get_index_hash()) {
+        error = client_error_code::inconsistent_methylome_metadata;
+        return {};
+      }
+      meth.get_levels<lvl_elem_t>(query, results.column_itr(col_id++));
+    }
+    return results;
+  }
+
+  template <typename lvl_elem_t>
+  [[nodiscard]] auto
+  get_levels_impl(const std::vector<std::string> &methylome_names,
+                  const genome_index &index, const std::uint32_t bin_size,
+                  std::error_code &error) const noexcept
+    -> level_container_md<lvl_elem_t> {
+    level_container_md<lvl_elem_t> results(index.get_n_bins(bin_size),
+                                           std::size(methylome_names));
+    bool first_methylome = true;
+    std::uint64_t index_hash = 0;
+    std::uint32_t col_id = 0;
+    for (const auto &methylome_name : methylome_names) {
+      const auto meth =
+        methylome::read(config.get_methylome_dir(), methylome_name, error);
+      if (error)
+        return {};
+      if (first_methylome) {
+        index_hash = meth.get_index_hash();
+        first_methylome = false;
+      }
+      else if (index_hash != meth.get_index_hash()) {
+        error = client_error_code::inconsistent_methylome_metadata;
+        return {};
+      }
+      meth.get_levels<lvl_elem_t>(bin_size, index,
+                                  results.column_itr(col_id++));
+    }
+    return results;
+  }
+};
+
+}  // namespace transferase
+
+#endif  // LIB_LOCAL_CLIENT_HPP_
---
+++ lib/remote_client.hpp
@@ -0,0 +1,164 @@
+/* MIT License
+ *
+ * Copyright (c) 2025 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LIB_METHYLOME_CLIENT_REMOTE_HPP_
+#define LIB_METHYLOME_CLIENT_REMOTE_HPP_
+
+#include "client_base.hpp"
+#include "client_config.hpp"
+#include "client_connection.hpp"
+#include "genome_index.hpp"
+#include "genome_index_set.hpp"
+#include "level_container_md.hpp"
+#include "query_container.hpp"
+#include "request.hpp"
+#include "request_type_code.hpp"
+
+#include <cstdint>
+#include <format>
+#include <iterator>  // for std::size
+#include <memory>
+#include <string>
+#include <system_error>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+// forward declarations
+namespace transferase {
+struct genomic_interval;
+struct level_element_covered_t;  // for is_same_v
+}  // namespace transferase
+
+namespace transferase {
+
+class methylome_client_remote : public client_base<methylome_client_remote> {
+public:
+  typedef client_base<methylome_client_remote> methylome_client_remote_parent;
+
+  explicit methylome_client_remote(const std::string &config_dir) :
+    client_base(config_dir) {
+    std::error_code error;
+    validate(error);
+    if (error)
+      throw std::system_error(error, "[Failed in remote constructor]");
+  }
+
+  auto
+  validate(std::error_code &error) noexcept -> void;
+
+  // intervals: takes a query, genome name specified
+  template <typename lvl_elem_t>
+  [[nodiscard]] auto
+  get_levels(
+    const std::string &genome, const std::vector<std::string> &methylome_names,
+    const query_container &query) const -> level_container_md<lvl_elem_t> {
+    request_type_code req_type = request_type_code::intervals;
+    if constexpr (std::is_same_v<lvl_elem_t, level_element_covered_t>)
+      req_type = request_type_code::intervals_covered;
+    std::error_code error{};
+    const auto index = indexes->get_genome_index(genome, error);
+    if (error)
+      throw std::system_error(error);
+    const auto req =
+      request{req_type, index->get_hash(), std::size(query), methylome_names};
+    auto result = get_levels_impl<lvl_elem_t>(req, query, error);
+    if (error)
+      throw std::system_error(error);
+    return result;
+  }
+
+  // intervals: takes a vector of genomic intervals, genome name specified
+  template <typename lvl_elem_t>
+  [[nodiscard]] auto
+  get_levels(const std::string &genome,
+             const std::vector<std::string> &methylome_names,
+             const std::vector<genomic_interval> &intervals) const
+    -> level_container_md<lvl_elem_t> {
+    request_type_code req_type = request_type_code::intervals;
+    if constexpr (std::is_same_v<lvl_elem_t, level_element_covered_t>)
+      req_type = request_type_code::intervals_covered;
+    std::error_code error{};
+    const auto index = indexes->get_genome_index(genome, error);
+    if (error)
+      throw std::system_error(error);
+    const auto query = index->make_query(intervals);
+    const auto req =
+      request{req_type, index->get_hash(), std::size(query), methylome_names};
+    auto result = get_levels_impl<lvl_elem_t>(req, query, error);
+    if (error)
+      throw std::system_error(error);
+    return result;
+  }
+
+  // bins: takes an index
+  template <typename lvl_elem_t>
+  [[nodiscard]] auto
+  get_levels(
+    const std::string &genome, const std::vector<std::string> &methylome_names,
+    const std::uint32_t bin_size) const -> level_container_md<lvl_elem_t> {
+    request_type_code req_type = request_type_code::bins;
+    if constexpr (std::is_same_v<lvl_elem_t, level_element_covered_t>)
+      req_type = request_type_code::bins_covered;
+    std::error_code error{};
+    const auto index = indexes->get_genome_index(genome, error);
+    if (error)
+      throw std::system_error(error);
+    const auto req =
+      request{req_type, index->get_hash(), bin_size, methylome_names};
+    auto result = get_levels_impl<lvl_elem_t>(req, error);
+    if (error)
+      throw std::system_error(error);
+    return result;
+  }
+
+private:
+  template <typename lvl_elem_t>
+  [[nodiscard]] auto
+  get_levels_impl(const request &req, const query_container &query,
+                  std::error_code &error) const noexcept
+    -> level_container_md<lvl_elem_t> {
+    intervals_client_connection<lvl_elem_t> cl(config.hostname, config.port,
+                                               req, query);
+    error = cl.run();
+    if (error)
+      return {};
+    return cl.take_levels();
+  }
+
+  template <typename lvl_elem_t>
+  [[nodiscard]] auto
+  get_levels_impl(const request &req, std::error_code &error) const noexcept
+    -> level_container_md<lvl_elem_t> {
+    bins_client_connection<lvl_elem_t> cl(config.hostname, config.port, req);
+    error = cl.run();
+    if (error)
+      return {};
+    return cl.take_levels();
+  }
+};
+
+}  // namespace transferase
+
+#endif  // LIB_METHYLOME_CLIENT_REMOTE_HPP_
