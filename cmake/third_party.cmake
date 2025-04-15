f3b38c454ff9c512d777a5166172051bd7165c1b
Andrew D Smith <andrewds@usc.edu>
2025-04-14 19:18:44 -0700

    cmake/third_party.cmake: removing things related to nanobind and simplifying the zlib stuff

--- cmake/third_party.cmake
+++ cmake/third_party.cmake
@@ -24,36 +24,28 @@ message(STATUS "Locating REQUIRED third-party packages")
 
 # ZLib
 if(BUILD_PYTHON)
-  # Typically ZLib isn't built with -fPIC, but it's small so we can
-  # clone and build it if needed
+  # Typically ZLib isn't built with -fPIC, but it's small so we
+  # clone and build it with POSITION_INDEPENDENT_CODE=on here
   message(STATUS "Configuring to clone ZLib for Python API")
   include(ExternalProject)
+  set(ZLIB_CMAKE_ARGS
+    -DZLIB_BUILD_EXAMPLES=off
+    -DSKIP_INSTALL_FILES=on
+    -DCMAKE_POSITION_INDEPENDENT_CODE=on
+    -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/src/zlib
+    -DCMAKE_BUILD_TYPE=Release
+  )
   if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-    ExternalProject_Add(
-      ZLIB
-      GIT_REPOSITORY https://github.com/madler/zlib.git
-      GIT_TAG master
-      CMAKE_ARGS
+    list(APPEND ZLIB_CMAKE_ARGS
       -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}
-      -DZLIB_BUILD_EXAMPLES=off
-      -DSKIP_INSTALL_FILES=on
-      -DCMAKE_POSITION_INDEPENDENT_CODE=on
-      -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/src/zlib
-      -DCMAKE_BUILD_TYPE=Release
-    )
-  else()
-    ExternalProject_Add(
-      ZLIB
-      GIT_REPOSITORY https://github.com/madler/zlib.git
-      GIT_TAG master
-      CMAKE_ARGS
-      -DZLIB_BUILD_EXAMPLES=off
-      -DSKIP_INSTALL_FILES=on
-      -DCMAKE_POSITION_INDEPENDENT_CODE=on
-      -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/src/zlib
-      -DCMAKE_BUILD_TYPE=Release
     )
   endif()
+  ExternalProject_Add(
+    ZLIB
+    GIT_REPOSITORY https://github.com/madler/zlib.git
+    GIT_TAG master
+    CMAKE_ARGS ${ZLIB_CMAKE_ARGS}
+  )
   # Include the built zlib headers and link against the built zlib library
   set(ZLIB_INCLUDE_DIR "${PROJECT_BINARY_DIR}/src/zlib/include")
   set(ZLIB_LIBRARY "${PROJECT_BINARY_DIR}/src/zlib/lib/libz.a")
@@ -69,48 +61,4 @@ if(BUILD_PYTHON)
   add_library(ZLIB::ZLIB ALIAS ZLIB_IMPORTED)
 else()
   find_package(ZLIB REQUIRED)
-endif()
-
-# Python and nanobind
-if(BUILD_PYTHON)
-  message(STATUS "Configuring nanobind for Python API")
-
-  ## Try to find nanobind on the system first
-  # For nanobind before Python; seems to work with ordinary
-  # 'Development' but Development.Module is recommended
-  set(DEV_MODULE Development.Module)
-
-  # Python has to be available before nanobind
-  find_package(Python COMPONENTS Interpreter ${DEV_MODULE} REQUIRED)
-  message(STATUS "Python include dir: ${Python_INCLUDE_DIRS}")
-  message(STATUS "Python libraries: ${Python_LIBRARIES}")
-  message(STATUS "Python version: ${Python_VERSION}")
-  message(STATUS "Python SOABI: ${Python_SOABI}")
-
-  # Detect the installed nanobind package
-  execute_process(
-    COMMAND "${Python_EXECUTABLE}" -m nanobind --cmake_dir
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    OUTPUT_VARIABLE nanobind_ROOT
-    ERROR_QUIET
-  )
-  if (nanobind_ROOT)
-    message(STATUS "Found nanobind on the system")
-  else()
-    message(STATUS "Configuring to clone nanobind")
-    # Didn't find nanobind so try to clone it
-    include(ExternalProject)
-    ExternalProject_Add(
-      nanobind
-      GIT_REPOSITORY https://github.com/wjakob/nanobind.git
-      GIT_TAG master
-      CMAKE_ARGS
-      -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/src/nanobind
-      -DCMAKE_BUILD_TYPE=Release
-    )
-  endif()
-  # Now try to import nanobind; if this step is not done here, then in
-  # python/CMakeLists.txt the
-  # nanobind_build_library(nanobind-static-abi3) will fail.
-  find_package(nanobind CONFIG)
 endif()
