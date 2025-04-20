89754def32b86a212d37e800553f38c76601787f
Andrew D Smith <andrewds@usc.edu>
2025-04-19 18:06:41 -0700

    lib/transfer_stats.hpp: adding a class to keep track of transfer stats for both client and server connections

---
+++ lib/transfer_stats.hpp
@@ -0,0 +1,55 @@
+/* MIT License
+ *
+ * Copyright (c) 2025 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LIB_TRANSFER_STATS_HPP_
+#define LIB_TRANSFER_STATS_HPP_
+
+#include <algorithm>
+#include <cstdint>
+#include <format>
+#include <limits>
+#include <string>
+
+struct transfer_stats {
+  std::uint32_t n_xfrs{};
+  std::uint32_t xfr_bytes{};
+  std::uint32_t min_xfr_size{std::numeric_limits<std::uint32_t>::max()};
+  std::uint32_t max_xfr_size{};
+
+  auto
+  update(const std::uint32_t n_bytes) -> void {
+    ++n_xfrs;
+    xfr_bytes += n_bytes;
+    max_xfr_size = std::max(max_xfr_size, n_bytes);
+    min_xfr_size = std::min(min_xfr_size, n_bytes);
+  }
+
+  [[nodiscard]] auto
+  str() const -> std::string {
+    static constexpr auto fmt = "{}B, xfrs={}, max={}B, min={}B, mean={}B";
+    return std::format(fmt, xfr_bytes, n_xfrs, max_xfr_size, min_xfr_size,
+                       xfr_bytes / n_xfrs);
+  }
+};
+
+#endif  // LIB_TRANSFER_STATS_HPP_
