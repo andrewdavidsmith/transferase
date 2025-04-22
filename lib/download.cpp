ddafb2e32fd226b6bae415ce07f667db0f832a1c
Andrew D Smith <andrewds@usc.edu>
2025-04-21 20:36:35 -0700

    lib/download.cpp: making this code more concise due to the updates in http_client code and the merging of http and https

--- lib/download.cpp
+++ lib/download.cpp
@@ -25,10 +25,10 @@
 
 #include "http_client.hpp"
 #include "http_header.hpp"
-#include "https_client.hpp"
 
 #include <cerrno>
 #include <chrono>
+#include <ctime>
 #include <filesystem>
 #include <fstream>
 #include <print>    // IWYU pragma: keep
@@ -37,8 +37,6 @@
 #include <system_error>
 #include <tuple>  // IWYU pragma: keep
 #include <unordered_map>
-
-#include <ctime>
 
 namespace transferase {
 
@@ -50,45 +48,36 @@ download(const download_request &dr)
   const auto outdir = fs::path{dr.outdir};
   const auto outfile = outdir / fs::path(dr.target).filename();
 
+  std::unordered_map<std::string, std::string> m;
+
   {
     std::error_code out_ec;
     if (fs::exists(outdir) && !fs::is_directory(outdir)) {
       out_ec = std::make_error_code(std::errc::file_exists);
-      // std::println(R"(Error validating output directory "{}": {})",
-      //              outdir.string(), out_ec.message());
-      return {std::unordered_map<std::string, std::string>{}, out_ec};
+      return {m, out_ec};
     }
     if (!fs::exists(outdir)) {
       const bool made_dir = fs::create_directories(outdir, out_ec);
-      if (!made_dir) {
-        // std::println(R"(Error output directory does not exist "{}": {})",
-        //              outdir.string(), out_ec.message());
-        return {std::unordered_map<std::string, std::string>{}, out_ec};
-      }
+      if (!made_dir)
+        return {m, out_ec};
     }
     std::ofstream out_test(outfile);
     if (!out_test) {
       out_ec = std::make_error_code(std::errc(errno));
-      // std::println(R"(Error validating output file: "{}": {})",
-      //              outfile.string(), out_ec.message());
-      return {std::unordered_map<std::string, std::string>{}, out_ec};
+      return {m, out_ec};
     }
     const bool remove_ok = fs::remove(outfile, out_ec);
     if (!remove_ok)
-      return {std::unordered_map<std::string, std::string>{}, out_ec};
+      return {m, out_ec};
   }
 
   const auto [header, error] =
-    (dr.port == "443")
-      ? download_https(dr.host, dr.port, dr.target, outfile, dr.connect_timeout,
-                       dr.download_timeout, dr.show_progress)
-      : download_http(dr.host, dr.port, dr.target, outfile, dr.connect_timeout,
-                      dr.download_timeout, dr.show_progress);
+    download_http(dr.host, dr.port, dr.target, outfile, dr.download_timeout,
+                  dr.show_progress);
 
   if (error)
-    return {std::unordered_map<std::string, std::string>{}, error};
+    return {m, error};
 
-  std::unordered_map<std::string, std::string> m;
   m.emplace("status", header.status_code);
   m.emplace("last-modified", header.last_modified);
   m.emplace("content-length", std::to_string(header.content_length));
@@ -101,33 +90,13 @@ download(const download_request &dr)
 auto
 get_timestamp(const download_request &dr)
   -> std::chrono::time_point<std::chrono::file_clock> {
-  // static constexpr auto http_time_format = "%a, %d %b %Y %T %z";
-
-  http_header header;
-
-  if (dr.port == "443") {
-    header = download_header_https(dr.host, dr.port, dr.target,
-                                   dr.connect_timeout, dr.download_timeout);
-  }
-  else {  // if (dr.port == "80") {
-    header = download_header_http(dr.host, dr.port, dr.target,
-                                  dr.connect_timeout, dr.download_timeout);
-  }
-
-  // clang-format off
-  struct tm tm{};
-  // clang-format on
-  strptime(header.last_modified.data(), "%a, %d %b %Y %H:%M:%S GMT", &tm);
-  const std::time_t u = std::mktime(&tm);
-  namespace chrono = std::chrono;
-  return chrono::file_clock::from_sys(chrono::system_clock::from_time_t(u));
-
-  // std::istringstream is{header.last_modified};
-  // std::chrono::time_point<std::chrono::file_clock> tp;
-  // if (!(is >> std::chrono::parse(http_time_format, tp)))
-  //   return {};
-
-  // return tp;
+  static constexpr auto http_time_format = "%a, %d %b %Y %H:%M:%S GMT";
+  const auto header =
+    download_header_http(dr.host, dr.port, dr.target, dr.download_timeout);
+  struct tm t;
+  strptime(header.last_modified.data(), http_time_format, &t);
+  const auto stime = std::chrono::system_clock::from_time_t(std::mktime(&t));
+  return std::chrono::file_clock::from_sys(stime);
 }
 
 }  // namespace transferase
