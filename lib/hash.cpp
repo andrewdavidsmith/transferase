ccc5400ade4ca1ba46a3bb0f91fcf0c8349622f1
Andrew D Smith <andrewds@usc.edu>
2025-12-14 14:32:26 -0800

    lib/hash.hcpp: linting

--- lib/hash.cpp
+++ lib/hash.cpp
@@ -1,6 +1,6 @@
 /* MIT License
  *
- * Copyright (c) 2024 Andrew D Smith
+ * Copyright (c) 2024-2025 Andrew D Smith
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,8 +22,6 @@
  */
 
 #include "hash.hpp"
-
-#include <zlib.h>  // for adler32_z
 
 #include <cassert>
 #include <cerrno>
